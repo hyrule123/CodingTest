1d50e808d7ed6805a01c8d8a3983faf451696ba6
HWNOTE <ekdrn1@naver.com>
2024-04-22 12:54:58 +0900

    백준 10926

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,26 +1,25 @@
 ﻿#ifndef LOCAL
+#define USING_IOSTREAM (void)0
 #define READ_INPUT (void)0
 #define WRITE_OUTPUT (void)0
 #endif //NDEF LOCAL
 
+int sLen;
 
 #include <stdio.h>
+#include <string.h>
 int main()
 {
     READ_INPUT;
     WRITE_OUTPUT;
 
-    int a, b;
+    char str[50 + 3];
 
-    (void)scanf("%d %d", &a, &b);
+    (void)scanf("%s", &str);
 
-    //두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
-    (void)printf("%d\n", a + b);
-    (void)printf("%d\n", a - b);
-    (void)printf("%d\n", a * b);
-    (void)printf("%d\n", a / b);
-    (void)printf("%d", a % b);
+    (void)strcat(str, "??!");
 
+    (void)printf("%s", str);
 
     return 0;
 }
