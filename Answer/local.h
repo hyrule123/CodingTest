907612ea26fffd29008368ae2f9a0c81cdee57f8
HWNOTE <ekdrn1@naver.com>
2024-04-22 12:25:39 +0900

    백준 10998

--- Answer/local.h
+++ Answer/local.h
@@ -5,7 +5,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#define PAUSE printf("\n"); system("pause")
+
+//#define PAUSE \system("pause")
 
 #define READ_INPUT (void)freopen("input.txt", "r", stdin)
 #define WRITE_OUTPUT (void)freopen("output.txt", "w", stdout)
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,5 +1,4 @@
 ﻿#ifndef LOCAL
-#define PAUSE (void)0
 #define READ_INPUT (void)0
 #define WRITE_OUTPUT (void)0
 #endif //NDEF LOCAL
@@ -14,8 +13,8 @@ int main()
     int a, b;
     
     (void)scanf("%d %d", &a, &b);
-    (void)printf("%d", a - b);
+    (void)printf("%d", a * b);
 
-    PAUSE;
+
     return 0;
 }
