aab683762d913ce5eaf33c7fc98e6f4de5be391b
HWNOTE <ekdrn1@naver.com>
2024-04-22 12:54:39 +0900

    local.h에서 헤더 include 제거, USING_IOSTREAM 매크로 추가

--- Answer/local.h
+++ Answer/local.h
@@ -4,9 +4,9 @@
 #ifdef LOCAL
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <iostream>
-
-//#define PAUSE \system("pause")
+#define USING_IOSTREAM \
+std::ios_base::sync_with_stdio(false);\
+std::cin.tie(nullptr)
 
 #define READ_INPUT (void)freopen("input.txt", "r", stdin)
 #define WRITE_OUTPUT (void)freopen("output.txt", "w", stdout)
