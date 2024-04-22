15644e9f599e6decff6d705f80ef5d3142cb81d7
HWNOTE <ekdrn1@naver.com>
2024-04-22 12:35:40 +0900

    백준 10869

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,7 +13,13 @@ int main()
     int a, b;
 
     (void)scanf("%d %d", &a, &b);
-    (void)printf("%.9lf", (double)a / (double)b);
+
+    //두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+    (void)printf("%d\n", a + b);
+    (void)printf("%d\n", a - b);
+    (void)printf("%d\n", a * b);
+    (void)printf("%d\n", a / b);
+    (void)printf("%d", a % b);
 
 
     return 0;
