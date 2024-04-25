c23f898bda0b0fee5f2cf8e1e820e6c5786ce068
HWNOTE <ekdrn1@naver.com>
2024-04-25 21:30:34 +0900

    읽기 쓰기 모드를 "rt", "wt" 로 변경함.

--- Answer/local.h
+++ Answer/local.h
@@ -8,7 +8,7 @@
 std::ios_base::sync_with_stdio(false);\
 std::cin.tie(nullptr)
 
-#define READ_INPUT (void)freopen("input.txt", "r", stdin)
-#define WRITE_OUTPUT (void)freopen("output.txt", "w", stdout)
+#define READ_INPUT (void)freopen("input.txt", "rt", stdin)
+#define WRITE_OUTPUT (void)freopen("output.txt", "wt", stdout)
 
 #endif //LOCAL
