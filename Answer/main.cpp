bcf7e359b6b3c4972ffd1bc1160d89edae2ee143
HWNOTE <ekdrn1@naver.com>
2024-04-26 17:13:22 +0900

    백준 1152 (단어의 개수)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -12,7 +12,7 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 #include <limits>
 
-#include <vector>
+#include <string>
 
 int main()
 {
@@ -23,29 +23,14 @@ int main()
 
     using namespace std;
 
-    int T{};
-    std::cin >> T;
-
-    int R{}; char S[21]{};
-    char output[161]{};
-
-    for (int i = 0; i < T; ++i)
+    std::string S{};
+    int vocCount{};
+    while (std::cin >> S)
     {
-        std::cin >> R >> S;
-
-        for (int j = 0; j < 21; ++j)
-        {
-            if ('\0' == S[j])
-            {
-                int eot = j * R;
-                output[eot] = '\n';
-                output[eot + 1] = '\0';
-                break;
-            }
-
-            memset(output + (j * R), S[j], R);
-        }
-        std::cout << output;
+        ++vocCount;
     }
+    
+    std::cout << vocCount;
+
     return 0;
 }
