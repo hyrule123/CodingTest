2f69f0007f09cef3125ce9478baec97a2b76cf3a
HWNOTE <ekdrn1@naver.com>
2024-04-28 23:49:32 +0900

    백준 9506 (약수들의 합)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -23,29 +23,55 @@ int main()
 
     using namespace std;
 
-    int N, K;
-    std::cin >> N >> K;
-    --K;
-
     std::vector<int> divisors{};
     divisors.reserve(100);
-    divisors.push_back(1);
-    for (int i = 2; i < N; ++i)
+    while (true)
     {
-        if (0 == N % i)
+        int n{};
+        std::cin >> n;
+        if (-1 == n)
         {
-            divisors.push_back(i);
+            break;
         }
-    }
-    divisors.push_back(N);
 
-    if (K < (int)divisors.size())
-    {
-        std::cout << divisors[K];
-    }
-    else
-    {
-        std::cout << 0;
+        divisors.clear();
+        divisors.push_back(1);
+        for (int i = 2; i < n; ++i)
+        {
+            if (0 == n % i)
+            {
+                divisors.push_back(i);
+            }
+        }
+
+        int sumDivisor{};
+        for (size_t i = 0; i < divisors.size(); ++i)
+        {
+            sumDivisor += divisors[i];
+        }
+
+        if (sumDivisor == n)
+        {
+            std::cout << n << " = ";
+
+            size_t end = divisors.size() - 1;
+            for (size_t i = 0; i <= end; ++i)
+            {
+                std::cout << divisors[i];
+                if (i != end)
+                {
+                    std::cout << " + ";
+                }
+                else
+                {
+                    std::cout << "\n";
+                }
+            }
+        }
+        else
+        {
+            std::cout << n << " is NOT perfect.\n";
+        }
     }
     return 0;
 }
