a92242bf7b0ed8e7409b10702bfb698bf0fc21c8
HWNOTE <ekdrn1@naver.com>
2024-04-29 12:50:11 +0900

    백준 27323 (직사각형)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -12,7 +12,6 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 #include <limits>
 
-#include <vector>
 
 int main()
 {
@@ -23,30 +22,10 @@ int main()
 
     using namespace std;
 
-    int N{};
-    std::cin >> N;
+    int A{}, B{};
+    std::cin >> A >> B;
 
-    int loopSize = N;
-    for (int i = 2; i * i <= loopSize; ++i)
-    {
-        //나눠떨어질 경우에는 소인수이다.
-        while (0 == N % i)
-        {
-            N /= i;
-            std::cout << i << '\n';
-        }
-
-        if (1 == N)
-        {
-            break;
-        }
-    }
-
-    //1이 아닌 값이남아있을 경우 이것까지 출력
-    if (N != 1)
-    {
-        std::cout << N;
-    }
+    std::cout << A * B;
 
     return 0;
 }
