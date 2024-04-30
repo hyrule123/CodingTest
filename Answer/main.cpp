847d6a43bbbdb41d22d6abdd7b61b05928ec52bd
HWNOTE <ekdrn1@naver.com>
2024-04-30 18:23:52 +0900

    백준 2231 (분해합)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,6 +14,32 @@ std::ios_base::sync_with_stdio(false)
 
 #include <vector>
 
+inline int PowInt(const int _base, int _exponent)
+{
+    int ret = 1;
+
+    while (0 < _exponent--)
+    {
+        ret *= _base;
+    }
+
+    return ret;
+}
+
+inline int GetDigitCount(int _num)
+{
+    int digitCount{};
+
+    while (0 != _num)
+    {
+        _num /= 10;
+        ++digitCount;
+    }
+
+    return digitCount;
+}
+
+
 int main()
 {
     USING_IOSTREAM;
@@ -21,35 +47,45 @@ int main()
     READ_INPUT;
     WRITE_OUTPUT;
 
-    std::int64_t N{}, M{};
-    std::cin >> N >> M;
+    int num{};
+    std::cin >> num;
 
-    std::vector<std::int64_t> cards{};
-    cards.resize(N);
-    for (size_t i = 0; i < cards.size(); ++i)
+    int digitSize = GetDigitCount(num);
+
+    //각 자리당 최대 9까지 뺼 수 있으므로
+    //최소값(시작값)은 9 * 자리갯수를 뺀 값이 될수밖에 없음.
+    int start = num - 9 * digitSize;
+    //예외처리
+    if (start < 0)
     {
-        std::cin >> cards[i];
+        start = 0;
     }
 
-    std::int64_t nearest{ std::numeric_limits<std::int64_t>::max() };
-
-    const size_t iEnd = cards.size() - (size_t)2;
-    const size_t jEnd = cards.size() - (size_t)1;
-    for (size_t i = 0; i < iEnd; ++i)
+    //시작값부터 자신의 값 까지
+    for (start; start < num; ++start)
     {
-        for (size_t j = i + 1; j < jEnd; ++j)
+        int sum = start;
+        int exp = PowInt(10, GetDigitCount(start) - 1);
+
+        //자릿수별로 더해준다.
+        int temp = start;
+        while (0 != temp)
         {
-            for (size_t k = j + 1; k < cards.size(); ++k)
-            {
-                std::int64_t dist = M - (cards[i] + cards[j] + cards[k]);
-                if (0 <= dist && dist < nearest)
-                {
-                    nearest = dist;
-                }
-            }
+            int div = temp / exp;
+            temp -= div * exp;
+            sum += div;
+            exp /= 10;
+        }
+
+        //동일한 값을 찾았을 경우 출력 후 return
+        if (sum == num)
+        {
+            std::cout << start;
+            return 0;
         }
     }
-    std::cout << M - nearest;
 
+    //못 찾았을경우 여기 들어오게됨
+    std::cout << 0;
     return 0;
 }
