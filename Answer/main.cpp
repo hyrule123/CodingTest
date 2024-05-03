eaef6095e3e33133ebb41897afac90e3e3f3b069
HWNOTE <ekdrn1@naver.com>
2024-05-03 23:01:42 +0900

    Min Heap Sort 구현(내일 할것: Templatized Heap Sort 구현하기, Min Heap과 Max Heap은 다른 모든게 똑같고 비교를 어떻게 하는지만 다름.

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -12,6 +12,52 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 #include <limits>
 
+#include <vector>
+#include <type_traits>
+
+
+void MinHeapify(std::vector<int>& _arr, size_t _size, size_t _idx) 
+{
+    size_t smallestIdx = _idx;
+    size_t left = 2 * _idx + 1;
+    size_t right = 2 * _idx + 2;
+
+    
+    if (left < _size && _arr[left] >= _arr[smallestIdx]) {
+        smallestIdx = left;
+    }
+
+    
+    if (right < _size && _arr[right] >= _arr[smallestIdx]) {
+        smallestIdx = right;
+    }
+
+    
+    if (smallestIdx != _idx) {
+        std::swap(_arr[_idx], _arr[smallestIdx]);
+        MinHeapify(_arr, _size, smallestIdx);
+    }
+}
+
+void BuildMinHeap(std::vector<int>& _arr)
+{
+    //전체 사이즈의 반으로 나누면 부모 노드일 가능성이 있는 애들만 남음.
+    for (size_t i = _arr.size() / 2; i != -1; --i)
+    {
+        MinHeapify(_arr, _arr.size(), i);
+    }
+}
+
+void MinHeapSort(std::vector<int>& _arr)
+{
+    BuildMinHeap(_arr);
+
+    for (size_t i = _arr.size() - 1; i > 0; --i)
+    {
+        std::swap(_arr[0], _arr[i]);
+        MinHeapify(_arr, i, 0);
+    }
+}
 
 int main()
 {
@@ -20,6 +66,9 @@ int main()
     READ_INPUT;
     WRITE_OUTPUT;
 
+    std::vector<int> test = { 1, 1, 2, 2, 3, 2, 4, 5 };
+
+    MinHeapSort(test);
 
     return 0;
 }
