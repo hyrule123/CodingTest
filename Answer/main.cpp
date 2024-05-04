6ebb2042f41024027cef01908c2de876b58d38ef
HWNOTE <ekdrn1@naver.com>
2024-05-04 22:39:44 +0900

    백준 11651 (좌표 정렬하기 2)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -17,33 +17,28 @@ std::ios_base::sync_with_stdio(false)
 #include <type_traits>
 #include <random>
 template <typename CompareStruct, typename T = CompareStruct::Type>
-concept is_comparable = requires(T _a, T _b)
-{
+concept is_comparable = requires(T _a, T _b) {
     { CompareStruct::Compare(_a, _b) } -> std::same_as<bool>;
 };
 //리턴값: 정렬된 pivot의 위치
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
-{
+inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx) {
+
     size_t start = _startIdx;
     size_t end = _endIdx - 1;
     T& pivot = _vec[_endIdx];
 
-    while (true)
-    {
+    while (true) {
         //quick sort 진행
         //<=인 이유: 가장 뒤쪽에 있는 pivot 인덱스와도 비교를 해야됨
-        while (start <= end && CompareStruct::Compare(_vec[start], pivot))
-        {
+        while (start <= end && CompareStruct::Compare(_vec[start], pivot)) {
             ++start;
         }
-        while (start < end && false == CompareStruct::Compare(_vec[end], pivot))
-        {
+        while (start < end && false == CompareStruct::Compare(_vec[end], pivot)) {
             --end;
         }
         //left, right 포인터가 교차할 때 start 위치가 pivot값이 들어갈 자리이다.
-        if (start >= end)
-        {
+        if (start >= end) {
             std::swap(_vec[start], pivot);
             break;
         }
@@ -52,14 +47,12 @@ inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size
         std::swap(_vec[start], _vec[end]);
     }
 
-
     return start;
 }
 
 //범위 안의 랜덤한 인덱스를 인덱스의 끝으로 보냄(Pivot 역할)
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-inline size_t Partition_Random(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
-{
+inline size_t Partition_Random(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx) {
     //나오는 범위: _start ~ _end
     size_t randomIdx = _startIdx + (rand() % (_endIdx - _startIdx));
 
@@ -69,10 +62,8 @@ inline size_t Partition_Random(std::vector<T>& _vec, const size_t _startIdx, con
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void QuickSort(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
-{
-    if (_startIdx < _endIdx)
-    {
+void QuickSort(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx) {
+    if (_startIdx < _endIdx) {
         //partition 호출, 정렬된 피벗의 인덱스를 받아온다.
         size_t pivotIdx = Partition_Random<CompareStruct, T>(_vec, _startIdx, _endIdx);
 
@@ -82,28 +73,26 @@ void QuickSort(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void QuickSort(std::vector<T>& _vec)
-{
+void QuickSort(std::vector<T>& _vec) {
     QuickSort<CompareStruct, T>(_vec, 0, _vec.size() - 1);
 }
 
-int main()
-{
+int main() {
     USING_IOSTREAM;
 
     READ_INPUT;
     WRITE_OUTPUT;
 
     struct Coord { int x, y; };
-    struct CoordAscending
-    {
+    struct CoordAscending {
         using Type = Coord;
         static inline bool Compare(const Type& _a, const Type& _b) {
-            if (_a.x == _b.x) {
-                return _a.y <= _b.y;
+            if (_a.y == _b.y)
+            {
+                return _a.x <= _b.x;
             }
 
-            return _a.x < _b.x;
+            return _a.y < _b.y;
         }
     };
 
