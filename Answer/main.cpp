b571b2f8aa480ec67e0e040734b02f28c66d064d
HWNOTE <ekdrn1@naver.com>
2024-05-04 16:38:52 +0900

    백준 11650 (좌표 정렬하기) - 임시 저장

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -15,6 +15,7 @@ std::ios_base::sync_with_stdio(false)
 //Templatized QuickSort, QuickSelect
 #include <vector>
 #include <type_traits>
+#include <random>
 template <typename CompareStruct, typename T = CompareStruct::Type>
 concept is_comparable = requires(T _a, T _b)
 {
@@ -24,23 +25,9 @@ concept is_comparable = requires(T _a, T _b)
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
 {
-    //예외 처리
-    if (_startIdx > _endIdx)
-    {
-        return -1;
-    }
-
-    const size_t size = _endIdx - _startIdx + 1;
-    if (size == 1)
-    {
-        return _startIdx;
-    }
-    //////////////////////////////////
-
     size_t start = _startIdx;
     size_t end = _endIdx - 1;
-    std::swap(_vec[end], _vec[_endIdx]);
-    T pivot = _vec[_endIdx];
+    T& pivot = _vec[_endIdx];
 
     while (true)
     {
@@ -54,24 +41,38 @@ inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size
         {
             --end;
         }
-        //left, right 포인터가 교차하면 정렬 완료
+        //left, right 포인터가 교차할 때 start 위치가 pivot값이 들어갈 자리이다.
         if (start >= end)
         {
-            std::swap(_vec[start], _vec[_endIdx]);
+            std::swap(_vec[start], pivot);
             break;
         }
 
+        //서로 교차하지 않았을 경우 start와 end에 있는 값을 교환해준다.
         std::swap(_vec[start], _vec[end]);
     }
 
+
     return start;
+}
+
+//범위 안의 랜덤한 인덱스를 인덱스의 끝으로 보냄(Pivot 역할)
+template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
+inline size_t Partition_Random(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
+{
+    //나오는 범위: _start ~ _end - 1
+    size_t randomIdx = _startIdx + (rand() % (_endIdx - _startIdx));
+
+    std::swap(_vec[_endIdx], _vec[randomIdx]);
+
+    return Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 void QuickSelectRecursive(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx, size_t _idxToFind)
 {
     //partition 호출, 정렬된 피벗 인덱스를 받아온다.
-    size_t pivotIdx = Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
+    size_t pivotIdx = Partition_Random<CompareStruct, T>(_vec, _startIdx, _endIdx);
 
     if (-1 == pivotIdx)
     {
@@ -98,30 +99,22 @@ void QuickSelect(std::vector<T>& _vec, size_t _idxToFind)
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void QuickSortRecursive(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
+void QuickSort(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
 {
-    //partition 호출, 정렬된 피벗의 인덱스를 받아온다.
-    size_t pivotIdx = Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
+    if (_startIdx < _endIdx)
+    {
+        //partition 호출, 정렬된 피벗의 인덱스를 받아온다.
+        size_t pivotIdx = Partition_Random<CompareStruct, T>(_vec, _startIdx, _endIdx);
 
-    if (-1 == pivotIdx)
-    {
-        return;
-    }
-
-    if (0 < pivotIdx)
-    {
-        QuickSortRecursive<CompareStruct, T>(_vec, _startIdx, pivotIdx - 1);
-    }
-    if (pivotIdx < _vec.size())
-    {
-        QuickSortRecursive<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx);
+        QuickSort<CompareStruct, T>(_vec, _startIdx, pivotIdx);
+        QuickSort<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx);
     }
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 void QuickSort(std::vector<T>& _vec)
 {
-    QuickSortRecursive<CompareStruct, T>(_vec, 0, _vec.size() - 1);
+    QuickSort<CompareStruct, T>(_vec, 0, _vec.size() - 1);
 }
 
 inline bool IsDecimalNumber(char _c)
@@ -136,31 +129,34 @@ int main()
     READ_INPUT;
     WRITE_OUTPUT;
 
-    std::vector<char> digits{};
-    digits.reserve(11);
-    while (true) {
-        char c{};
-        std::cin >> c;
-        if (IsDecimalNumber(c)) {
-            digits.push_back(c);
-        }
-        else {
-            break;
-        }
-    }
+    struct Coord { int x, y; };
+    struct CoordAscending
+    {
+        using Type = Coord;
+        static inline bool Compare(const Type& _a, const Type& _b) {
+            if (_a.x == _b.x) {
+                return _a.y <= _b.y;
+            }
 
-    struct CharAscending {
-        using Type = char;
-        inline static bool Compare(char _a, char _b) {
-            return _a >= _b;
+            return _a.x < _b.x;
         }
     };
 
-    QuickSort<CharAscending>(digits);
+    std::size_t N{};
+    std::cin >> N;
 
-    for (size_t i = 0; i < digits.size(); ++i) {
-        std::cout << digits[i];
+    std::vector<Coord> arrCoord{};
+    arrCoord.resize(N);
+
+    for (size_t i = 0; i < N; ++i) {
+        std::cin >> arrCoord[i].x >> arrCoord[i].y;
     }
 
+    QuickSort<CoordAscending>(arrCoord);
+
+    for (size_t i = 0; i < N; ++i) {
+        std::cout << arrCoord[i].x << ' ' << arrCoord[i].y << '\n';
+    }
+    
     return 0;
 }
