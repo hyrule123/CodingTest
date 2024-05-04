2af6c748d4250e8ff31d62b9941a27973488c4b8
HWNOTE <ekdrn1@naver.com>
2024-05-05 00:35:40 +0900

    백준 10814 (나이순 정렬)

--- Answer/Sort/MergeSort.h
+++ Answer/Sort/MergeSort.h
@@ -1,20 +1,15 @@
-#pragma once
-
 //Templatized MergeSort//
 #include <vector>
 #include <type_traits>
 template <typename CompareStruct, typename T = CompareStruct::Type>
-concept is_comparable = requires(T _a, T _b)
-{
+concept is_comparable = requires(T _a, T _b) {
     { CompareStruct::Compare(_a, _b) } -> std::same_as<bool>;
 };
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, const size_t _start, const size_t _end)
-{
+void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, const size_t _start, const size_t _end) {
     const size_t size = _end - _start + 1;
-    if (size <= 1)
-    {
+    if (size <= 1) {
         return;
     }
 
@@ -29,17 +24,14 @@ void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, con
     size_t writeIter = lIter;
 
     //Merge
-    while (lIter <= mid && rIter <= _end)
-    {
+    while (lIter <= mid && rIter <= _end) {
         //번갈아가면서 비교하고, 조건에 부합해서 값을 넣었을 경우 인덱스를 한단계씩 올려준다.
-        if (CompareStruct::Compare(_orig[lIter], _orig[rIter]))
-        {
+        if (CompareStruct::Compare(_orig[lIter], _orig[rIter])) {
             _temp[writeIter] = _orig[lIter];
             ++lIter;
             ++writeIter;
         }
-        else
-        {
+        else {
             _temp[writeIter] = _orig[rIter];
             ++rIter;
             ++writeIter;
@@ -47,13 +39,11 @@ void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, con
     }
 
     //분할 갯수가 불균형하여 한 쪽이 남아있을 수도 있음 -> 남아있는 값들을 복사해준다.
-    for (; lIter <= mid; ++lIter)
-    {
+    for (; lIter <= mid; ++lIter) {
         _temp[writeIter] = _orig[lIter];
         ++writeIter;
     }
-    for (; rIter <= _end; ++rIter)
-    {
+    for (; rIter <= _end; ++rIter) {
         _temp[writeIter] = _orig[rIter];
         ++writeIter;
     }
@@ -63,8 +53,7 @@ void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, con
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void MergeSort(std::vector<T>& _vec)
-{
+void MergeSort(std::vector<T>& _vec) {
     std::vector<T> temp{};
     temp.resize(_vec.size());
 
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,86 +13,76 @@ std::ios_base::sync_with_stdio(false)
 #include <limits>
 
 #include <string>
+#include <array>
 
-//Templatized HeapSort
+//Templatized MergeSort//
 #include <vector>
 #include <type_traits>
 template <typename CompareStruct, typename T = CompareStruct::Type>
-concept is_comparable = requires(T _a, T _b)
-{
+concept is_comparable = requires(T _a, T _b) {
     { CompareStruct::Compare(_a, _b) } -> std::same_as<bool>;
 };
 
-//CompareStruct에서 정의된 비교 함수에 따라 최대힙/최소힙으로 정렬
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void Heapify(std::vector<T>& _arr, const size_t _arrSize, const size_t _idx)
-{
-    //제일 크거나 제일 작은 값을 가지고 있는 변수
-    //초기값 = 부모 노드
-    size_t extremeIdx = _idx;
-    //왼쪽, 오른쪽 자식 노드(* 2 + 1 or 2)
-    size_t left = 2 * _idx + 1;
-    size_t right = 2 * _idx + 2;
-
-    //왼쪽 자식 노드와 부모 노드를 비교, 자식 노드가 비교에서 true가 반환되면 인덱스를 변경
-    if (left < _arrSize && CompareStruct::Compare(_arr[extremeIdx], _arr[left]))
-    {
-        extremeIdx = left;
+void DivideAndConquerRecursive(std::vector<T>& _orig, std::vector<T>& _temp, const size_t _start, const size_t _end) {
+    const size_t size = _end - _start + 1;
+    if (size <= 1) {
+        return;
     }
 
-    //오른쪽 자식 노드와 부모 노드를 비교, 부모 노드(혹은 왼쪽 노드와 비교 후 더 크다고 판정될 경우 인덱스를 변경)
-    if (right < _arrSize && CompareStruct::Compare(_arr[extremeIdx], _arr[right]))
-    {
-        extremeIdx = right;
+    const size_t mid = (_start + _end) / 2;
+
+    //Divide, Conquer
+    DivideAndConquerRecursive<CompareStruct, T>(_orig, _temp, _start, mid);
+    DivideAndConquerRecursive<CompareStruct, T>(_orig, _temp, mid + 1, _end);
+
+    size_t lIter = _start;
+    size_t rIter = mid + 1;
+    size_t writeIter = lIter;
+
+    //Merge
+    while (lIter <= mid && rIter <= _end) {
+        //번갈아가면서 비교하고, 조건에 부합해서 값을 넣었을 경우 인덱스를 한단계씩 올려준다.
+        if (CompareStruct::Compare(_orig[lIter], _orig[rIter])) {
+            _temp[writeIter] = _orig[lIter];
+            ++lIter;
+            ++writeIter;
+        }
+        else {
+            _temp[writeIter] = _orig[rIter];
+            ++rIter;
+            ++writeIter;
+        }
     }
 
-    if (extremeIdx != _idx)
-    {
-        //극단값을 이진트리 규칙에 맞는 자리로 옮겨준다.
-        std::swap(_arr[_idx], _arr[extremeIdx]);
-
-        //값이 변경된 노드에 대해 이진트리 규칙 검사(재귀)
-        Heapify<CompareStruct, T>(_arr, _arrSize, extremeIdx);
+    //분할 갯수가 불균형하여 한 쪽이 남아있을 수도 있음 -> 남아있는 값들을 복사해준다.
+    for (; lIter <= mid; ++lIter) {
+        _temp[writeIter] = _orig[lIter];
+        ++writeIter;
     }
+    for (; rIter <= _end; ++rIter) {
+        _temp[writeIter] = _orig[rIter];
+        ++writeIter;
+    }
+
+    //_temp에 정렬되어있는 데이터를 가져온다.
+    std::copy(_temp.begin() + _start, _temp.begin() + _end + 1, _orig.begin() + _start);
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void BuildHeap(std::vector<T>& _arr)
-{
-    //전체 사이즈의 반으로 나누면 부모 노드일 가능성이 있는 애들만 남음.
-    //부모 노드일 가능성이 있는 노드의 뒤쪽 부분부터 하나씩 올라온다.
-    //루트노드는 모든 자식노드의 영향을 받음
-    const size_t arrSize = _arr.size();
-    for (size_t i = _arr.size() / 2; i != -1; --i)
-    {
-        Heapify<CompareStruct, T>(_arr, arrSize, i);
-    }
+void MergeSort(std::vector<T>& _vec) {
+    std::vector<T> temp{};
+    temp.resize(_vec.size());
+
+    DivideAndConquerRecursive<CompareStruct, T>(_vec, temp, 0, temp.size() - 1);
 }
 
-template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void HeapSort(std::vector<T>& _arr)
-{
-    //이진힙 규칙에 맞게 변경
-    BuildHeap<CompareStruct, T>(_arr);
-
-    for (size_t i = _arr.size() - 1; i > 0; --i)
-    {
-        //이진힙 규칙에 맞게 된다면, 가장 큰/작은 값이 0번 루트노드에 들어왔을 것이다.
-        //이 값을 배열의 끝부분에 하나씩 채워준다.
-        std::swap(_arr[0], _arr[i]);
-
-        //루트 노드에 온 수는 이진힙 규칙에 맞지 않으므로, 다시 규칙에 맞게 트리를 재배열한다.
-        //재배열하면 루트 노드에는 다음으로 큰 수가 들어오게 된다. 
-        Heapify<CompareStruct, T>(_arr, i, 0);
-    }
-}
-
-//캐시적중률 높이기위한 고정배열
-struct CustomStr {
-    static constexpr const size_t MaxLen = 51;
-    char S[MaxLen];
-    size_t StrLen;
+//정렬의 대상이 되는 구조체는 string_view만 들고있어준다.
+struct  MemberInfo {
+    std::int64_t Age;
+    std::string_view Name;
 };
+
 
 int main() {
     USING_IOSTREAM;
@@ -100,48 +90,36 @@ int main() {
     READ_INPUT;
     WRITE_OUTPUT;
 
-    struct StringDictionaryAscending {
-        using Type = CustomStr;
+    struct MemberInfoAscending {
+        using Type = MemberInfo;
         static inline bool Compare(const Type& _a, const Type& _b) {
-            if (_a.StrLen == _b.StrLen) {
-                for (size_t i = 0; i < _a.StrLen; ++i) {
-                    if (_a.S[i] == _b.S[i]) {
-                        continue;
-                    }
-
-                    return _a.S[i] < _b.S[i];
-                }
-            }
-
-            return _a.StrLen < _b.StrLen;
+            return _a.Age <= _b.Age;
         }
     };
 
     std::size_t N{};
     std::cin >> N;
 
-    std::vector<CustomStr> arrString{};
-    arrString.resize(N);
+    std::vector<MemberInfo> members{};
+    members.resize(N);
+
+    //실제 이름은 별도의 배열에 보관(비교 대상이 아니므로 따로 보관해도 상관 없음)
+    std::vector<std::array<char, 128>> arrNames{};
+    arrNames.resize(N);
     
-    {
-        CustomStr str{};
-        std::cin.getline(str.S, CustomStr::MaxLen);
-    }
-    
-    for (size_t i = 0; i < N; ++i) {
-        std::cin.getline(arrString[i].S, arrString[i].MaxLen);
-        arrString[i].StrLen = std::strlen(arrString[i].S);
+    for (size_t i = 0; i < members.size(); ++i) {
+        std::cin >> members[i].Age;
+        std::cin.get();
+        std::cin.getline(arrNames[i].data(), arrNames[i].size());
+        members[i].Name = arrNames[i].data();
     }
 
-    HeapSort<StringDictionaryAscending>(arrString);
+    //참고: Merge sort는 stable sort이므로 정렬 전의 상대 순서가 유지된다는 특성이 있다.
+    //* Heap sort는 stable sort가 아니기에 유지되지 않음.
+    MergeSort<MemberInfoAscending>(members);
 
-    if (false == arrString.empty()) {
-        std::cout << arrString[0].S << '\n';
-        for (size_t i = 1; i < N; ++i) {
-            if (0 != std::strcmp(arrString[i - 1].S, arrString[i].S)) {
-                std::cout << arrString[i].S << '\n';
-            }
-        }
+    for (size_t i = 0; i < members.size(); ++i) {
+        std::cout << members[i].Age << ' ' << members[i].Name << '\n';
     }
 
     return 0;
