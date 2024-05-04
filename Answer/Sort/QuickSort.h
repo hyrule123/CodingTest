af306d5ac3d3ea1ca7e8800db80a5b0486e37593
HWNOTE <ekdrn1@naver.com>
2024-05-04 16:52:02 +0900

    QuickSort Random Pivot 사용하도록 코드 수정

--- Answer/Sort/QuickSort.h
+++ Answer/Sort/QuickSort.h
@@ -1,6 +1,7 @@
 //Templatized QuickSort, QuickSelect
 #include <vector>
 #include <type_traits>
+#include <random>
 template <typename CompareStruct, typename T = CompareStruct::Type>
 concept is_comparable = requires(T _a, T _b)
 {
@@ -10,23 +11,9 @@ concept is_comparable = requires(T _a, T _b)
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
 {
-    //예외 처리
-    if (_startIdx > _endIdx)
-    {
-        return -1;
-    }
-
-    const size_t size = _endIdx - _startIdx + 1;
-    if (size == 1)
-    {
-        return _startIdx;
-    }
-    //////////////////////////////////
-
     size_t start = _startIdx;
     size_t end = _endIdx - 1;
-    std::swap(_vec[end], _vec[_endIdx]);
-    T pivot = _vec[_endIdx];
+    T& pivot = _vec[_endIdx];
 
     while (true)
     {
@@ -40,40 +27,53 @@ inline size_t Partition(std::vector<T>& _vec, const size_t _startIdx, const size
         {
             --end;
         }
-        //left, right 포인터가 교차하면 정렬 완료
+        //left, right 포인터가 교차할 때 start 위치가 pivot값이 들어갈 자리이다.
         if (start >= end)
         {
-            std::swap(_vec[start], _vec[_endIdx]);
+            std::swap(_vec[start], pivot);
             break;
         }
 
+        //서로 교차하지 않았을 경우 start와 end에 있는 값을 교환해준다.
         std::swap(_vec[start], _vec[end]);
     }
 
+
     return start;
+}
+
+//범위 안의 랜덤한 인덱스를 인덱스의 끝으로 보냄(Pivot 역할)
+template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
+inline size_t Partition_Random(std::vector<T>& _vec, const size_t _startIdx, const size_t _endIdx)
+{
+    //나오는 범위: _start ~ _end
+    size_t randomIdx = _startIdx + (rand() % (_endIdx - _startIdx));
+
+    //나온 랜덤 인덱스 번호의 값을 끝으로 보낸다(Pivot으로 설정)
+    std::swap(_vec[_endIdx], _vec[randomIdx]);
+
+    return Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 void QuickSelectRecursive(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx, size_t _idxToFind)
 {
-    //partition 호출, 정렬된 피벗 인덱스를 받아온다.
-    size_t pivotIdx = Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
-
-    if (-1 == pivotIdx)
+    if (_startIdx < _endIdx)
     {
-        return;
-    }
+        //partition 호출, 정렬된 피벗 인덱스를 받아온다.
+        size_t pivotIdx = Partition_Random<CompareStruct, T>(_vec, _startIdx, _endIdx);
 
-    //찾을 인덱스가 피벗 좌측(재귀)
-    if (_idxToFind < pivotIdx)
-    {
-        QuickSelectRecursive<CompareStruct, T>(_vec, _startIdx, pivotIdx - 1, _idxToFind);
-    }
+        //찾을 인덱스가 피벗 좌측(재귀)
+        if (_idxToFind < pivotIdx)
+        {
+            QuickSelectRecursive<CompareStruct, T>(_vec, _startIdx, pivotIdx, _idxToFind);
+        }
 
-    //찾을 인덱스가 피벗 우측(재귀)
-    else if (pivotIdx < _idxToFind)
-    {
-        QuickSelectRecursive<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx, _idxToFind);
+        //찾을 인덱스가 피벗 우측(재귀)
+        else if (pivotIdx < _idxToFind)
+        {
+            QuickSelectRecursive<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx, _idxToFind);
+        }
     }
 }
 
@@ -84,28 +84,20 @@ void QuickSelect(std::vector<T>& _vec, size_t _idxToFind)
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
-void QuickSortRecursive(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
+void QuickSort(std::vector<T>& _vec, size_t _startIdx, size_t _endIdx)
 {
-    //partition 호출, 정렬된 피벗의 인덱스를 받아온다.
-    size_t pivotIdx = Partition<CompareStruct, T>(_vec, _startIdx, _endIdx);
+    if (_startIdx < _endIdx)
+    {
+        //partition 호출, 정렬된 피벗의 인덱스를 받아온다.
+        size_t pivotIdx = Partition_Random<CompareStruct, T>(_vec, _startIdx, _endIdx);
 
-    if (-1 == pivotIdx)
-    {
-        return;
-    }
-
-    if (0 < pivotIdx)
-    {
-        QuickSortRecursive<CompareStruct, T>(_vec, _startIdx, pivotIdx - 1);
-    }
-    if (pivotIdx < _vec.size())
-    {
-        QuickSortRecursive<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx);
+        QuickSort<CompareStruct, T>(_vec, _startIdx, pivotIdx);
+        QuickSort<CompareStruct, T>(_vec, pivotIdx + 1, _endIdx);
     }
 }
 
 template <typename CompareStruct, typename T = CompareStruct::Type> requires is_comparable<CompareStruct>
 void QuickSort(std::vector<T>& _vec)
 {
-    QuickSortRecursive<CompareStruct, T>(_vec, 0, _vec.size() - 1);
+    QuickSort<CompareStruct, T>(_vec, 0, _vec.size() - 1);
 }
