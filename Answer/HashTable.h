27a54691e94a2ed32c649b1e1f19c6170ba2ca6e
HWNOTE <ekdrn1@naver.com>
2024-05-07 15:44:17 +0900

    Templattized Hash Table 구현

---
+++ Answer/HashTable.h
@@ -0,0 +1,98 @@
+#include <string>
+#include <vector>
+#include <list>
+
+inline bool IsPrime(size_t _num) {
+	if (_num <= 1) { return false; }
+	if (_num <= 3) { return true; }
+	if (_num % 2 == 0 || _num % 3 == 0) { return false; }
+
+	for (size_t i = 5; i * i <= _num; i += 6) {
+		if (_num % i == 0 || _num % (i + 2) == 0) return false;
+	}
+
+	return true;
+}
+
+template <typename K, typename Hasher>
+concept is_hasher_valid = requires (K _key) {
+	{ Hasher::Hash(_key) } -> std::same_as<size_t>;
+};
+
+template <typename K, typename V, typename Hasher> requires is_hasher_valid<K, Hasher>
+class HashTable
+{
+public:
+	struct Pair {
+		K Key;
+		V Value;
+	};
+
+	HashTable(size_t _bucketCapacity = 20011) : m_container{} {
+		while (false == IsPrime(_bucketCapacity)) {
+			++_bucketCapacity;
+		}
+		m_container.resize(_bucketCapacity); 
+	}
+	~HashTable() {}
+
+	const V* Insert(Pair&& _pair) {
+		size_t hash = Hasher::Hash(_pair.Key) % m_container.size();
+		
+		std::list<Pair>& bucket = m_container[hash];
+
+		//중복 검사 및 chaining
+		auto iterEnd = bucket.end();
+		for (auto iter = bucket.begin(); iter != iterEnd; ++iter) {
+			if (iter->Key == _pair.Key) {
+				return nullptr;
+			}
+		}
+		
+		bucket.push_back(std::forward<Pair>(_pair));
+
+		return &(bucket.back().Value);
+	}
+
+	const V* Find(const K& _key) const {
+		size_t hash = Hasher::Hash(_key) % m_container.size();
+
+		const std::list<Pair>& bucket = m_container[hash];
+
+		auto iterEnd = bucket.cend();
+		for (auto iter = bucket.cbegin(); iter != iterEnd; ++iter) {
+			if (_key == iter->Key) {
+				return &(iter->Value);
+			}
+		}
+
+		return nullptr;
+	}
+
+	V& operator[](const K& _key) {
+		size_t hash = Hasher::Hash(_key) % m_container.size();
+
+		std::list<Pair>& bucket = m_container[hash];
+
+		//순회돌면서 검색
+		auto iterEnd = bucket.end();
+		for (auto iter = bucket.begin(); iter != iterEnd; ++iter) {
+			if (_key == iter->Key) {
+				return iter->Value;
+			}
+		}
+
+		//못찾았다면 새로 하나 만들어서 반환
+		bucket.push_back(Pair{ _key, {} });
+		return bucket.back().Value;
+	}
+
+	const std::vector<std::list<Pair>>& GetContainer() const { 
+		return m_container; 
+	}
+
+private:
+	std::vector<std::list<Pair>> m_container;
+};
+
+
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -12,36 +12,69 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 #include <limits>
 
-//unordered_set 사용 버전
-#include <unordered_set>
+#include <array>
+#include <string>
+
+
+struct StringHasher {
+    inline static size_t Hash(const std::string_view _str)
+    {
+        constexpr const size_t p = 53;//31;
+        constexpr const size_t mod = 1'000'000'007;
+        size_t result = 0;
+
+        if (_str.empty()) { return result; }
+
+        //라빈-카프 문자열 해싱, 호너의 법칙
+        size_t i = _str.size() - 1;
+        result = static_cast<size_t>(_str[i]);
+        for (; i > 0; --i) {
+            result = (result * p + _str[i]) % mod;
+        }
+        result = (result + _str[0]) % mod;
+
+        return result;
+    }
+};
+
+#include "HashTable.h"
 int main() {
     USING_IOSTREAM;
 
     READ_INPUT;
     WRITE_OUTPUT;
 
-    size_t N{}, M{};
-    std::cin >> N >> M;
-    std::unordered_set<std::string> stringSet{};
+    size_t N{};
+    std::cin >> N;
+    
+    HashTable<std::string, bool, StringHasher> attendanceLog(N);
+    using myPair = HashTable<std::string, bool, StringHasher>::Pair;
 
-    std::string input{};
-    input.reserve(501);
+    constexpr const std::string_view enterMsg = "enter";
+
     for (size_t i = 0; i < N; ++i) {
-        input.clear();
-        std::cin >> input;
-        stringSet.insert(input);
+        std::string name{};
+        std::string entered{};
+        std::cin >> name >> entered;
+
+        attendanceLog[name] = (entered == enterMsg);
     }
 
-    size_t existCount{};
-    for (size_t i = 0; i < M; ++i) {
-        input.clear();
-        std::cin >> input;
-        auto iter = stringSet.find(input);
-        if (iter != stringSet.end()) {
-            ++existCount;
+    const auto& cont = attendanceLog.GetContainer();
+
+    for (size_t i = 0; i < cont.size(); ++i) {
+        auto& bucket = cont[i];
+
+        auto iterEnd = bucket.end();
+        for (auto iter = bucket.begin(); iter != iterEnd; ++iter) {
+
+            if (iter->Value) {
+                std::cout << iter->Key << '\n';
+            } 
         }
     }
+    
+    
 
-    std::cout << existCount;
     return 0;
 }
