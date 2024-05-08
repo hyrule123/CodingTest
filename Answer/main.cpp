ce942843fe1676ed49662479e9ca7afebfbdb740
HWNOTE <ekdrn1@naver.com>
2024-05-08 22:10:55 +0900

    백준 1764 (듣보잡) - 쓸모없는 풀이(STL 도배)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -12,67 +12,93 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 #include <limits>
 
-#include <list>
-constexpr const unsigned int g_tableSize = 700'009;
+#include <vector>
+#include <set>
+#include <compare>
+#include <string_view>
+#include <algorithm>
 
-struct CardInfo {
-    int Num;
-    int Count;
+struct Str {
+    constexpr static const size_t s_maxLength = 21;
+    char s[s_maxLength];
+    int length;
+
+    inline bool operator == (const Str& _other) const {
+        if (this->length != _other.length) {
+            return false;
+        }
+
+        for (int i = 0; i < length; ++i) {
+            if (this->s[i] != _other.s[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    //사전순 정렬 Spaceship operator
+    inline std::strong_ordering operator <=> (const Str& _other) const {
+        int size = std::min(length, _other.length);
+        
+        for (int i = 0; i < size; ++i) {
+            if (this->s[i] == _other.s[i]) {
+                continue;
+            }
+
+            return (this->s[i] <=> _other.s[i]);
+        }
+
+        return this->length <=> _other.length;
+    }
 };
 
-std::list<CardInfo> cardsTable[g_tableSize]{};
-inline unsigned int Hash(int _orig) {
-    unsigned int ret{};
-
-    return (unsigned int)_orig % g_tableSize;
-}
-
-//chaining을 list로 한 버전: 메모리 34136, 시간 384ms
+//처음에는 이진탐색트리에 전부 삽입해서 정렬된 상태를 만들고, 한번에 출력하는 건줄 알았는데, 그게 아닌듯...
+//문제는 듣보의 '보'(M으로 들어오는 애들)이 정렬되어 있는지 모른다는것임...
+//결국 전부 받아서 검사해 본 뒤에야 순서를 결정할 수 있기 때문에 '듣'을 unordered_map에 넣고, '보'와 공통된 친구들을 따로 빼놓은 뒤, 정렬을 돌려야 함...
 int main() {
     USING_IOSTREAM;
-
     READ_INPUT;
     WRITE_OUTPUT;
 
-    int N{};
-    std::cin >> N;
+    int N{}, M{};
+    std::cin >> N >> M;
 
+    std::set<Str, std::less<Str>> D{};  
+
+    Str input{};
+    std::cin.getline(input.s, input.s_maxLength);
     for (int i = 0; i < N; ++i) {
-        int cardNum{};
-        std::cin >> cardNum;
-        unsigned int hash = Hash(cardNum);
-        
-        bool found = false;
-        std::list<CardInfo>& bucket = cardsTable[hash];
-        for (auto& iter : bucket) {
-            if (iter.Num == cardNum) {
-                ++(iter.Count);
-                found = true;
-                break;
-            }
-        }
+        input.s[0] = '\0';
+        input.length = 0;
 
-        if (false == found) {
-            bucket.push_back(CardInfo{ cardNum, 1 });
+        std::cin.getline(input.s, input.s_maxLength);
+        input.length = (int)std::strlen(input.s);
+
+        D.insert(input);
+    }
+
+    std::vector<std::string_view> DBJ{};
+    DBJ.reserve(N);
+
+    for (int i = 0; i < M; ++i) {
+        input.s[0] = '\0';
+        input.length = 0;
+
+        std::cin.getline(input.s, input.s_maxLength);
+        input.length = (int)std::strlen(input.s);
+
+        auto iter = D.find(input);
+        if (iter != D.end()) {
+            DBJ.push_back(std::string_view(iter->s, iter->length));
         }
     }
 
-    int M{};
-    std::cin >> M;
-    for (int i = 0; i < M; ++i) {
-        int cardNum{};
-        std::cin >> cardNum;
-        unsigned int hash = Hash(cardNum);
+    std::sort(DBJ.begin(), DBJ.end());
 
-        int count = 0;
-        std::list<CardInfo>& bucket = cardsTable[hash];
-        for (auto& iter : bucket) {
-            if (iter.Num == cardNum) {
-                count = iter.Count;
-                break;
-            }
-        }
-        std::cout << count << ' ';
+    std::cout << DBJ.size() << '\n';
+    for (size_t i = 0; i < DBJ.size(); ++i) {
+        std::cout << DBJ[i] << '\n';
     }
 
     return 0;
