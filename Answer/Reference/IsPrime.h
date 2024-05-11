3f1f8197338664130a065014e05d0fbae2acfb35
HWNOTE <ekdrn1@naver.com>
2024-05-11 20:49:02 +0900

    임시

---
+++ Answer/Reference/IsPrime.h
@@ -0,0 +1,18 @@
+constexpr inline bool IsPrime(size_t _num) {
+    if (_num <= 1) return false; // 1 and numbers less than 1 are not prime
+    if (_num <= 3) return true; // 2 and 3 are prime
+
+    if (_num % 2 == 0 || _num % 3 == 0) return false; // multiples of 2 and 3 are not prime
+
+    // We only need to check divisors up to sqrt(n)
+    //int maxDivisor = sqrt(_num);
+
+    // We start from 5 and increment by 6 (since all primes greater than 3 are of the form 6k ¡¾ 1)
+    for (int i = 5; i * i <= _num; i += 6) {
+        if (_num % i == 0 || _num % (i + 2) == 0) {
+            return false; // If n is divisible by i or i+2, then it's not prime
+        }
+    }
+
+    return true; // If n is not divisible by any divisor up to sqrt(n), then it's prime
+}
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,8 +13,72 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 ////////////////////////////
 
+#include <array>
 #include <string>
+#include <string_view>
+constexpr size_t bucketSize = 1259;
+constexpr size_t GetPrime(size_t _num) {
+    if (_num <= 1) {
+        return 2;
+    }
+    else if (_num <= 3) {
+        return _num;
+    }
 
+    while (++_num) {
+        if (0 == _num % 2 || 0 == _num % 3) {
+            continue;
+        }
+
+        bool found = true;
+        for (size_t i = 5; i * i <= _num; i += 6) {
+            if (0 == _num % i || 0 == _num % (i + 2)) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) { break; }
+    }
+
+    return _num;
+}
+
+inline size_t Hash(const std::string_view _str) {
+    size_t ret = 0;
+
+    //Empty 예외처리 안해도 됨
+    if (1 == _str.size()) {
+        ret = _str[0] % bucketSize;
+    }
+    else {
+        ret = (size_t)_str[0];
+        for (size_t i = 1; i < _str.size(); ++i) {
+            ret = ((ret * 2) % bucketSize + _str[i]) % bucketSize;
+        }
+    }
+    
+    return ret;
+}
+
+//Rabin-Karp fingerprint
+inline size_t RollHash(size_t _orig, size_t _strLen, char _eraseChar, char _endChar) {
+    if (1 == _strLen) {
+        return _endChar % bucketSize;
+    }
+
+    size_t ret = _eraseChar % bucketSize;
+    for (size_t i = 1; i < _strLen; ++i) {
+        ret = ret * 2 % bucketSize;
+    }
+
+            //음수 방지용
+    return (((_orig + bucketSize) - ret) * 2 % bucketSize + _endChar) % bucketSize;
+}
+
+std::array<std::string_view, bucketSize> HashTable{};
+
+//로직: 전체 경우의 수에서 중복되는 경우의 수를 하나씩 빼주기(Rabin-Karp Roll Hash 활용)
 int main() {
     USING_IOSTREAM;
 
@@ -22,29 +86,60 @@ int main() {
     WRITE_OUTPUT;
 
     std::string input{};
-    input.reserve(1000);
+    input.reserve(1001);
     std::cin >> input;
 
     //문자열에서 가능한 전체 케이스(sum 1 + 2 + ... + n)
     //문자열 1000개일 때 500500개 가능
-    size_t cases = (1 + input.size()) * input.size() / 2;
-    constexpr size_t c = (1 + 1000) * 1000 / 2;
+    size_t possibleCases = (1 + input.size()) * input.size() / 2;
+
     
-    //'%' 연산자는 *, /와 동일한 우선순위로 우측으로 진행된다.
-    int test = 100 * 2 % 7;
+    //문자열의 길이 1개 ~ n개 확인
+    for (size_t checkLen = 1, maxLen = input.size() - 1; checkLen <= maxLen; ++checkLen) {
+        //매 순회마다 HashTable 초기화
+        HashTable.fill(std::string_view());
 
-    //'abrac'의 일부 문자열에 대한 테스트
+        //한칸씩 오른쪽으로 이동해가면서 확인
 
-    //'abra'
-    size_t hashabra = ((('a' * 2 % 7 + 'b') * 2 % 7 + 'r') * 2 % 7) + 'a';
+        //0번째 인덱스 처리
+        std::string_view check(input.data(), checkLen); //substr
+        size_t hash = Hash(check);   //prevhash
+        HashTable[hash] = check;
+        for (size_t i = 1, end = input.size() - checkLen; i <= end; ++i) {
+            char toErase = check[0];
+            check = std::string_view(input.data() + i, checkLen);
+            char toAdd = check.back();
 
-    //roll hash
-    size_t hashbrac = (hashabra - ((('a' * 2 % 7) * 2 % 7) * 2 % 7)) * 2 % 7 + 'c';
+            //RollHash를 통해 해쉬 연산량 최대한 줄여준다
+            hash = RollHash(hash, checkLen, toErase, toAdd);
 
-    //'brac'
-    size_t hashbrac2 = ((('b' * 2 % 7 + 'r') * 2 % 7 + 'a') * 2 % 7) + 'c';
+            //해쉬 충돌 체크(linear probing)
+            bool found = false;
+            while (false == HashTable[hash].empty()) {
 
-    //hashbrac == hashbrac2
+                //중복된 값을 넣었을 경우에는 전체 경우의 수에서 빼준다.
+                if (HashTable[hash] == check) {
+                    --possibleCases;
+                    found = true;
+                    break;
+                }
 
+                
+                ++hash;
+                if (hash >= bucketSize) {
+                    hash = 0;
+                }
+            }
+
+            if (found) {
+                continue;
+            }
+            
+            //처음 발견한 해시값일경우 테이블에 추가
+            HashTable[hash] = check;
+        }
+    }
+
+    std::cout << possibleCases;
     return 0;
 }
