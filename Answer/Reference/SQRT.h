c33c0785871b525fe46de1479934080d31069311
HWNOTE <ekdrn1@naver.com>
2024-05-14 12:50:52 +0900

    바빌로니안법 알고리즘을 사용한 SQRT 참고용 코드 추가

---
+++ Answer/Reference/SQRT.h
@@ -0,0 +1,27 @@
+//babylonian algorithm
+double SQRT(double _num) {
+    if (_num <= 1) { return _num; }
+
+    double a = _num / 2.0;
+    constexpr double precision = 0.000001;
+    while (a * a > _num + precision) {
+        a = (a + _num / a) / 2.0;
+    }
+
+    return a;
+}
+
+//최적화 버전(from geeksforgeeks)
+float squareRoot(float n)
+{
+    /*We are using n itself as initial approximation
+      This can definitely be improved */
+    float x = n;
+    float y = 1;
+    constexpr float e = 0.000001; /* e decides the accuracy level*/
+    while (x - y > e) {
+        x = (x + y) / 2;
+        y = n / x;
+    }
+    return x;
+}
