e7899e8f3cecdcef6c79fdcbfacbebc473b2682c
HWNOTE <ekdrn1@naver.com>
2024-05-16 10:43:55 +0900

    28279 (덱 2) - Double Linked List 활용, 백준에서는 소멸자부분을 틀렸다고 채점함, 확인을 위해 테스트케이스 생성 코드가 추가되어 있음

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,29 +13,75 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 ////////////////////////////
 
-#include <vector>
-struct CircleQueue {
-    CircleQueue(size_t _capacity) : front(), end(), capacity(_capacity) { 
-        cont.resize(_capacity + 1); 
-    }
+struct Node {
+    Node* prev = nullptr;
+    Node* next = nullptr;
+    int data = 0;
+};
 
-    inline size_t NextCursor(size_t _cursor) { if ((++_cursor) >= cont.size()) { _cursor = 0; } return _cursor; }
-    inline bool Empty() { return (front == end); }
-    inline bool Full() { return (front == NextCursor(end)); }
-
-    inline void PushBack(int _i) { 
-        size_t nextEnd = NextCursor(end); 
-        if (front != nextEnd) {
-            cont[end] = _i; end = nextEnd;
+//doubly linked list로 구현해봄
+struct deque {
+    deque() : size(0) { begin.next = &end; end.prev = &begin; begin.data = -1; end.data = -1; }
+    ~deque() {
+        Node* todelete = begin.next;
+        while (todelete != &end) {
+            Node* temp = todelete;
+            todelete = todelete->next;
+            delete temp;
         }
     }
-    inline int Front() { return cont[front]; }
-    inline void Pop() { if (false == Empty()) { front = NextCursor(front); } }
 
-    std::vector<int> cont;
-    size_t front;
-    size_t end;
-    size_t capacity;
+    inline bool Empty() { return (size == 0); }
+    inline int Size() { return size; }
+
+    inline void PushFront(int _u) {
+        Node* temp = begin.next;
+
+        begin.next = new Node;
+        begin.next->data = _u;
+        begin.next->next = temp;
+        begin.next->prev = &begin;
+
+        temp->prev = begin.next;
+        ++size;
+    }
+    inline void PushBack(int _u) {
+        Node* temp = end.prev;
+
+        end.prev = new Node;
+        end.prev->data = _u;
+        end.prev->prev = temp;
+        end.prev->next = &end;
+
+        temp->next = end.prev;
+        ++size;
+    }
+    inline void PopFront() {
+        if (false == Empty()) {
+            Node* temp = begin.next;
+
+            begin.next = temp->next;
+            begin.next->prev = &begin;
+            delete temp;
+            --size;
+        }
+    }
+    inline void PopBack() {
+        if (false == Empty()) {
+            Node* temp = end.prev;
+
+            end.prev = temp->prev;
+            end.prev->next = &end;
+            delete temp;
+            --size;
+        }
+    }
+    inline int Front() { return begin.next->data; }
+    inline int Back() { return end.prev->data; }
+
+    Node begin;
+    Node end;
+    int size;
 };
 
 int main() {
@@ -44,29 +90,63 @@ int main() {
     READ_INPUT;
     WRITE_OUTPUT;
 
-    int N{}; int K{}; std::cin >> N >> K;
-    //K번째 사람을 꺼내와야 하므로 K-1번째까지만 반복하기 위함
-    --K;
+    //_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
-    CircleQueue josephus{ (size_t)N };
+    /* TC Generation Code */
+    //{
+    //    srand(time(0));
+    //    int gen = 1000000;
+    //    int inserts = 0;
+    //    std::cout << gen << '\n';
+    //    for (int i = 0; i < gen; ++i) {
+    //        int com = 1 + rand() % 8;
+    //        std::cout << com;
+    //        if (com == 1 || com == 2) {
+    //            std::cout << ' ' << (1 + rand() % 10000);
+    //            ++inserts;
+    //        }
+    //        std::cout << '\n';
+    //    }
+    //    std::cout << "Inserts: " << inserts;
+
+    //    return 0;
+    //}
+
+    deque dq{};
+
+    int N{}; std::cin >> N;
     for (int i = 0; i < N; ++i) {
-        josephus.PushBack(i + 1);
-    }
-
-    std::cout << '<';
-    while (false == josephus.Empty()) {
-        for (int i = 0; i < K; ++i) {
-            int front = josephus.Front();
-            josephus.Pop();
-            josephus.PushBack(front);
-        }
+        int command{}; std::cin >> command;
         
-        int front = josephus.Front();
-        josephus.Pop();
-        std::cout << front << ", ";
+        if (command == 1) {
+            int input{}; std::cin >> input;
+            dq.PushFront(input);
+        }
+        else if (command == 2) {
+            int input{}; std::cin >> input;
+            dq.PushBack(input);
+        }
+        else if (command == 3) {
+            std::cout << dq.Front() << '\n';
+            dq.PopFront();
+        }
+        else if (command == 4) {
+            std::cout << dq.Back() << '\n';
+            dq.PopBack();
+        }
+        else if (command == 5) {
+            std::cout << dq.Size() << '\n';
+        }
+        else if (command == 6) {
+            std::cout << (int)dq.Empty() << '\n';
+        }
+        else if (command == 7) {
+            std::cout << dq.Front() << '\n';
+        }
+        else if (command == 8) {
+            std::cout << dq.Back() << '\n';
+        }
     }
-    std::cout.seekp(-2, std::ios::end);
-    std::cout << '>';
 
     return 0;
 }
