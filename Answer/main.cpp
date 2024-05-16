56d2fde8b26c905f9afc75b6279ac472f8f90ed5
HWNOTE <ekdrn1@naver.com>
2024-05-16 23:45:38 +0900

    백준 24511 (queuestack)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,59 +14,71 @@ std::ios_base::sync_with_stdio(false)
 ////////////////////////////
 
 #include <vector>
+struct Deque {
+    Deque(size_t _cap) : cont(_cap + 1), front(), rear(), size(), capacity(_cap) {}
 
-struct balloon {
-    int idx;
-    int moveDist;
-};
-
-struct deque {
-    deque(size_t _capacity) : cont(), capacity(_capacity), size(), begin(), end() { cont.resize(capacity + 1); }
-
-    size_t PrevCursor(size_t _cursor) { if (_cursor == 0) { _cursor = cont.size(); } return --_cursor; }
-    size_t NextCursor(size_t _cursor) { if (++_cursor == cont.size()) { _cursor = 0; } return _cursor; }
     bool Empty() { return (size == 0); }
     bool Full() { return (size == capacity); }
-
     size_t Size() { return size; }
 
-    void PushFront(balloon _b) {
+    void Resize(size_t _cap) {
+        capacity = _cap;
+        std::vector<int> temp(capacity + 1);
+
+        size_t prevSize = size;
+        for (int i = 0; false == Empty(); ++i) {
+            temp[i] = Front();
+            PopFront();
+        }
+
+        cont.swap(temp);
+        front = 0;
+        rear = prevSize;
+        size = prevSize;
+    }
+
+    size_t NextCursor(size_t _cur) { if (++_cur == cont.size()) { _cur = 0; } return _cur; }
+    size_t PrevCursor(size_t _cur) { if (_cur == 0) { _cur = cont.size(); } return --_cur; }
+    
+    void PushFront(int _i) {
         if (Full()) { return; }
 
-        begin = PrevCursor(begin);
-        cont[begin] = _b;
+        front = PrevCursor(front);
+        cont[front] = _i;
         ++size;
     }
-    void PushBack(balloon _b) {
+    void PushBack(int _i) {
         if (Full()) { return; }
 
-        cont[end] = _b;
-        end = NextCursor(end);
+        cont[rear] = _i;
+        rear = NextCursor(rear);
         ++size;
     }
     void PopFront() {
         if (false == Empty()) {
-            begin = NextCursor(begin);
+            front = NextCursor(front);
             --size;
         }
     }
     void PopBack() {
         if (false == Empty()) {
-            end = PrevCursor(end);
+            rear = PrevCursor(rear);
             --size;
         }
     }
-    balloon Front() { return cont[begin]; }
-    balloon Back() { return cont[PrevCursor(end)]; }
 
-    std::vector<balloon> cont;
-    size_t capacity;
+    int Front() { return cont[front]; }
+    int Back() { return cont[PrevCursor(rear)]; }
+
+    std::vector<int> cont;
+    size_t front;
+    size_t rear;
     size_t size;
-    size_t begin;
-    size_t end;
+    size_t capacity;
 };
 
-
+//deque를 활용하면 풀수 있다.
+//stack자료구조 뭐시기 데이터는 쓸모 없음
 int main() {
     USING_IOSTREAM;
 
@@ -74,39 +86,48 @@ int main() {
     WRITE_OUTPUT;
 
     int N{}; std::cin >> N;
-    deque dq{ (size_t)N };
 
-    for (int i = 1; i <= N; ++i) {
+    std::vector<int> isStackElem((size_t)N);
+    size_t queueSize{};
+    
+    //Input Line 2
+    for (int i = 0; i < N; ++i) {
+        std::cin >> isStackElem[i];
+        if (false == isStackElem[i]) {
+            ++queueSize;
+        }
+    }
+    Deque dq(queueSize);
+
+    //Input Line 3
+    for (int i = 0; i < N; ++i) {
         int input{}; std::cin >> input;
-        dq.PushBack(balloon{ i, input });
+        if (false == isStackElem[i]) {
+            //3번줄의 입력은 입력 순서대로 앞에서부터 채워준다.(1 4)
+            dq.PushBack(input);
+        }
     }
 
-    balloon move = dq.Front();
-    dq.PopFront();
-    std::cout << 1 << ' ';
-    for (int i = 1; i < N; ++i) {
-        if (move.moveDist > 0) {
-            --move.moveDist;
-            for (int i = 0; i < move.moveDist; ++i) {
-                balloon temp = dq.Front();
-                dq.PopFront();
-                dq.PushBack(temp);
-            }
-            move = dq.Front();
-            std::cout << move.idx << ' ';
-            dq.PopFront();
-        }
-        else {
-            ++move.moveDist;
-            for (int i = move.moveDist; i < 0; ++i) {
-                balloon temp = dq.Back();
-                dq.PopBack();
-                dq.PushFront(temp);
-            }
-            move = dq.Back();
-            std::cout << move.idx << ' ';
-            dq.PopBack();
+    //Input Line 4
+    int M{}; std::cin >> M;
+    dq.Resize(queueSize + M);
+    for (int i = 0; i < M; ++i) {
+        int input{}; std::cin >> input;
+        //Input Line 5, 앞에서부터 거꾸로 채워나간다(PushFront) -> 7 4 2 (1 4)
+        dq.PushFront(input);
+
+        //M개만 출력하면 되므로 M개 이상 되면 중단
+        ++queueSize;
+        if (queueSize > M) {
+            break;
         }
     }
+
+    //M개만큼 뒤에서 출력
+    for (int i = 0; i < M; ++i) {
+        std::cout << dq.Back() << ' ';
+        dq.PopBack();
+    }
+
     return 0;
 }
