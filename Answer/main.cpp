37e87bcb62c84b626250dd9b4d865e442e3cc901
HWNOTE <ekdrn1@naver.com>
2024-05-17 21:12:32 +0900

    백준 11050 (이항 계수 1)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -29,8 +29,9 @@ int main() {
     READ_INPUT;
     WRITE_OUTPUT;
 
-    uint64 N{}; std::cin >> N;
-    std::cout << Factorial(N);
+    uint64 N{}, K{}; std::cin >> N >> K;
+    //N C K -> N! / ( K!(N - K)! )
+    std::cout << Factorial(N) / (Factorial(K) * Factorial(N - K));
 
     return 0;
 }
