4ed15a94abb07185f7c4c56fcfb75bf303a0b83f
HWNOTE <ekdrn1@naver.com>
2024-05-18 17:34:39 +0900

    백준 25192 (인사성 밝은 곰곰이)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,50 +13,106 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 ////////////////////////////
 
-using uint = std::uint32_t;
-uint GCD(uint _a, uint _b) {
+#include <vector>
+#include <string>
+constexpr size_t bucketSize = 120001;
 
-    while (_b != 0) {
-        uint r = _a % _b;
-        _a = _b;
-        _b = r;
+struct str {
+    char cont[24]{};
+    size_t strLen{};
+    bool Empty() const { return (0 == strLen); }
+
+    bool operator == (const str& _other) const {
+        if (strLen != _other.strLen) { return false; }
+
+        if (false == Empty()) {
+            for (size_t i = 0; i < strLen; ++i) {
+                if (cont[i] != _other.cont[i]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
+size_t Hash(const str& _str) {
+    size_t ret{};
+
+    constexpr size_t p = 31;
+
+    if (false == _str.Empty()) {
+        ret = (size_t)_str.cont[0];
+        for (size_t i = 1; i < _str.strLen; ++i) {
+            ret = (ret * p + (size_t)_str.cont[i]) % bucketSize;
+        }
     }
 
-    return _a;
+    return ret;
 }
 
-uint LCM(uint _a, uint _b) {
-    //a * b / GCD(a,b) -> overflow 방지를 위해 a / gcd(a, b) * b
-    return (_a / GCD(_a, _b) * _b);
-}
+constexpr str ENTER = { "ENTER", 5 };
 
-//오답 코드//
-//이유: 최소공배수로 구할 시 소수들의 곱으로 표현되는 수가 정답인 경우 예외가 생길 수 있음.
-//ex: 7 49 343의 최소공배수는 686 -> 7 49 343 이외에도 다른 약수를 가지고 있다.
+struct userLog {
+    str name;
+    int welcomeCount{};
+};
+
 int main() {
     USING_IOSTREAM;
 
     READ_INPUT;
     WRITE_OUTPUT;
 
-    uint divisorCount{}; std::cin >> divisorCount;
+    int N{}; std::cin >> N;
 
-    if (1 == divisorCount) {
-        uint input{}; std::cin >> input;
-        std::cout << input * input;
-    }
-    else {
-        uint biggest{};
-        std::cin >> biggest;
-        uint smallest = biggest;
-        for (uint i = 1; i < divisorCount; ++i) {
-            uint input{}; std::cin >> input;
-            biggest = std::max(biggest, input);
-            smallest = std::min(smallest, input);
+    std::vector<userLog> chatLog{};
+    chatLog.resize(bucketSize);
+
+    int enterCount{};   //들어온 사람 숫자
+    int gomgomCount{};  //곰곰티콘 쓴 사람 숫자
+    str input{};
+    for (int i = 0; i < N; ++i) {
+        input.cont[0] = '\0';
+        std::cin >> input.cont;
+        input.strLen = std::strlen(input.cont);
+
+        if (input == ENTER) {
+            ++enterCount;
+            continue;
         }
 
-        std::cout << biggest * smallest;
+        size_t hash = Hash(input);
+
+        bool found = false;
+        while (false == chatLog[hash].name.Empty()) {
+            if (chatLog[hash].name == input) {
+                found = true;
+                
+                //인사한횟수가 들어온 사람수보다 적다면, 인사를 한것임
+                if (enterCount > chatLog[hash].welcomeCount) {
+                    ++gomgomCount;
+
+                    //인사횟수 갱신
+                    chatLog[hash].welcomeCount = enterCount;
+                }
+
+                break;
+            }
+
+            ++hash;
+            hash %= chatLog.size();
+        }
+
+        //못찾았다면, 즉 처음 메시지를 보낸사람이라면 등록후 gomgomCount 사용처리
+        if (false == found) {
+            chatLog[hash].name = input;
+            chatLog[hash].welcomeCount = enterCount;
+            ++gomgomCount;
+        }
     }
+
+    std::cout << gomgomCount;
 
     return 0;
 }
