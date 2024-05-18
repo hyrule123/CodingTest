b81a9a5cb9ea201e7950a399c84bd5b0be3b5493
HWNOTE <ekdrn1@naver.com>
2024-05-18 15:20:19 +0900

    백준 1037 (약수) - 오답(이유: 최소공배수로는 구할 수 없다.)

--- Answer/Reference/GCD.h
+++ Answer/Reference/GCD.h
@@ -2,6 +2,7 @@
 //Euclidian algorithm
 inline int GCD(int _a, int _b) {
     int r{};
+    //요약: (b가 0이 될 때까지) b로 나눈 나머지를 구하고 -> b를 그 나머지로 나누기를 반복한다.
     while (_b != 0) {
         r = _a % _b;
         _a = _b;
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,33 +13,51 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 ////////////////////////////
 
-using uint64 = std::uint64_t;
-uint64 Factorial(uint64 _num, uint64 _range) {
-    uint64 ret = 1;
+using uint = std::uint32_t;
+uint GCD(uint _a, uint _b) {
 
-    for (uint64 i = 0; i < _range; ++i) {
-        ret *= _num;
-        --_num;
+    while (_b != 0) {
+        uint r = _a % _b;
+        _a = _b;
+        _b = r;
     }
 
-    return ret;
+    return _a;
 }
 
-uint64 Combination(uint64 _items, uint64 _selections) {
-    uint64 range = std::min(_selections, _items - _selections);
-    return Factorial(_items, range) / Factorial(range, range);
+uint LCM(uint _a, uint _b) {
+    //a * b / GCD(a,b) -> overflow 방지를 위해 a / gcd(a, b) * b
+    return (_a / GCD(_a, _b) * _b);
 }
 
+//오답 코드//
+//이유: 최소공배수로 구할 시 소수들의 곱으로 표현되는 수가 정답인 경우 예외가 생길 수 있음.
+//ex: 7 49 343의 최소공배수는 686 -> 7 49 343 이외에도 다른 약수를 가지고 있다.
 int main() {
     USING_IOSTREAM;
 
     READ_INPUT;
     WRITE_OUTPUT;
 
-    int T{}; std::cin >> T;
-    for (int i = 0; i < T; ++i) {
-        uint64 N{}, M{}; std::cin >> N >> M;
-        std::cout << Combination(M, N) << '\n';
+    uint divisorCount{}; std::cin >> divisorCount;
+
+    if (1 == divisorCount) {
+        uint input{}; std::cin >> input;
+        std::cout << input * input;
+    }
+    else {
+        uint lcm{}; std::cin >> lcm;
+        uint biggest = lcm;
+        uint smallest = lcm;
+        for (uint i = 1; i < divisorCount; ++i) {
+            uint input{}; std::cin >> input;
+            lcm = LCM(lcm, input);
+            if (biggest < input) { biggest = input; }
+        }
+        //제일 큰 숫자도 N이 아닌 약수이므로
+        lcm = LCM(lcm, biggest * 2);
+
+        std::cout << lcm;
     }
 
     return 0;
