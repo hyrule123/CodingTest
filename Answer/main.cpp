c226e77ea7b235540eb61fd075301e661a3daf2c
HWNOTE <ekdrn1@naver.com>
2024-05-21 20:33:06 +0900

    백준 24060 (알고리즘 수업 - 병합 정렬 1)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,15 +13,60 @@ std::ios_base::sync_with_stdio(false)
 #include <cstring>  //memset
 ////////////////////////////
 
-int recursion(const char* s, int l, int r, int& callCount) {
-    ++callCount;
-    if (l >= r) return 1;
-    else if (s[l] != s[r]) return 0;
-    else return recursion(s, l + 1, r - 1, callCount);
+#include <vector>
+int DnC(std::vector<int>& _vec, std::vector<int>& _temp, const size_t _start, const size_t _end, int& _nthWrite) {
+    if (_start >= _end) { return -1; }
+
+
+    const size_t mid = _start + (_end - _start) / 2;
+    size_t l = _start;
+    size_t r = mid + 1;
+
+    int ret = 0;
+    ret = DnC(_vec, _temp, l, mid, _nthWrite);
+    if (-1 != ret) { return ret; }
+
+    ret = DnC(_vec, _temp, r, _end, _nthWrite);
+    if (-1 != ret) { return ret; }
+
+    size_t cursor = _start;
+    while (l <= mid && r <= _end) {
+        if (_vec[l] <= _vec[r]) {
+            _temp[cursor] = _vec[l];
+            ++cursor; ++l;
+        }
+        else {
+            _temp[cursor] = _vec[r];
+            ++cursor; ++r;
+        }
+    }
+
+    while (l <= mid) {
+        _temp[cursor] = _vec[l];
+        ++cursor; ++l;
+    }
+    while (r <= _end) {
+        _temp[cursor] = _vec[r];
+        ++cursor; ++r;
+    }
+
+    for (size_t i = _start; i <= _end; ++i) {
+        _vec[i] = _temp[i];
+        --_nthWrite;
+        if (_nthWrite == 0) {
+            return _vec[i];
+        }
+    }
+
+    return -1;
 }
 
-int isPalindrome(const char* s, int& callCount) {
-    return recursion(s, 0, (int)strlen(s) - 1, callCount);
+int MergeSortNth(std::vector<int>& _vec, int _nthWrite) {
+    if (_vec.size() < 2) { return -1; }
+
+    std::vector<int> temp(_vec.size());
+
+    return DnC(_vec, temp, 0, temp.size() - 1, _nthWrite);
 }
 
 int main() {
@@ -29,17 +74,14 @@ int main() {
 
     READ_INPUT;
     WRITE_OUTPUT;
-    
-    int T{}; std::cin >> T;
 
-    char input[1004]{};
-    for (int i = 0; i < T; ++i) {
-        input[0] = '\0';
-        std::cin >> input;
-        int callCount = 0;
-        int ispal = isPalindrome(input, callCount);
-        std::cout << ispal << ' ' << callCount << '\n';
+    size_t N{}; int K{}; std::cin >> N >> K;
+    std::vector<int> vec(N);
+    for (size_t i = 0; i < N; ++i) {
+        std::cin >> vec[i];
     }
+
+    std::cout << MergeSortNth(vec, K);
 
     return 0;
 }
