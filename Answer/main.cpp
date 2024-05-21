7dae194e75498947f97ff292b02a153dcb9423c3
HWNOTE <ekdrn1@naver.com>
2024-05-21 23:24:14 +0900

    백준 2447 (별 찍기 - 10)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,18 +14,30 @@ std::ios_base::sync_with_stdio(false)
 ////////////////////////////
 
 #include <vector>
-void Cantor(std::vector<char>& _arr, const size_t _start, const size_t _end) {
-    size_t len = _end - _start;
-    if (len <= 1) { 
-        return; 
+#include <string>
+
+void SetStar(std::vector<std::vector<char>>& _sqre, const size_t _startX, const size_t _startY, size_t _length) {
+    if (_length <= 1) { return; }
+    _length /= 3;
+    
+    size_t xErase = _startX + _length;
+    size_t xEraseEnd = xErase + _length;
+    size_t yErase = _startY + _length;
+    size_t yEraseEnd = yErase + _length;
+
+    //중간 뚫기
+    for (size_t y = yErase; y < yEraseEnd; ++y) {
+        memset(_sqre[y].data() + xErase, ' ', _length);
     }
-    len /= 3;
 
-    size_t part = _start + len;
-    memset(_arr.data() + part, ' ', len);
-
-    Cantor(_arr, _start, part);
-    Cantor(_arr, part + len, _end);
+    //8방향 재귀
+    for (size_t x = _startX, xEnd = xEraseEnd + _length; x < xEnd; x += _length) {
+        for (size_t y = _startY, yEnd = yEraseEnd + _length; y < yEnd; y += _length) {
+            //방금 했던 중간부분은 호출 안해도 됨
+            if (x == xErase && y == yErase) { continue; }
+            SetStar(_sqre, x, y, _length);
+        }
+    }
 }
 
 int main() {
@@ -34,22 +46,22 @@ int main() {
     READ_INPUT;
     WRITE_OUTPUT;
 
-    std::vector<char> arr{};
-    int input{};
-    while (std::cin >> input) {
-        size_t size = 1;
-        while ((--input) >= 0) {
-            size *= 3;
-        }
-        arr.clear();
-        arr.resize(size + 2, '-');
-        
-        arr[arr.size() - 2] = '\n';
-        arr[arr.size() - 1] = '\0';
-        
-        Cantor(arr, 0, size);
-
-        std::cout << arr.data();
+    size_t N{}; std::cin >> N;
+    
+    std::vector<std::vector<char>> square;
+    square.resize(N);
+    size_t charSize = N + 2;
+    for (size_t i = 0; i < N; ++i) {
+        square[i].resize(charSize, '*');
+        square[i][charSize - 2] = '\n';
+        square[i][charSize - 1] = '\0';
     }
+
+    SetStar(square, 0, 0, N);
+
+    for (size_t i = 0; i < N; ++i) {
+        std::cout << square[i].data();
+    }
+
     return 0;
 }
