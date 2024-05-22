5fee3e745efc3f1b6e7196aa130e20abcbef1cf0
HWNOTE <ekdrn1@naver.com>
2024-05-22 23:09:02 +0900

    백준 15649 (N과 M(1))

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,32 +14,34 @@ std::ios_base::sync_with_stdio(false)
 ////////////////////////////
 
 #include <vector>
-
-using uint = unsigned int;
-void HanoiRecursive(uint _curLevel, uint _from, uint _to, uint _temp) {
-    if (_curLevel == 0) { 
+void DigitRecursive(std::vector<int>& _inUse, const int _range, const size_t _digitMax) {
+    if (_inUse.size() == _digitMax) { 
+        for (size_t i = 0; i < _inUse.size(); ++i) {
+            std::cout << _inUse[i] << ' ';
+        }
+        std::cout << '\n';
         return; 
     }
 
-    //가장 작은 원반부터 옮기기 시작(1차 재귀) - temp와 to에 번갈아가면서 쌓아준다.
-    HanoiRecursive(_curLevel - 1, _from, _temp, _to);
-
-    //내 원반을 목표 봉에 넣어주고
-    std::cout << _from << ' ' << _to << '\n';
-
-    //temp 봉에 옮겨놓은 나보다 작은 원반들을 목표 봉으로 옮겨준다(2차 재귀)
-    HanoiRecursive(_curLevel - 1, _temp, _to, _from);
+    for (int i = 1; i <= _range; ++i) {
+        bool cont = false;
+        for (size_t j = 0; j < _inUse.size(); ++j) {
+            if (_inUse[j] == i) {
+                cont = true;
+                break;
+            }
+        }
+        if (cont) { continue; }
+        _inUse.push_back(i);
+        DigitRecursive(_inUse, _range, _digitMax);
+        _inUse.pop_back();
+    }
 }
 
-void Hanoi(uint _levels) {
-    uint trials = 1;
-    for (uint i = 0; i < _levels; ++i) {
-        trials *= 2;
-    }
-    trials -= 1;
-    std::cout << trials << '\n';
-
-    HanoiRecursive(_levels, 1u, 3u, 2u);
+void Digit(int N, size_t M) {
+    std::vector<int> inUse{};
+    inUse.reserve(M);
+    DigitRecursive(inUse, N, M);
 }
 
 int main() {
@@ -47,10 +49,9 @@ int main() {
 
     READ_INPUT;
     WRITE_OUTPUT;
-
-    uint N{}; std::cin >> N;
-
-    Hanoi(N);
     
+    int N{}; size_t M{}; std::cin >> N >> M;
+    Digit(N, M);
+
     return 0;
 }
