2df4944c1b558235bc1ec097e025c17a6d2b5bec
HWNOTE <ekdrn1@naver.com>
2024-05-23 23:16:24 +0900

    백준 15651 (N과 M (3))

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -2,51 +2,38 @@
 #define READ_INPUT (void)0
 #define WRITE_OUTPUT (void)0
 #endif //NDEF LOCAL
-
 #define USING_IOSTREAM \
 std::cin.tie(nullptr);\
 std::ios_base::sync_with_stdio(false)
-
-////////////////////////////
 #include <stdio.h>
 #include <iostream>
 #include <cstring>  //memset
-////////////////////////////
+//////////////////
+
 
 #include <vector>
-
-void AscendingRecursive(std::vector<int>& _before, const int _range, const size_t _max) {
-    if (_before.size() == _max) {
-        for (size_t i = 0; i < _before.size(); ++i) {
-            std::cout << _before[i] << ' ';
+void Recursive(std::vector<int>& _arr, const int _range, const size_t _max) {
+    if (_arr.size() == _max) {
+        for (size_t i = 0; i < _arr.size(); ++i) {
+            std::cout << _arr[i] << ' ';
         }
         std::cout << '\n';
         return;
     }
 
-
-    for (int i = (_before.empty() ? 0 : _before.back()) + 1; i <= _range; ++i) {
-        _before.push_back(i);
-        AscendingRecursive(_before, _range, _max);
-        _before.pop_back();
+    for (int i = 1; i <= _range; ++i) {
+        _arr.push_back(i);
+        Recursive(_arr, _range, _max);
+        _arr.pop_back();
     }
 }
 
-void Ascending(int _n, size_t _m) {
-    std::vector<int> before;
-    before.reserve(_n);
-
-    AscendingRecursive(before, _n, _m);
-}
-
-int main() {
-    USING_IOSTREAM;
-
-    READ_INPUT;
-    WRITE_OUTPUT;
+int main() { READ_INPUT; WRITE_OUTPUT; USING_IOSTREAM;
 
     int N{}; size_t M{}; std::cin >> N >> M;
-    Ascending(N, M);
+    std::vector<int> arr;
+    arr.reserve(M);
+    Recursive(arr, N, M);
 
     return 0;
 }
