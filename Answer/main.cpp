e7a4486fc0ec64596a4eb12493c68fa1cd42b8d9
HWNOTE <ekdrn1@naver.com>
2024-05-27 14:55:35 +0900

    백준 24416 (알고리즘 수업 - 피보나치 수 1) - 동적프로그래밍

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -9,94 +9,36 @@
 #include <cstring>  //memset
 //////////////////
 
-#include <vector>
-#include <array>
 using uint = unsigned int;
-
-size_t minGap = std::numeric_limits<size_t>::max();
-
-std::vector<std::vector<size_t>> synergyScores{};
-
-std::vector<bool> occupied{};
-std::vector<size_t> teamANumber{};
-std::vector<size_t> teamBNumber{};
-size_t N{};
-size_t teamSize{};
-
-void Recursive() {
-    //팀 A가 결정됐을 경우, 팀 B는 자동 결정됨.
-    if (teamSize == teamANumber.size()) {
-        size_t scoreA{}, scoreB{};
- 
-        //시너지 점수 합 계산 후 return
-        teamBNumber.clear();
-        for (size_t i = 0; i < N; ++i) {
-            if (false == occupied[i]) {
-                teamBNumber.push_back(i);
-            }
-        }
-        
-        for (size_t i = 0, iEnd = teamSize - 1; i < iEnd; ++i) {
-            for (size_t j = i + 1; j < teamSize; ++j) {
-                size_t idx = teamANumber[i];
-                size_t jdx = teamANumber[j];
-
-                scoreA += synergyScores[idx][jdx] + synergyScores[jdx][idx];
-                
-                idx = teamBNumber[i];
-                jdx = teamBNumber[j];
-                scoreB += synergyScores[idx][jdx] + synergyScores[jdx][idx];
-            }
-        }
-
-        size_t gap = scoreA > scoreB ? scoreA - scoreB : scoreB - scoreA;
-        if (gap < minGap) { minGap = gap; }
-
-        return;
-    }
-
-    //A팀에 넣을 선수 번호, 오름차순으로만(이러면 중복되지 않음)
-    for (size_t n = teamANumber.back() + 1; n < N; ++n) {
-        teamANumber.push_back(n);
-        occupied[n] = true;
-
-        Recursive();
-        if (minGap == 0) { return; }
-
-        occupied[n] = false;
-        teamANumber.pop_back();
-    }
+#include <vector>
+uint FibRecursive(size_t _n, uint& _code1Count) {
+    if (_n <= 2) { ++_code1Count; return 1; }
+    
+    return FibRecursive(_n - 1, _code1Count) + FibRecursive(_n - 2, _code1Count);
 }
 
-void DivideTeam() {
-    teamSize = synergyScores.size() / 2;
-    teamANumber.reserve(teamSize);
-    teamBNumber.reserve(teamSize);
+uint FibDynamic(size_t _n, uint& _code2Count) {
+    std::vector<uint> seq(_n, 1);
 
-    //A팀을 구성하면 B팀은 자동 확정되므로, A팀만 뽑아준다.
-    //2n명의 구성원을 n명/n명 중복 및 순서 없이 나눠야 한다.
-    //오름차순으로 구성할 시 중복 및 순서가 모두 제외된다.
-    //무조건 A팀에 B팀보다 낮은 수가 들어가야 하므로(오름차순), 0(1번선수)을 넣어준다.
-    //ex)134 / 256 -> 256 / 134 등의 중복 경우가 제외됨.    
-    teamANumber.push_back(0u);
-    occupied.resize(N);
-    occupied[0] = true;
+    for (size_t i = 2; i < _n; ++i) {
+        seq[i] = seq[i - 1] + seq[i - 2];
+        ++_code2Count;
+    }
 
-    Recursive();
-    std::cout << minGap;
+    return seq[_n - 1];
 }
 
 int main() { 
     READ_INPUT; WRITE_OUTPUT; USING_IOSTREAM;
 
-    std::cin >> N;
-    synergyScores.resize(N, std::vector<size_t>(N));
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < N; ++j) {
-            std::cin >> synergyScores[i][j];
-        }
-    }
-    DivideTeam();
+    size_t N{}; std::cin >> N;
+    uint count = 0;
+    FibRecursive(N, count);
+    std::cout << count << ' ';
+
+    count = 0;
+    FibDynamic(N, count);
+    std::cout << count;
 
     return 0;
 }
