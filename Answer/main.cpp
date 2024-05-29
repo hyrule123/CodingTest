c2857193a32de0d256b6beb3cd542bcb328446bf
HWNOTE <ekdrn1@naver.com>
2024-05-29 21:59:43 +0900

    백준 1912 (연속합)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -9,28 +9,23 @@
 #include <cstring>  //memset
 //////////////////
 
-#include <array>
-using uint64 = std::uint64_t;
-constexpr size_t size = 100;
-constexpr std::array<uint64, size> PadovanPreMake() {
-    std::array<uint64, size> ret{ 1, 1, 1, 2, 2, };
-
-    for (size_t i = 5; i < size; ++i) {
-        ret[i] = ret[i - 1] + ret[i - 5];
-    }
-
-    return ret;
-}
-constexpr std::array<uint64, size> PadovanSeq = PadovanPreMake();
+#include <vector>
 
 int main() {
     READ_INPUT; WRITE_OUTPUT; USING_IOSTREAM;
 
-    int N; std::cin >> N;
-    for (int i = 0; i < N; ++i) {
-        size_t T; std::cin >> T;
-        std::cout << PadovanSeq[T - 1] << '\n';
+    size_t N{}; std::cin >> N;
+    std::vector<int> seq(N);
+    std::cin >> seq[0];
+    int maxSum = seq[0];
+    for (size_t i = 1; i < N; ++i) {
+        std::cin >> seq[i];
+        
+        //지난 연속합에 이어서 더한다. 더한 값을 쓰는것보다 새로 시작하는게 더 클경우 여기부터 연속합을 새로 시작한다.
+        seq[i] = std::max(seq[i - 1] + seq[i], seq[i]);
+        maxSum = std::max(seq[i], maxSum);
     }
+    std::cout << maxSum;
 
     return 0;
 }
