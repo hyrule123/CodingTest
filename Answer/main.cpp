869449e7306d89ee0981ba6a6b6d4cd6c2fc5056
HWNOTE <ekdrn1@naver.com>
2024-05-31 22:30:48 +0900

    백준 1463 (1로 만들기)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -9,46 +9,49 @@
 #include <cstring>  //memset
 //////////////////
 
+using uint = unsigned int;
 #include <vector>
-struct accScore { int idx, score; };
 
 int main() {
     READ_INPUT; WRITE_OUTPUT; USING_IOSTREAM;
 
-    size_t N{}; std::cin >> N;
-    std::vector<int> scores(N);
-    for (size_t i = 0; i < N; ++i) {
-        std::cin >> scores[i];
-    }
-    //사이즈가 2 이하일 경우 경우의 수는 하나
-    if (N <= 2) {
-        int sum{};
-        for (size_t i = 0; i < scores.size(); ++i) {
-            sum += scores[i];
+    size_t N{}; std::cin >> N; ;
+    if (N == 1) { std::cout << 0; return 0; }
+    else if (N <= 3) { std::cout << 1; return 0; }
+
+    std::vector<uint> memo(N + 1);
+    
+    //1 to 1 -> 0, 2 to 1 -> 1, 3 to 1 -> 1
+    //4 to 1부터는 3가지 경우의수를 가진다.
+    
+    //ex)4 to 1
+    //(4-1)==3 to 1 : 1 + 1(-1 했으므로)
+    //(4/2)==2 to 1 : 1 + 1(/2 했으므로)
+    //(4/3): 없음
+    //>> -1번째, /2번째(나누어떨어질 경우), /3번째(나누떨어질 경우)
+    //인덱스에서 + 1을 한 결과와 같음
+
+    //ex)6 to 1
+    //6 - 1 to 1 + 1
+    //6 / 2 to 1 + 1
+    //6 / 3 to 1 + 1
+    //셋중 최솟값을 찾는다
+
+    memo[1] = 0; memo[2] = 1; memo[3] = 1;
+    for (size_t i = 4; i <= N; ++i) {
+        //-1
+        memo[i] = memo[i - 1] + 1;
+
+        // /2
+        if (i % 2 == 0) {
+            memo[i] = std::min(memo[i], memo[i / 2] + 1);
         }
-        std::cout << sum;
-        return 0;
+        // /3
+        if (i % 3 == 0) {
+            memo[i] = std::min(memo[i], memo[i / 3] + 1);
+        }
     }
-
-    std::vector<int> steps(N);
-    steps[0] = scores[0];
-
-    //두번째 계단까지 최대 점수는 무조건 계단을 다 밟고오는것
-    steps[1] = scores[0] + scores[1];
-
-    //계단 2(3번째)오는법: 0 2, 1, 2 둘중 큰거
-    steps[2] = std::max(scores[0], scores[1]) + scores[2];
-
-    for (size_t i = 3; i < N; ++i) {
-        //3계단을 올라왔을 때(2 + 1)
-        steps[i] = steps[i - 3] + scores[i - 1];
-
-        //2계단을 올라왔을때와 비교하고, 더 큰값을 채택
-        steps[i] = std::max(steps[i], steps[i - 2]);
-        steps[i] += scores[i];
-    }
-
-    std::cout << steps.back();
+    std::cout << memo.back();
 
     return 0;
 }
