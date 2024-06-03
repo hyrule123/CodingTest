7ed86d3bac057f33e6f4f7ed3dc99109f85b4886
HWNOTE <ekdrn1@naver.com>
2024-06-03 20:25:05 +0900

    백준 11054 (가장 긴 바이토닉 부분 수열) - O(logN) 방식 풀이 사용 코드

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -9,45 +9,64 @@
 #include <cstring>  //memset
 //////////////////
 
+//LIS 확인 시간복잡도 O(logN)인 버전
 #include <vector>
+#include <stack>
 using uint = unsigned int;
+size_t SearchDP(std::vector<uint>& _dp, uint _u) {
+	size_t start = 0;
+	size_t end = _dp.size();
+	size_t mid = end / 2;
+	while (start < end && 0 < mid) {
+		if (_dp[mid - 1] < _u && _u <= _dp[mid]) { break; }
+		if (_dp[mid] < _u) { start = mid; }
+		else { end = mid; }
+		mid = (start + end) / 2;
+	}
+	return mid;
+}
+
+//return val: 이번에 들어온 _u의 LIS 길이
+size_t InsertDP(std::vector<uint>& _dp, uint _u) {
+	if (_dp.empty() || _dp.back() < _u) {
+		_dp.push_back(_u);
+		return _dp.size();
+	}
+	size_t idx = SearchDP(_dp, _u);
+	_dp[idx] = _u;
+	return idx + 1;
+}
 
 int main() {
 	READ_INPUT; WRITE_OUTPUT; USING_IOSTREAM;
 
 	size_t N{}; std::cin >> N;
-	std::vector<uint> seq(N);
+	std::vector<uint> inputs(N);
+	std::vector<uint> leftLIS; leftLIS.reserve(N);
+	std::vector<uint> rightLIS; leftLIS.reserve(N);
+
 	for (size_t i = 0; i < N; ++i) {
-		std::cin >> seq[i];
+		std::cin >> inputs[i];
 	}
 
-	struct length { uint fromLeft, fromRight; };
-	std::vector<length> dp(N, { 1u, 1u });
-
-	for (size_t left = 0; left < N; ++left) {
-		//왼쪽에서 오른쪽으로
-		for (size_t j = 0; j < left; ++j) {
-			if (seq[j] < seq[left]) {
-				dp[left].fromLeft = std::max(dp[left].fromLeft, dp[j].fromLeft + 1);
-			}
-		}
-
-		//오른쪽에서 왼쪽으로
-		size_t right = N - left - 1;
-		for (size_t j = N - 1; j > right; --j) {
-			if (seq[right] > seq[j]) {
-				dp[right].fromRight = std::max(dp[right].fromRight, dp[j].fromRight + 1);
-			}
-		}
-	}
-
-	//left + right가 최대가 되는 부분을 구해준다.(둘다 각각 최소값 1) { 1 }의 max = 2 -> 나중에 1 빼줘야함
-	uint max = 2;
+	size_t max = 2;
 	for (size_t i = 0; i < N; ++i) {
-		max = std::max(max, dp[i].fromLeft + dp[i].fromRight);
+		//이번에 넣은 값이 몇개의 수열을 만들수있는지 확인
+		
+		//dpLeft는 하나씩 추가해가면서 
+		size_t sum = InsertDP(leftLIS, inputs[i]);
+
+		//dpRight는 매번 새로 만들어준다.(되돌릴 방법을 모르겠음, stack 써야되나)
+		rightLIS.clear();
+		for (size_t j = N - 1; j > i; --j) {
+			InsertDP(rightLIS, inputs[j]);
+		}
+		sum += InsertDP(rightLIS, inputs[i]);
+
+		max = std::max(max, sum);
 	}
-	--max;
-	std::cout << max;
+
+	std::cout << max - 1;
 
 	return 0;
 }
