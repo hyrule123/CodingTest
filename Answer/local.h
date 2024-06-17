55c005240052ac675e677231dc5f4e1ae478e46d
hw <ekdrn1@naver.com>
2024-06-17 22:50:46 +0900

    백준 1780 (종이의 개수)

--- Answer/local.h
+++ Answer/local.h
@@ -3,10 +3,7 @@
 
 #ifdef LOCAL
 #define _CRT_SECURE_NO_WARNINGS
-
-#define READ_INPUT (void)freopen("input.txt", "rt", stdin)
-#define WRITE_OUTPUT (void)freopen("output.txt", "wt", stdout)
-
+#define LOCAL_IO (void)freopen("input.txt", "rt", stdin); (void)freopen("output.txt", "wt", stdout)
 #endif //LOCAL
 
 //std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,50 +1,65 @@
 ﻿#ifndef LOCAL
-#define READ_INPUT (void)0
-#define WRITE_OUTPUT (void)0
+#define LOCAL_IO (void)0
 #endif
-
 #include <stdio.h>
 #include <iostream>
 #include <limits>
 #include <cstring>  //memset
 //////////////////
-#include <vector>
-std::vector<std::vector<char>> screen;
-void RecursivePrint(int _r, int _c, int _size) {
-	char first = screen[_r][_c];
 
-	bool diff = false;
+#include <vector>
+struct paperCount { 
+	int p[3];
+	void operator += (const paperCount& _other) {
+		p[0] += _other.p[0];
+		p[1] += _other.p[1];
+		p[2] += _other.p[2];
+	}
+};
+std::vector<std::vector<int>> paper;
+paperCount Recursive(int _size, int _r, int _c) {
+	paperCount ret{};
+	//이거 안하는게 더 빠름(했을 때 384 -> 안했을 때 348)
+	//if (_size == 1) { ret.p[paper[_r][_c]] = 1; return ret; }
+
+	int first = paper[_r][_c];
 	for (int r = _r; r < _r + _size; ++r) {
 		for (int c = _c; c < _c + _size; ++c) {
-			if (first != screen[r][c]) {
-				std::cout << '(';
 
-				int half = _size / 2;
-				RecursivePrint(_r, _c, half);
-				RecursivePrint(_r, _c + half, half);
-				RecursivePrint(_r + half, _c, half);
-				RecursivePrint(_r + half, _c + half, half);
-
-				std::cout << ')';
-				return;
+			//하나라도 다를 경우
+			if (first != paper[r][c]) {
+				int oneThird = _size / 3;
+				for (int i = 0; i < _size; i += oneThird) {
+					for (int j = 0; j < _size; j += oneThird) {
+						ret += Recursive(oneThird, _r + i, _c + j);
+					}
+				}
+				return ret;
 			}
 		}
 	}
 
-	std::cout << first;
+	//모두 같을 경우
+	ret.p[first] = 1;
+	return ret;
 }
 
 int main() {	
 	std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
-	READ_INPUT; WRITE_OUTPUT;
+	LOCAL_IO;
 
 	int N; std::cin >> N;
-	(void)std::cin.get();
-	screen.resize(N, std::vector<char>(N + 1));
+	paper.resize(N, std::vector<int>(N));
 	for (int i = 0; i < N; ++i) {
-		std::cin.getline(screen[i].data(), N + 1);
+		for (int j = 0; j < N; ++j) {
+			std::cin >> paper[i][j];
+			//-1 0 1을 인덱스로 사용하기 위해서 +1
+			++(paper[i][j]);
+		}
 	}
-	RecursivePrint(0, 0, N);
+
+	paperCount counts = Recursive(N, 0, 0);
+	std::cout << counts.p[0] << '\n' << counts.p[1] << '\n' << counts.p[2];
 
 	return 0;
 }
