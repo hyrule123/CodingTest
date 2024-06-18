4defe16fa255ccf0e9ab2e59f563ee51b4e55dab
hw <ekdrn1@naver.com>
2024-06-18 22:45:48 +0900

    백준 1629 (곱셈)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -6,41 +6,24 @@
 #include <limits>
 #include <cstring>  //memset
 //////////////////
+using uint64 = std::uint64_t;
 
-#include <vector>
-struct paperCount { 
-	int p[3];
-	void operator += (const paperCount& _other) {
-		p[0] += _other.p[0];
-		p[1] += _other.p[1];
-		p[2] += _other.p[2];
+uint64 PowRecursive(uint64 _base, uint64 _exp, uint64 _mod) {
+	if (_exp == 0) {
+		return 1;
 	}
-};
-std::vector<std::vector<int>> paper;
-paperCount Recursive(int _size, int _r, int _c) {
-	paperCount ret{};
-	//이거 안하는게 더 빠름(했을 때 384 -> 안했을 때 348)
-	//if (_size == 1) { ret.p[paper[_r][_c]] = 1; return ret; }
-
-	int first = paper[_r][_c];
-	for (int r = _r; r < _r + _size; ++r) {
-		for (int c = _c; c < _c + _size; ++c) {
-
-			//하나라도 다를 경우
-			if (first != paper[r][c]) {
-				int oneThird = _size / 3;
-				for (int i = 0; i < _size; i += oneThird) {
-					for (int j = 0; j < _size; j += oneThird) {
-						ret += Recursive(oneThird, _r + i, _c + j);
-					}
-				}
-				return ret;
-			}
-		}
+	else if (_exp == 1) {
+		return _base % _mod;
 	}
 
-	//모두 같을 경우
-	ret.p[first] = 1;
+	//2^5 = 2^2 * 2^2 * 2
+	//앞에 2개 분할정복으로 재사용하고, 나머지 홀수부분은 따로 곱해준다
+	bool isOdd = _exp % 2 == 1;
+	if (isOdd) { --_exp; }
+	uint64 ret = PowRecursive(_base, _exp / 2, _mod);
+	ret = ret * ret % _mod;
+	if (isOdd) { ret = ret * _base % _mod; }
+
 	return ret;
 }
 
@@ -48,18 +31,8 @@ int main() {
 	std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
 	LOCAL_IO;
 
-	int N; std::cin >> N;
-	paper.resize(N, std::vector<int>(N));
-	for (int i = 0; i < N; ++i) {
-		for (int j = 0; j < N; ++j) {
-			std::cin >> paper[i][j];
-			//-1 0 1을 인덱스로 사용하기 위해서 +1
-			++(paper[i][j]);
-		}
-	}
-
-	paperCount counts = Recursive(N, 0, 0);
-	std::cout << counts.p[0] << '\n' << counts.p[1] << '\n' << counts.p[2];
+	uint64 A, B, C; std::cin >> A >> B >> C;
+	std::cout << PowRecursive(A, B, C);
 
 	return 0;
 }
