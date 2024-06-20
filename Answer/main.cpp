1e6eed6f8a45dd93ba1ee00c3a1dd37ebb1b4ddf
hw <ekdrn1@naver.com>
2024-06-21 01:44:19 +0900

    백준 10830 (행렬 제곱)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -8,6 +8,7 @@
 //////////////////
 
 struct matrix {
+	matrix() : r{}, c{}, size{}, elem{} {}
 	matrix(int _r, int _c) { 
 		r = _r; 
 		c = _c; 
@@ -15,47 +16,83 @@ struct matrix {
 		elem = new int[size];
 		memset(elem, 0, size * sizeof(int));
 	}
-	matrix(std::istream& _in) {
-		std::cin >> r >> c;
-		size = r * c;
+	~matrix() { if (elem) { delete[] elem; } }
+
+	matrix(const matrix& _other) {
+		r = _other.r;
+		c = _other.c;
+		size = _other.size;
 		elem = new int[size];
-		for (int i = 0; i < size; ++i) {
-			_in >> elem[i];
+		memcpy(elem, _other.elem, size * sizeof(int));
+	}
+
+	void operator =(matrix&& _move) noexcept {
+		if (this != &_move) {
+			Move(std::move(_move));
 		}
 	}
-	~matrix() { delete[] elem; }
+	matrix(matrix&& _move) noexcept : matrix() { Move(std::move(_move)); }
+	inline void Move(matrix&& _move) noexcept {
+		r = _move.r;
+		c = _move.c;
+		size = _move.size;
+		std::swap(elem, _move.elem);
+	}
 
-	int r, c, size;
-	int* elem;
 	int* operator[] (int _row) { return &(elem[_row * c]); }
-
 	matrix operator * (matrix& _other) {
 		matrix ret(r, _other.c);
 
 		for (int _r = 0; _r < ret.r; ++_r) {
 			int rIdx = _r * ret.c;
 			for (int _c = 0; _c < ret.c; ++_c) {
-				
 				for (int _e = 0; _e < c; ++_e) {
-					ret.elem[rIdx + _c] += (*this)[_r][_e] * _other[_e][_c];
+					(ret.elem[rIdx + _c] += (*this)[_r][_e] * _other[_e][_c]) %= 1000;
 				}
 			}
 		}
 
 		return ret;
 	}
+	matrix operator % (int _div) {
+		matrix ret = *this;
+		for (int i = 0; i < ret.size; ++i) {
+			ret.elem[i] %= _div;
+		}
+		return ret;
+	}
+
+	int r, c, size;
+	int* elem;
 };
 
+matrix PowRecursive(matrix& _base, size_t _exp) {
+	if (_exp == 1) { return _base % 1000; }
+
+	bool isOdd = (_exp % 2 == 1);
+	if (isOdd) { --_exp; }
+	matrix ret = PowRecursive(_base, _exp / 2);
+	ret = ret * ret;
+	if (isOdd) { ret = ret * _base; }
+
+	return ret;
+}
 
 int main() {	
 	std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
 	LOCAL_IO;
 
-	matrix A(std::cin), B(std::cin);
-	matrix result = A * B;
-	for (int _r = 0; _r < result.r; ++_r) {
-		for (int _c = 0; _c < result.c; ++_c) {
-			std::cout << result[_r][_c] << ' ';
+	int size; size_t exp; std::cin >> size >> exp;
+	matrix A(size, size); 
+	for (int i = 0; i < A.size; ++i) {
+		std::cin >> A.elem[i];
+	}
+
+	matrix result = PowRecursive(A, exp);
+
+	for (int r = 0; r < result.r; ++r) {
+		for (int c = 0; c < result.c; ++c) {
+			std::cout << result[r][c] << ' ';
 		}
 		std::cout << '\n';
 	}
