fe2f11c15b51d21820b0f0dbdfa7c7735842fe20
hw <ekdrn1@naver.com>
2024-06-21 23:57:34 +0900

    백준 6549 (히스토그램에서 가장 큰 직사각형) - 내 풀이 오답과 반례

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -7,51 +7,61 @@
 #include <cstring>  //memset
 //////////////////
 
-//참고: 나무위키
-//행렬로 피보나치 계산하기
-//F2 F1		1 1
-//		== 
-//F1 F0		1 0
-//이 행렬 m을 거듭제곱 하면
-//m^n == F(n+1) F(n)
-//		 F(n)	F(n-1)
-
+#include <vector>
 using ull = unsigned long long;
-struct mat22 {
-	ull m[4];
-
-	ull* operator [](ull _row) { if (_row) { _row = 2; } return &(m[_row]); }
-	mat22 operator *(const mat22& _other) const {
-		mat22 ret{};
-
-		ret.m[0] = (m[0] * _other.m[0] + m[1] * _other.m[2]) % 1'000'000'007;
-		ret.m[1] = (m[0] * _other.m[1] + m[1] * _other.m[3]) % 1'000'000'007;
-		ret.m[2] = (m[2] * _other.m[0] + m[3] * _other.m[2]) % 1'000'000'007;
-		ret.m[3] = (m[2] * _other.m[1] + m[3] * _other.m[3]) % 1'000'000'007;
-
+struct hRect {
+	ull maxHeight, width, square;
+	hRect operator + (const hRect& _other) const {
+		hRect ret{};
+		ret.maxHeight = std::min(maxHeight, _other.maxHeight);
+		ret.width = width + _other.width;
+		ret.square = ret.maxHeight * ret.width;
 		return ret;
 	}
 };
+std::vector<ull> histogram;
+
+hRect Recursive(ull& _maxSquare, ull _start, ull _end) {
+	if (_start >= _end) {
+		hRect ret{};
+		ret.maxHeight = histogram[_start];
+		ret.width = 1;
+		ret.square = ret.maxHeight;
+		return ret;
+	}
+
+	//분할 결과마다 최댓값을 기록
+	ull mid = (_start + _end) / 2;
+	hRect left = Recursive(_maxSquare, _start, mid);
+	_maxSquare = std::max(_maxSquare, left.square);
+
+	hRect right = Recursive(_maxSquare, mid + 1, _end);
+	_maxSquare = std::max(_maxSquare, right.square);
+
+	//반환값: 좌, 우 히스토그램을 합친 부분에서 가장 큰 직사각형 넓이
+	return left + right;
+}
 
 int main() {	
 	std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
 	LOCAL_IO;
+	
+	while (true) {
+		ull input; std::cin >> input;
+		if (0 == input) { break; }
 
-	mat22 output{ 1,0,0,1 };
-	mat22 base{ 1,1,1,0 };
-	ull exp; std::cin >> exp;
-	if (exp == 0) { std::cout << 0; return 0; }
-	if (exp & 1) { output = base; }
+		histogram.resize(input);
+		for (ull i = 0; i < input; ++i) {
+			std::cin >> histogram[i];
+		}
 
-	for (ull i = 1; i < 64; ++i) {
-		ull bit = 1ull << i;
-		if (bit > exp) { break; }
+		ull maxSquare = 0;
+		hRect finalRect = Recursive(maxSquare, 0, histogram.size() - 1);
 
-		base = base * base;
-		
-		if (bit & exp) { output = output * base; }
+		//마지막 결과 비교
+		maxSquare = std::max(maxSquare, finalRect.square);
+		std::cout << maxSquare << '\n';
 	}
-	std::cout << output[0][1];
 
 	return 0;
 }
