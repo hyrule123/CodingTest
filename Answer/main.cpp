caa36cb92ed74024f4a0fd5fc3c222316fbd5d90
hw <ekdrn1@naver.com>
2024-06-21 22:10:57 +0900

    백준 11144 (피보나치 수 6)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -7,95 +7,51 @@
 #include <cstring>  //memset
 //////////////////
 
-struct matrix {
-	matrix() : r{}, c{}, size{}, elem{} {}
-	matrix(int _r, int _c) { 
-		r = _r; 
-		c = _c; 
-		size = r * c;
-		elem = new int[size];
-		memset(elem, 0, size * sizeof(int));
-	}
-	~matrix() { if (elem) { delete[] elem; } }
+//참고: 나무위키
+//행렬로 피보나치 계산하기
+//F2 F1		1 1
+//		== 
+//F1 F0		1 0
+//이 행렬 m을 거듭제곱 하면
+//m^n == F(n+1) F(n)
+//		 F(n)	F(n-1)
 
-	matrix(const matrix& _other) {
-		r = _other.r;
-		c = _other.c;
-		size = _other.size;
-		elem = new int[size];
-		memcpy(elem, _other.elem, size * sizeof(int));
-	}
+using ull = unsigned long long;
+struct mat22 {
+	ull m[4];
 
-	void operator =(matrix&& _move) noexcept {
-		if (this != &_move) {
-			Move(std::move(_move));
-		}
-	}
-	matrix(matrix&& _move) noexcept : matrix() { Move(std::move(_move)); }
-	inline void Move(matrix&& _move) noexcept {
-		r = _move.r;
-		c = _move.c;
-		size = _move.size;
-		std::swap(elem, _move.elem);
-	}
+	ull* operator [](ull _row) { if (_row) { _row = 2; } return &(m[_row]); }
+	mat22 operator *(const mat22& _other) const {
+		mat22 ret{};
 
-	int* operator[] (int _row) { return &(elem[_row * c]); }
-	matrix operator * (matrix& _other) {
-		matrix ret(r, _other.c);
-
-		for (int _r = 0; _r < ret.r; ++_r) {
-			int rIdx = _r * ret.c;
-			for (int _c = 0; _c < ret.c; ++_c) {
-				for (int _e = 0; _e < c; ++_e) {
-					(ret.elem[rIdx + _c] += (*this)[_r][_e] * _other[_e][_c]) %= 1000;
-				}
-			}
-		}
+		ret.m[0] = (m[0] * _other.m[0] + m[1] * _other.m[2]) % 1'000'000'007;
+		ret.m[1] = (m[0] * _other.m[1] + m[1] * _other.m[3]) % 1'000'000'007;
+		ret.m[2] = (m[2] * _other.m[0] + m[3] * _other.m[2]) % 1'000'000'007;
+		ret.m[3] = (m[2] * _other.m[1] + m[3] * _other.m[3]) % 1'000'000'007;
 
 		return ret;
 	}
-	matrix operator % (int _div) {
-		matrix ret = *this;
-		for (int i = 0; i < ret.size; ++i) {
-			ret.elem[i] %= _div;
-		}
-		return ret;
-	}
-
-	int r, c, size;
-	int* elem;
 };
-
-matrix PowRecursive(matrix& _base, size_t _exp) {
-	if (_exp == 1) { return _base % 1000; }
-
-	bool isOdd = (_exp % 2 == 1);
-	if (isOdd) { --_exp; }
-	matrix ret = PowRecursive(_base, _exp / 2);
-	ret = ret * ret;
-	if (isOdd) { ret = ret * _base; }
-
-	return ret;
-}
 
 int main() {	
 	std::cin.tie(nullptr); std::ios_base::sync_with_stdio(false);
 	LOCAL_IO;
 
-	int size; size_t exp; std::cin >> size >> exp;
-	matrix A(size, size); 
-	for (int i = 0; i < A.size; ++i) {
-		std::cin >> A.elem[i];
-	}
+	mat22 output{ 1,0,0,1 };
+	mat22 base{ 1,1,1,0 };
+	ull exp; std::cin >> exp;
+	if (exp == 0) { std::cout << 0; return 0; }
+	if (exp & 1) { output = base; }
 
-	matrix result = PowRecursive(A, exp);
+	for (ull i = 1; i < 64; ++i) {
+		ull bit = 1ull << i;
+		if (bit > exp) { break; }
 
-	for (int r = 0; r < result.r; ++r) {
-		for (int c = 0; c < result.c; ++c) {
-			std::cout << result[r][c] << ' ';
-		}
-		std::cout << '\n';
+		base = base * base;
+		
+		if (bit & exp) { output = output * base; }
 	}
+	std::cout << output[0][1];
 
 	return 0;
 }
