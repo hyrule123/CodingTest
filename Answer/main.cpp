8f7128df12e8f0a29bf51e1a503b8138832c5ad9
hw <ekdrn1@naver.com>
2024-06-28 00:21:59 +0900

    Red-Black tree 완성 + 참고용 코드(출처 geeksforgeeks)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -23,7 +23,7 @@ public:
 	class Node {
 		friend class RedBlackTree;
 	private:
-		Node() : parent{ nullptr }, left{ &nil }, right{ &nil }, data{}, color{ Color::Red } {}
+		Node() : parent{}, left{}, right{}, data{}, color{ Color::Red } {}
 		Node(int _data) 
 			: Node()
 		{
@@ -37,29 +37,71 @@ public:
 			, color(_other.color)
 		{}
 		~Node() {
-			if (left != NIL()) { delete left; }
-			if (right != NIL()) { delete right; }
+			if (left) { delete left; }
+			if (right) { delete right; }
 		}
-		
-		static Node* NIL() { return &nil; }
-		static bool IsNil(Node* _node) { return (&nil == _node); }
+
+		bool IsLeftChild() const { return this == parent->left; }
+		bool IsRightChild() const { return this == parent->right; }
+
+		Node* Uncle() {
+			if (nullptr == parent || nullptr == parent->parent) {
+				return nullptr;
+			}
+
+			if (parent->IsLeftChild()) {
+				return parent->parent->right;
+			}
+			else {
+				return parent->parent->left;
+			}
+		}
+
+		Node* Sibling() {
+			if (nullptr == parent) {
+				return nullptr;
+			}
+
+			if (IsLeftChild()) {
+				return parent->right;
+			}
+			else {
+				return parent->left;
+			}
+		}
+
+		//_parent의 자식으로 들어감
+		void MoveDown(Node* _parent) {
+			if (parent) {
+				if (IsLeftChild()) {
+					parent->left = _parent;
+				}
+				else {
+					parent->right = _parent;
+				}
+			}
+
+			_parent->parent = parent;
+			parent = _parent;
+		}
+
+		bool HasRedChild() const { 
+			return (left && left->color == Color::Red)
+				|| (right && right->color == Color::Red);
+		}
 
 		Node * parent, * left, * right;
-		int data;
-		Color color;
+		Color color; 
 
-		static Node nil;
+	public:
+		int data;
 	};
 
-	RedBlackTree() : root{ &Node::nil } { root->color = Color::Black; }
-	~RedBlackTree() {
-		if (root != Node::NIL()) {
-			delete root;
-		}
-	}
+	RedBlackTree() : root{} {}
+	~RedBlackTree() { if (root) { delete root; } }
 
 	void Insert(int _data);
-	void Delete(int _data);
+	void Erase(int _data);
 	void Traverse() const;
 	const Node* Search(int _data) const { return Search(_data); }
 	Node* Root() const { return root; }
@@ -67,15 +109,23 @@ public:
 private:
 	void LeftRotate(Node* _x);
 	void RightRotate(Node* _x);
-	void FixInsert(Node* _node);
-	Node* Search(int _data);
-	void _Delete(Node* _node);
+	void FixRedRed(Node* _node);
 
-	void TraverseRecursive(Node* _node) const;
+	//Deletion 작업 시 이 자리에 올 수 있는 Node를 찾아준다.
+	//후보: Left 노드로 한칸 가서 끝까지 오른쪽으로 이동한 리프노드
+	//, 또는 Right 노드로 한칸 가서 끝까지 왼쪽으로 간 리프노드(현재 채택)
+	Node* MinNode(Node* _node);
+	//삭제될 노드를 대체할 노드 탐색
+	Node* BSTReplace(Node* _node);
+	void Delete(Node* _node);			//recursive
+	void FixDoubleBlack(Node* _node);	//recursive
+
+	Node* Search(int _data);
+
+	void TraverseRecursive(Node* node) const;
 	
 	Node* root;
 };
-RedBlackTree::Node RedBlackTree::Node::nil{};
 
 void RedBlackTree::Insert(int _data)
 {
@@ -84,7 +134,7 @@ void RedBlackTree::Insert(int _data)
 	Node* current = root;
 
 	//리프노드까지 탐색 삽입(NIL이 나올때까지 내려간다)
-	while (current != Node::NIL()) {
+	while (current != nullptr) {
 		parent = current;
 		if (_data == current->data) {
 			return;
@@ -103,6 +153,7 @@ void RedBlackTree::Insert(int _data)
 	//부모노드가 없을 시 root, 아닐 시 하위 노드로 들어간다
 	if (parent == nullptr) {
 		root = newNode;
+		root->color = Color::Black;
 	}
 	else if (newNode->data < parent->data) {
 		parent->left = newNode;
@@ -111,26 +162,16 @@ void RedBlackTree::Insert(int _data)
 		parent->right = newNode;
 	}
 
-	//루트노드는 BLACK
-	if (nullptr == newNode->parent) {
-		newNode->color = Color::Black;
-		return;
-	}
-
-	//루트노드 밑에 삽입되었을 경우: 규칙이 유지되므로 return
-	if (nullptr == newNode->parent->parent) {
-		return;
-	}
-
 	//루트노드가 아닐 경우 레드블랙 트리 규칙을 유지
-	FixInsert(newNode);
+	FixRedRed(newNode);
 }
 
-void RedBlackTree::Delete(int _data)
+void RedBlackTree::Erase(int _data)
 {
 	Node* find = Search(_data);
-	if (nullptr == find) { return; }
-	_Delete(find);
+	if (find) {
+		Delete(find);
+	}
 }
 
 void RedBlackTree::Traverse() const
@@ -139,132 +180,288 @@ void RedBlackTree::Traverse() const
 	std::cout << '\n';
 }
 
-void RedBlackTree::LeftRotate(Node* _x)
+void RedBlackTree::LeftRotate(Node* _node)
 {
-	//반시계방향(왼쪽) 회전: 오른쪽 자식(y)을 현재 _x 자리로 올린다
-	Node* y = _x->right;
+	//반시계방향(왼쪽) 회전: 오른쪽 자식이 새 부모가 된다.
+	Node* nParent = _node->right;
 
-	//y의 자식 중 작은 노드를 x의 오른쪽으로 옮긴다(y의 자식은 무조건 x보다 크다)
-	_x->right = y->left;
-	if (Node::NIL() != _x->right) {
-		_x->right->parent = _x;
-	}
+	if (_node == root) { root = nParent; }
 
-	//부모 변경(없을 경우 root, 있을 경우 부모의 자식 x를 y로 변경
-	y->parent = _x->parent;
-	if (nullptr == y->parent) {
-		root = y;
-	}
-	else if (_x->parent->left == _x){
-		y->parent->left = y;
-	}
-	else {
-		y->parent->right = y;
-	}
-
-	y->left = _x;
-	_x->parent = y;
-}
-
-void RedBlackTree::RightRotate(Node* _x)
-{
-	Node* y = _x->left;
-	_x->left = y->right;
-	if (Node::NIL() != _x->left) {
-		_x->left->parent = _x;
-	}
-
-	y->parent = _x->parent;
-	if (nullptr == y->parent) {
-		root = y;
-	}
-	else if (y->parent->left == _x) {
-		y->parent->left = y;
-	}
-	else {
-		y->parent->right = y;
-	}
-
-	_x->parent = y;
-	y->right = _x;
-}
-
-
-void RedBlackTree::FixInsert(Node* _node)
-{
-	//_node는 삽입 규칙에 따라 Red 노드이다.
-
-	//부모 노드가 있는데 레드 노드일 경우: 규칙 위반(연속된 레드 노드 존재 불가)
-	while (_node != root && _node->parent->color == Color::Red) {
-		//Red 노드는 Black 부모가 반드시 존재하므로, 조부모 노드가 있다는 건 확실
-		
-		//부모 노드가 조부모의 left인지, right인지에 따라 다르게 처리
-		if (_node->parent == _node->parent->parent->left) {
-			Node* uncle = _node->parent->parent->right;
-
-			//삼촌이 red-> 규칙 위반이므로 변경
-			//부모+삼촌을 black으로, 조부모는 red로
-			if (uncle->color == Color::Red) {
-				_node->parent->color = Color::Black;
-				uncle->color = Color::Black;
-				uncle->parent->color = Color::Red;
-				
-				//이러면 조부모와 고조부모가 규칙 위반이 될 수 있으므로
-				//루프를 통해 다시 검사
-				_node = uncle->parent;
-			}
-
-			//삼촌이 Black(규칙 일치)
-			else {
-				//Node가 안쪽 자손일 경우(지그재그일 경우)
-				//부모 = 조부모 왼쪽(위에서 체크함), 나 = 부모 오른쪽
-				//부모를 기준으로 바꾸고, 왼쪽 회전시켜 부모를 바깥쪽 자식으로 만든다
-				//cf)부모 노드는 Red 상태
-				if (_node == _node->parent->right) {
-					_node = _node->parent;
-					LeftRotate(_node);
-				}
-
-				//Node가 바깥쪽 자손, Red
-				//Node 부모는 Black, 조부모는 Red가 되어야 한다.
-				//cf)새로 삽입된 노드는 형제 노드를 가지고 있지 않으므로(NIL) 
-				//	형제 노드가 어떻게 되는지는 고려 안해도 됨
-				_node->parent->color = Color::Black;
-				_node->parent->parent->color = Color::Red;
-				RightRotate(_node->parent->parent);
-			}
+	//Parent의 자식으로 들어간다
+	if (_node->parent) {
+		if (_node == _node->parent->left) {
+			_node->parent->left = nParent;
 		}
-		//위와 반대
 		else {
-			Node* uncle = _node->parent->parent->left;
-			if (uncle->color == Color::Red) {
-				_node->parent->color = Color::Black;
-				uncle->color = Color::Black;
-				uncle->parent->color = Color::Red;
-				_node = uncle->parent;
-			}
-			else {
-				if (_node == _node->parent->left) {
-					_node = _node->parent;
-					RightRotate(_node);
+			_node->parent->right = nParent;
+		}
+	}
+	nParent->parent = _node->parent;
+	_node->parent = nParent;
+
+	//  nP
+	//n   nPr
+	//  nr
+	_node->right = nParent->left;
+
+	if (_node->right) {
+		_node->right->parent = _node;
+	}
+
+	nParent->left = _node;
+}
+
+void RedBlackTree::RightRotate(Node* _node)
+{
+	Node* nParent = _node->left;
+
+	if (root == _node) { root = nParent; }
+
+	if (_node->parent) {
+		if (_node == _node->parent->left) {
+			_node->parent->left = nParent;
+		}
+		else {
+			_node->parent->right = nParent;
+		}
+	}
+	nParent->parent = _node->parent;
+	_node->parent = nParent;
+
+	_node->left = nParent->right;
+	if (_node->left) {
+		_node->left->parent = _node;
+	}
+
+	nParent->right = _node;
+}
+
+void RedBlackTree::FixRedRed(Node* _node)
+{
+	if (_node == root) { root->color = Color::Black; return; }
+
+	Node  * parent = _node->parent
+		, * grandParent = parent->parent
+		, * uncle = _node->Uncle();
+
+	//Red의 부모는 Red일 수 없다
+	if (parent->color == Color::Red) {
+		if (uncle && uncle->color == Color::Red) {
+			parent->color = Color::Black;
+			uncle->color = Color::Black;
+			grandParent->color = Color::Red;
+
+			//위쪽 노드에 대해 재귀 Red-Red 검사
+			FixRedRed(grandParent);
+		}
+		else {
+			if (parent->IsLeftChild()) {
+				//Outer child일 경우: 색상만 변경
+				if (_node->IsLeftChild()) {
+					std::swap(parent->color, grandParent->color);
 				}
-				_node->parent->color = Color::Black;
-				_node->parent->parent->color = Color::Red;
-				LeftRotate(_node->parent->parent);
+				//Inner child일 경우: parent 기준 왼쪽 회전, parent가 outer child가 되고 _node가 parent가 됨
+				else {
+					LeftRotate(parent);
+					std::swap(_node->color, grandParent->color);
+				}
+
+				//GrandParent를 기준으로 다시 오른쪽으로 회전시킨다
+				//GrandParent 자리에는 _node 또는 parent가 오게 된다.
+				RightRotate(grandParent);
+			}
+			//좌우 반전
+			else {
+				if (_node->IsRightChild()) {
+					std::swap(parent->color, grandParent->color);
+				}
+				else {
+					RightRotate(parent);
+					std::swap(_node->color, grandParent->color);
+				}
+				LeftRotate(grandParent);
 			}
 		}
 	}
-	//루트노드 무조건 Black
-	root->color = Color::Black;
+}
+
+
+RedBlackTree::Node* RedBlackTree::MinNode(Node* _node)
+{
+	while (nullptr != _node->left) {
+		_node = _node->left;
+	}
+	return _node;
+}
+
+RedBlackTree::Node* RedBlackTree::BSTReplace(Node* _node)
+{
+	//자식 둘다 있을 경우
+	if (_node->left && _node->right) {
+		return MinNode(_node->right);
+	}
+
+	//둘중 하나 있을 경우
+	else if (_node->left) {
+		return _node->left;
+	}
+	else if (_node->right) {
+		return _node->right;
+	}
+
+	//둘다 없을경우(Leaf)
+	return nullptr;
+}
+
+void RedBlackTree::Delete(Node* _node)
+{
+	Node* replace = BSTReplace(_node);
+
+	//삭제 대상 + 교체 대상 모두 black인 경우를 Double Black condition이라고 한다.
+	//nullptr은 NIL 노드이고, NIL 노드는 항상 Black이다
+	bool isDoubleBlack = (_node->color == Color::Black) && (nullptr == replace || replace->color == Color::Black);
+
+	//replace == NIL
+	if (nullptr == replace) {
+		if (root == _node) { root = nullptr; }
+		else{
+			if (isDoubleBlack) {
+				FixDoubleBlack(_node);
+			}
+			//Double Black이 아닌 경우(둘중 하나는 Red인 경우)
+			else {
+				Node* sib = _node->Sibling();
+				if (sib) {
+					sib->color = Color::Red;
+				}
+			}
+
+			if (_node->IsLeftChild()) { _node->parent->left = nullptr; }
+			else { _node->parent->right = nullptr; }
+		}
+
+		_node->left = _node->right = nullptr;
+		delete _node;
+	}
+
+	//replace 있음 + 자녀가 1개뿐일 때
+	else if (_node->left == nullptr || _node->right == nullptr) {
+
+		//노드가 루트일 경우 replace의 값만 끌올
+		if (_node == root) {
+			root->data = replace->data;
+			root->left = root->right = nullptr;
+			delete replace;
+		}
+		else {
+			if (_node->IsLeftChild()) {
+				_node->parent->left = replace;
+			}
+			else {
+				_node->parent->right = replace;
+			}
+			replace->parent = _node->parent;
+			if (isDoubleBlack) {
+				FixDoubleBlack(replace);
+			}
+			else {
+				replace->color = Color::Black;
+			}
+
+			_node->left = _node->right = nullptr;
+			delete _node;
+		}
+	}
+	//자식을 2개 가지고 있을 경우 replace의 값을 가져온뒤, replace를 제거(재귀 호출)
+	else {
+		std::swap(_node->data, replace->data);
+		Delete(replace);
+	}
+}
+
+void RedBlackTree::FixDoubleBlack(Node* _node)
+{
+	if (root == _node) { return; }
+
+	Node* sibling = _node->Sibling(), * parent = _node->parent;
+
+	//검사할 자매 노드 없음 -> 부모 노드로 재귀 호출
+	if(nullptr == sibling) { 
+		FixDoubleBlack(_node->parent);
+	}
+	else {
+		if (sibling->color == Color::Red) {
+			parent->color = Color::Red;
+			sibling->color = Color::Black;
+
+			//sibling을 부모 노드 위치로 옮겨준다(== 알맞게 회전시킨다)
+			if (sibling->IsLeftChild()) {
+				RightRotate(parent);
+			}
+			else {
+				LeftRotate(parent);
+			}
+
+			//Sibling Right 또는 Left를 새로운 Sibling으로 해서 검사한다.
+			//	    P	    SIB(B)
+			//	 /    \	     	\
+			//	SIB(R) X(B)      P(R)
+			//   \			    / \
+			//   SIBR	      SIBR X(B)
+			FixDoubleBlack(_node);
+		}
+		//Sibling == Black
+		else {
+			if (sibling->HasRedChild()) {
+				if (sibling->left && sibling->left->color == Color::Red) {
+					//sibling->left가 outer child일 경우(left left)
+					if (sibling->IsLeftChild()) {
+						sibling->left->color = sibling->color;
+						sibling->color = parent->color;
+						RightRotate(parent);//sibling이 parent 자리로 오게됨, sibling Left가 sibling 자리로 옴
+					}
+					//inner child
+					else {
+						sibling->left->color = parent->color;
+						RightRotate(sibling);//sibling의 왼쪽 자식을 끌어온다.
+						LeftRotate(parent);//parent를 왼쪽으로 돌리면 sibling의 왼쪽 자식이 parent 위치에 옴
+					}
+				}
+				//좌우반전
+				else {
+					//outer child case
+					if (sibling->IsRightChild()) {
+						sibling->right->color = sibling->color;
+						sibling->color = parent->color;
+						LeftRotate(parent);
+					}
+					else {
+						sibling->right->color = parent->color;
+						LeftRotate(sibling);
+						RightRotate(parent);
+					}
+				}
+				parent->color = Color::Black;
+			}
+			//2개의 Black child->자신이 Red가 되면 됨
+			else {
+				sibling->color = Color::Red;
+				if (parent->color == Color::Black) {
+					FixDoubleBlack(parent);
+				}
+				else {
+					parent->color = Color::Black;
+				}
+			}
+		}
+	}
 }
 
 RedBlackTree::Node* RedBlackTree::Search(int _data)
 {
 	Node* node = root;
-	while (node != Node::NIL()) {
-		if (node->data == _data) {
-			return node;
-		}
+	while (node != nullptr) {
+		if (node->data == _data) { break; }
 		else if (_data < node->data) {
 			node = node->left;
 		}
@@ -272,87 +469,15 @@ RedBlackTree::Node* RedBlackTree::Search(int _data)
 			node = node->right;
 		}
 	}
-
-	if (node == Node::NIL()) {
-		node = nullptr;
-	}
-
 	return node;
 }
 
-void RedBlackTree::_Delete(Node* _node)
+void RedBlackTree::TraverseRecursive(Node* node) const
 {
-	bool isLeft = (_node->left != Node::NIL());
-	bool isRight = (_node->right != Node::NIL());
-
-	if (isLeft && isRight) {
-		Node* suc = _node;
-		Node* sucChild = _node->right;
-		while (sucChild != Node::NIL()) {
-			suc = sucChild;
-			sucChild = suc->left;
-		}
-
-		if (suc != _node) {
-			_node->data = suc->data;
-			
-			if(suc == suc->parent->left) {
-				suc->parent->left = Node::NIL();
-			}
-			else {
-				suc->parent->right = Node::NIL();
-			}
-
-			delete suc;
-		}
-	}
-	else if (isLeft) {
-		if (_node->parent) {
-			if (_node->parent->left == _node) {
-				_node->parent->left = _node->left;
-			}
-			else {
-				_node->parent->right = _node->left;
-			}
-		}
-		
-		_node->left->parent = _node->parent;
-		_node->left = Node::NIL();
-
-		delete _node;
-	}
-	else if (isRight) {
-		if (_node->parent) {
-			if (_node->parent->left == _node) {
-				_node->parent->left = _node->right;
-			}
-			else {
-				_node->parent->right = _node->right;
-			}
-		}
-
-		_node->right->parent = _node->parent;
-		_node->right = Node::NIL();
-
-		delete _node;
-	}
-	else {
-		if (_node->parent->left == _node) {
-			_node->parent->left = Node::NIL();
-		}
-		else {
-			_node->parent->right = Node::NIL();
-		}
-		delete _node;
-	}
-}
-
-void RedBlackTree::TraverseRecursive(Node* _node) const
-{
-	if (_node == Node::NIL()) { return; }
-	TraverseRecursive(_node->left);
-	std::cout << _node->data << ' ';
-	TraverseRecursive(_node->right);
+	if (node == nullptr) { return; }
+	TraverseRecursive(node->left);
+	std::cout << node->data << ' ';
+	TraverseRecursive(node->right);
 }
 
 int main() {
@@ -360,14 +485,24 @@ int main() {
 	LOCAL_IO;
 
 	RedBlackTree rbtree;
-	rbtree.Insert(1);
-	rbtree.Insert(2);
+	rbtree.Insert(7);
 	rbtree.Insert(3);
-	rbtree.Insert(4);
-	rbtree.Insert(5);
+	rbtree.Insert(18);
+	rbtree.Insert(10);
+	rbtree.Insert(22);
+	rbtree.Insert(8);
+	rbtree.Insert(11);
+	rbtree.Insert(26);
+	rbtree.Insert(2);
 	rbtree.Insert(6);
+	rbtree.Insert(13);
+	rbtree.Traverse();
 
-	rbtree.Delete(1);
+	rbtree.Erase(18);
+	rbtree.Erase(11);
+	rbtree.Erase(3);
+	rbtree.Erase(10);
+	rbtree.Erase(22);
 
 	rbtree.Traverse();
 
