dd41d62b4742ca0ff20dd5e6d17e4aed157f2730
hw <ekdrn1@naver.com>
2024-07-02 10:53:39 +0900

    백준 2110 (공유기 설치)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -10,39 +10,82 @@
 #include <vector>
 using uint = unsigned int;
 
+std::vector<uint> dists;
+std::vector<uint> temp;
+void MergeSort(uint _start, uint _end) {
+	if (_start >= _end) { return; }
+
+	uint mid = (_start + _end) / 2;
+	MergeSort(_start, mid);
+	MergeSort(mid + 1, _end);
+
+	uint l = _start;
+	uint r = mid + 1;
+	uint i = _start;
+	while (l <= mid && r <= _end) {
+		if (dists[l] < dists[r]) {
+			temp[i] = dists[l]; ++l;
+		}
+		else {
+			temp[i] = dists[r]; ++r;
+		}
+		++i;
+	}
+	for (; l <= mid; ++l) {
+		temp[i] = dists[l];
+		++i;
+	}
+	for (; r <= _end; ++r) {
+		temp[i] = dists[r];
+		++i;
+	}
+
+	memcpy(dists.data() + _start, temp.data() + _start, sizeof(uint) * (_end - _start + 1));
+}
+
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 	
-	uint N, M; std::cin >> N >> M;
-	std::vector<uint> trees(N);
-	uint start = 0, end{};	//톱의 최소 높이=0
+	uint N, C; std::cin >> N >> C;
+	dists.resize(N); temp.resize(N);
 	for (uint i = 0; i < N; ++i) {
-		std::cin >> trees[i];
-		if (end < trees[i]) { end = trees[i]; }
+		std::cin >> dists[i];
 	}
-	++end;
+	MergeSort(0, (uint)dists.size() - 1);
 
-	while (start + 1 < end) {
-		uint mid = (start + end) / 2;
-		uint height = 0;
+	//최대거리 구하고
+	uint minDist = 1, maxDist = dists.back() - dists.front() + 1;
+
+	//배열 안의 값들을 두 집 사이의 거리로 변경
+	for (uint i = 0; i < N - 1; ++i) {
+		dists[i] = dists[i + 1] - dists[i];
+	}
+	--N; dists.resize(N);
+	
+	while (minDist + 1 < maxDist) {
+		uint mid = (minDist + maxDist) / 2;
+
+		uint count = 1;	//첫 집에는 무조건 공유기를 설치해야 최대거리를 뽑을수 있다
+		uint sum = 0;
+
 		for (uint i = 0; i < N; ++i) {
-			//톱의 높이보다 나무가 길 경우에만 자를수 있다
-			if (mid < trees[i]) {
-				height += (trees[i] - mid);
+			sum += dists[i];
+			//이번 루프에서 요구하는 거리 이상이 되면 sum을 초기화하고 count를 올려준다(공유기를 설치한다)
+			if (mid <= sum) {
+				sum = 0;
+				++count;
 			}
-			//루프마다 종료조건 검사 안하면 오버플로우가 발생할 수 있다
-			if (M < height) { break; }
 		}
 
-		if (M <= height) {
-			start = mid;
+		if (C <= count) {
+			minDist = mid;
 		}
 		else {
-			end = mid;
+			maxDist = mid;
 		}
 	}
-	std::cout << start;
+	std::cout << minDist;
 
 	return 0;
 }
