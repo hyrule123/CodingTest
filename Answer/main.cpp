11c96e33b77d291613dfe949928922512c364115
hw <ekdrn1@naver.com>
2024-07-03 23:30:53 +0900

    백준 1927 (최소 힙)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -16,10 +16,10 @@ void HeapifyDown(size_t _idx, size_t _size) {
 		size_t left = _idx * 2;
 		size_t right = left + 1;
 
-		if (left < _size && heap[biggest] < heap[left]) {
+		if (left < _size && heap[biggest] > heap[left]) {
 			biggest = left;
 		}
-		if (right < _size && heap[biggest] < heap[right]) {
+		if (right < _size && heap[biggest] > heap[right]) {
 			biggest = right;
 		}
 
@@ -39,7 +39,7 @@ void InsertHeap(int _i) {
 	while (idx > 0) {
 		size_t parent = idx / 2;
 
-		if (heap[idx] > heap[parent]) {
+		if (heap[idx] < heap[parent]) {
 			std::swap(heap[idx], heap[parent]);
 		}
 		idx = parent;
@@ -49,21 +49,21 @@ void InsertHeap(int _i) {
 int PopHeap() {
 	int ret = 0;
 	if (heap.empty()) { return ret; }
-	
+
 	ret = heap[0];
 	heap[0] = heap.back();
 	heap.resize(heap.size() - 1);
 	if (1 < heap.size()) {
 		HeapifyDown(0, heap.size());
 	}
-	
+
 	return ret;
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
-	
+
 	int N; std::cin >> N;
 	heap.reserve(N);
 	for (int i = 0; i < N; ++i) {
