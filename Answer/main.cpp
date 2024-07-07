0982527a066cbab4574c895ed5fa3311caae947c
hw <ekdrn1@naver.com>
2024-07-07 23:26:50 +0900

    백준 1520 (내리막 길)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -7,36 +7,48 @@
 #include <cstring>  //memset
 //////////////////
 
-#include <vector>
-struct mat { int r, c; };
+#include <array>
+using uint = unsigned int;
+enum dir { left, up, right, down };
+
+uint N, M;
+uint path[502][502];
+uint memo[502][502];	//memo: x, y에서 N, M까지 갈 수 있는 방법의 수
+
+uint Recursive(uint x, uint y) {
+
+	//도착했을 경우 1을 return
+	if (x == N && y == M) { return 1; }
+
+	//아직 지나가지 않은 길일 경우 탐색
+	else if (0xff'ff'ff'ff == memo[x][y]) {
+		memo[x][y] = 0;
+
+		if (path[x - 1][y] < path[x][y]) { memo[x][y] += Recursive(x - 1, y); }
+		if (path[x][y - 1] < path[x][y]) { memo[x][y] += Recursive(x, y - 1); }
+		if (path[x + 1][y] < path[x][y]) { memo[x][y] += Recursive(x + 1, y); }
+		if (path[x][y + 1] < path[x][y]) { memo[x][y] += Recursive(x, y + 1); }
+	}
+
+	//이미 지나간 길일 경우 memo를 반환
+	return memo[x][y];
+}
+
+
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	int N; std::cin >> N;
-	std::vector<mat> input(N);
-	std::vector<std::vector<int>> dp(N, std::vector<int>(N));
-	std::cin >> input[0].r >> input[0].c;
-	for (int i = 1; i < N; ++i) {
-		std::cin >> input[i].r >> input[i].c;
-	}
-
-	for (int range = 1; range < N; ++range) {
-		for (int r = 0; r + range < N; ++r) {
-			int c = r + range;
-			dp[r][c] = std::numeric_limits<int>::max();
-
-			for (int pivot = r; pivot < c; ++pivot) {
-				dp[r][c] = std::min(dp[r][c],
-					dp[r][pivot] + dp[pivot + 1][c]
-
-					//이번 dp 경우의 수의 연산 횟수
-					+ input[r].r * input[pivot].c * input[c].c
-				);
-			}
+	std::cin >> N >> M;
+	memset(path, 0xff, sizeof(uint) * 502 * 502);
+	memset(memo, 0xff, sizeof(uint) * 502 * 502);
+	for (uint r = 1; r <= N; ++r) {
+		for (uint c = 1; c <= M; ++c) {
+			std::cin >> path[r][c];
 		}
 	}
-	std::cout << dp[0][N - 1];
+
+	std::cout << Recursive(1, 1);
 
 	return 0;
 }
