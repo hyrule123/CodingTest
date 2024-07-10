22c670502d91a1a578a1de14ce92ae203cccba84
hw <ekdrn1@naver.com>
2024-07-10 21:47:16 +0900

    백준 11066 (파일 합치기) - 복습

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -7,51 +7,48 @@
 #include <cstring>  //memset
 //////////////////
 
-//이번 앱을 끄거나 켜거나
+//11066 (파일 합치기) - 복습
 #include <vector>
-struct app { int mem, cost; };
-std::vector<app> apps;
-//i = i번째 앱까지 , j: 제거 비용, val: 제거되는 최대 메모리
-std::vector<std::vector<int>> dp;
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	int N, M; std::cin >> N >> M;
-	apps.resize(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		std::cin >> apps[i].mem;
-	}
-	int maxCost = 0;
-	for (int i = 1; i <= N ; ++i) {
-		std::cin >> apps[i].cost;
-		maxCost += apps[i].cost;
-	}
+	int T; std::cin >> T;
+	std::vector<int> f_pSum;	//파일 용량의 구간합
+	std::vector<std::vector<int>> dp;	//dp[i][j] i부터 j까지 합쳤을 때의 최소비용
+	while (T--) {
+		int K; std::cin >> K;
+		//file_size_max = 10000, file_count_max = 500 -> 5000000(int로 커버 가능)
+		f_pSum.clear(); f_pSum.resize(K + 1);
+		for (int i = 1; i <= K; ++i) {
+			int input; std::cin >> input;
+			f_pSum[i] = f_pSum[i - 1] + input;
+		}
 
-	int minCost = std::numeric_limits<int>::max();
-	//최악의 경우 최대 비용은 100 * 100 = 10000
-	dp.resize(N + 1, std::vector<int>(maxCost + 1));
-	for (int i = 1; i <= N; ++i) {
+		dp.clear();
+		dp.resize(K + 1, std::vector<int>(K + 1));
 
-		for (int j = 0; j <= maxCost; ++j) {
-			//이번 앱을 제거하지 않았을 때
-			int exclude = dp[i - 1][j];
+		//[i][i+1] 부터 [i][K]까지 범위를 1씩 늘려가며 탐색한다.
+		for (int range = 1; range <= K; ++range) {
+			for (int i = 1; i + range <= K; ++i) {
+				int j = i + range;
 
-			//이번 앱을 제거했을 때: [j - (이번앱 cost)].mem + (이번앱 mem)
-			int include = 0;
-			if (apps[i].cost <= j) {
-				include = dp[i - 1][j - apps[i].cost] + apps[i].mem;
-			}
+				dp[i][j] = std::numeric_limits<int>::max();
 
-			dp[i][j] = std::max(exclude, include);
-			if (M <= dp[i][j]) {
-				minCost = std::min(minCost, j);
+				//pivot을 잡고, 좌/우로 한칸씩 이동하며 각각의 비용 중 최소값을 가져온다.
+				for (int pivot = i; pivot < j; ++pivot) {
+					dp[i][j] = std::min(dp[i][j], dp[i][pivot] + dp[pivot + 1][j]);
+				}
+
+				//이번 파일들을 마지막 하나로 합치는 비용을 추가한다.(이건 언제나 동일함)
+				dp[i][j] += f_pSum[j] - f_pSum[i - 1];
 			}
 		}
-	}
-	std::cout << minCost;
 
+		//파일 1번부터 K번까지를 합치는 비용은 dp[1][K]
+		std::cout << dp[1][K] << '\n';
+	}
 
 	return 0;
 }
