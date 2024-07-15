fd5315678fa5e382960be4aa76076d10650542b0
hw <ekdrn1@naver.com>
2024-07-15 22:14:02 +0900

    백준 2178 (미로 탐색)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -4,84 +4,119 @@
 #include <iostream>
 #include <cstring> //memset
 /*
-백준 2667 (단지번호붙이기)
+백준 2178 (미로 탐색)
 */
-using uint = unsigned int;
-//테두리에 0으로 둘러줄예정 + 2, 비트플래그 사용
-uint bits[27];
-char input[26];
-//체스판처럼 101010일 경우 -> 최대 313개가 나옴
-uint ans[320], ansIdx = 0;
+struct coord { int x, y; };
+struct CircleQueue {
+	bool Empty() { return size == 0; }
+	bool Full() { return capacity == size + 1; }
+	int Next(int i) { if (++i >= capacity) { i -= capacity; } return i; }
+	void Reserve(int cap) {
+		if (++cap <= capacity) { return; }
 
-void DFS(uint r, uint c) {
-	if (0u == (1u & bits[r] >> c)) { return; }
+		coord* temp = new coord[cap];
+		memset(temp, 0, sizeof(coord) * cap);
+		if (cont) {
+			if (end < start) {
+				int frontSize = capacity - start;
+				memcpy(temp, cont + start, sizeof(coord) * frontSize);
+				memcpy(temp + frontSize, cont, sizeof(coord) * end);
+			}
+			else {
+				memcpy(temp, cont + start, sizeof(coord) * (end - start + 1));
+			}
+			start = 0;
+			end = size;
 
-	//방문한곳은 플래그를 꺼준다
-	bits[r] &= ~(1u << c);
-	++(ans[ansIdx]);
-
-	DFS(r - 1, c);
-	DFS(r + 1, c);
-	DFS(r, c - 1);
-	DFS(r, c + 1);
-}
-
-uint Partition(uint s, uint e) {
-	uint pivot = ans[s];
-	uint i = s - 1, j = e + 1;
-
-	while (true) {
-		do { ++i; } while (ans[i] < pivot);
-		do { --j; } while (ans[j] > pivot);
-
-		if (i >= j) {
-			return j;
+			delete[] cont;
+		}
+		cont = temp;
+		capacity = cap;
+	}
+	void PushBack(int x, int y) {
+		if (Full()) {
+			Reserve(size * 2);
 		}
 
-		std::swap(ans[i], ans[j]);
+		cont[end] = { x, y };
+		end = Next(end);
+		++size;
 	}
+	void PopFront() { start = Next(start); --size; }
+	coord Front() { return cont[start]; }
+ 
+	coord* cont{};
+	int size{}, capacity{}, start{}, end{};
+} q;
+
+#include <array>
+#define WALL -1
+#define PATH -2
+//길 주변에 벽을 둘러줄예정 -> 둘레를 1씩 추가한다
+using Map = std::array<std::array<int, 102>, 102>;
+constexpr Map Fill() {
+	Map m;
+	for (int i = 0; i < 102; ++i) {
+		m[i].fill(WALL);
+	}
+	return m;
 }
+//기본 WALL(-1)로 채워놓는다
+Map map = Fill();
+int N, M;
 
-void QuickSort(uint s, uint e) {
-	if (s >= e) { return; }
+int BFS() {
+	//1,1부터 시작이므로 0
+	map[1][1] = 0;
+	q.PushBack(1, 1);
 
-	uint pivot = Partition(s, e);
+	int ret = 0;
+	while (false == q.Empty()) {
+		coord c = q.Front(); q.PopFront();
 
-	QuickSort(s, pivot);
-	QuickSort(pivot + 1, e);
+		//도착했을 경우 거리를 반환
+		if (c.x == N && c.y == M) { 
+			ret = map[c.x][c.y] + 1; break; 
+		}
+		
+		//지나갈 수 있는 길일 경우 그 자리에 여태까지 걸어온 거리를 기록
+		if (map[c.x][c.y + 1] == PATH) {
+			map[c.x][c.y + 1] = map[c.x][c.y] + 1;
+			q.PushBack(c.x, c.y + 1);
+		}
+		if (map[c.x + 1][c.y] == PATH) {
+			map[c.x + 1][c.y] = map[c.x][c.y] + 1;
+			q.PushBack(c.x + 1, c.y);
+		}
+		if (map[c.x][c.y - 1] == PATH) {
+			map[c.x][c.y - 1] = map[c.x][c.y] + 1;
+			q.PushBack(c.x, c.y - 1);
+		}
+		if (map[c.x - 1][c.y] == PATH) {
+			map[c.x - 1][c.y] = map[c.x][c.y] + 1;
+			q.PushBack(c.x - 1, c.y);
+		}
+	}
 
+	return ret;
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	uint side; std::cin >> side;
-	for (uint i = 1; i <= side; ++i) {
-		std::cin >> input;
+	std::cin >> N >> M;
 
-		//비트플래그 형태로 넣어준다, 역순으로 들어감
-		for (uint j = 1; j <= side; ++j) {
-			if (input[j - 1] == '1') {
-				bits[i] += 1u << j;
-			}
+	for (int r = 1; r <= N; ++r) {
+		char input[101]; std::cin >> input;
+		for (int c = 1; c <= M; ++c) {
+			//지나갈수 있는 길은 PATH(-2)로 채워준다
+			if (input[c - 1] == '1') { map[r][c] = PATH; }
 		}
 	}
 
-	for (uint i = 1; i <= side; ++i) {
-		for (uint j = 1; j <= side; ++j) {
-			if (1u & bits[i] >> j) {
-				DFS(i, j);
-				++ansIdx;
-			}
-		}
-	}
-
-	QuickSort(0, ansIdx - 1);
-	std::cout << ansIdx << '\n';
-	for (uint i = 0; i < ansIdx; ++i) {
-		std::cout << ans[i] << '\n';
-	}
+	q.Reserve(1000);
+	std::cout << BFS();
 
 	return 0;
 }
