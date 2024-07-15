bf0a4987cde5aac08793a2f4c03443f33b2ae8f6
hw <ekdrn1@naver.com>
2024-07-15 16:13:40 +0900

    백준 1260 (DFS와 BFS)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -4,18 +4,46 @@
 #include <iostream>
 #include <cstring> //memset
 /*
-백준 2606 (바이러스)
+백준 1260 (DFS와 BFS)
 */
-#include <bitset>
-std::bitset<101> links[101], visited;
-int nNode, nLink, infectedCount = -1;
-void DFS(int node) {
-	visited[node] = true;
-	++infectedCount;
+struct cq {
+	bool Empty() { return size == 0; }
+	int Size() { return size; }
+	int Next(int i) { if (++i >= 1001) { i -= 1001; } return i; }
+	void PushBack(int i) { cont[end] = i; end = Next(end); ++size; }
+	void PopFront() { start = Next(start); --size; }
+	int Front() { return cont[start]; }
 
-	for (int i = 1; i <= nNode; ++i) {
-		if (links[node][i] && false == visited[i]) {
+	int cont[1001]{}, size{}, start{}, end{};
+} bfsQ;
+
+#include <bitset>
+std::bitset<1001> links[1001], visited;
+int N, M, V;
+
+void DFS(int v) {
+	visited[v] = true;
+	std::cout << v << ' ';
+
+	for (int i = 1; i <= N; ++i) {
+		if (links[v][i] && false == visited[i]) {
 			DFS(i);
+		}
+	}
+}
+void BFS(int v) {
+	bfsQ.PushBack(V);
+	visited[V] = true;
+
+	while (false == bfsQ.Empty()) {
+		int front = bfsQ.Front(); bfsQ.PopFront();
+		std::cout << front << ' ';
+
+		for (int i = 1; i <= N; ++i) {
+			if (links[front][i] && false == visited[i]) {
+				visited[i] = true;
+				bfsQ.PushBack(i);
+			}
 		}
 	}
 }
@@ -24,14 +52,16 @@ int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	std::cin >> nNode >> nLink;
-	for (int i = 0; i < nLink; ++i) {
+	std::cin >> N >> M >> V;
+	for (int i = 0; i < M; ++i) {
 		int a, b; std::cin >> a >> b;
 		links[a][b] = true;
 		links[b][a] = true;
 	}
-	DFS(1);
-	std::cout << infectedCount;
+	DFS(V);
+	visited.reset();
+	std::cout << '\n';
+	BFS(V);
 
 	return 0;
 }
