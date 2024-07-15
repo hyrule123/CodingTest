3b4055ce932d628eae8adace6e0f67f8b8bb9bc1
hw <ekdrn1@naver.com>
2024-07-15 18:03:58 +0900

    백준 2667 (단지번호붙이기)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -4,64 +4,84 @@
 #include <iostream>
 #include <cstring> //memset
 /*
-백준 1260 (DFS와 BFS)
+백준 2667 (단지번호붙이기)
 */
-struct cq {
-	bool Empty() { return size == 0; }
-	int Size() { return size; }
-	int Next(int i) { if (++i >= 1001) { i -= 1001; } return i; }
-	void PushBack(int i) { cont[end] = i; end = Next(end); ++size; }
-	void PopFront() { start = Next(start); --size; }
-	int Front() { return cont[start]; }
+using uint = unsigned int;
+//테두리에 0으로 둘러줄예정 + 2, 비트플래그 사용
+uint bits[27];
+char input[26];
+//체스판처럼 101010일 경우 -> 최대 313개가 나옴
+uint ans[320], ansIdx = 0;
 
-	int cont[1001]{}, size{}, start{}, end{};
-} bfsQ;
+void DFS(uint r, uint c) {
+	if (0u == (1u & bits[r] >> c)) { return; }
 
-#include <bitset>
-std::bitset<1001> links[1001], visited;
-int N, M, V;
+	//방문한곳은 플래그를 꺼준다
+	bits[r] &= ~(1u << c);
+	++(ans[ansIdx]);
 
-void DFS(int v) {
-	visited[v] = true;
-	std::cout << v << ' ';
+	DFS(r - 1, c);
+	DFS(r + 1, c);
+	DFS(r, c - 1);
+	DFS(r, c + 1);
+}
 
-	for (int i = 1; i <= N; ++i) {
-		if (links[v][i] && false == visited[i]) {
-			DFS(i);
+uint Partition(uint s, uint e) {
+	uint pivot = ans[s];
+	uint i = s - 1, j = e + 1;
+
+	while (true) {
+		do { ++i; } while (ans[i] < pivot);
+		do { --j; } while (ans[j] > pivot);
+
+		if (i >= j) {
+			return j;
 		}
+
+		std::swap(ans[i], ans[j]);
 	}
 }
-void BFS(int v) {
-	bfsQ.PushBack(V);
-	visited[V] = true;
 
-	while (false == bfsQ.Empty()) {
-		int front = bfsQ.Front(); bfsQ.PopFront();
-		std::cout << front << ' ';
+void QuickSort(uint s, uint e) {
+	if (s >= e) { return; }
 
-		for (int i = 1; i <= N; ++i) {
-			if (links[front][i] && false == visited[i]) {
-				visited[i] = true;
-				bfsQ.PushBack(i);
-			}
-		}
-	}
+	uint pivot = Partition(s, e);
+
+	QuickSort(s, pivot);
+	QuickSort(pivot + 1, e);
+
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	std::cin >> N >> M >> V;
-	for (int i = 0; i < M; ++i) {
-		int a, b; std::cin >> a >> b;
-		links[a][b] = true;
-		links[b][a] = true;
+	uint side; std::cin >> side;
+	for (uint i = 1; i <= side; ++i) {
+		std::cin >> input;
+
+		//비트플래그 형태로 넣어준다, 역순으로 들어감
+		for (uint j = 1; j <= side; ++j) {
+			if (input[j - 1] == '1') {
+				bits[i] += 1u << j;
+			}
+		}
 	}
-	DFS(V);
-	visited.reset();
-	std::cout << '\n';
-	BFS(V);
+
+	for (uint i = 1; i <= side; ++i) {
+		for (uint j = 1; j <= side; ++j) {
+			if (1u & bits[i] >> j) {
+				DFS(i, j);
+				++ansIdx;
+			}
+		}
+	}
+
+	QuickSort(0, ansIdx - 1);
+	std::cout << ansIdx << '\n';
+	for (uint i = 0; i < ansIdx; ++i) {
+		std::cout << ans[i] << '\n';
+	}
 
 	return 0;
 }
