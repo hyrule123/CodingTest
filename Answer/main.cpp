7927d3f31e4e6d3a2abe36289fc6f7f1f858a3d7
hw <ekdrn1@naver.com>
2024-07-17 12:56:18 +0900

    백준 1697 (숨바꼭질)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -4,67 +4,62 @@
 #include <iostream>
 #include <cstring> //memset
 /*
-백준 1012 (유기농 배추) - stack 활용
+백준 1697 (숨바꼭질)
 */
-int TC, N, M, K;
-#include <bitset>
-std::bitset<52> farm[52];
-struct coord { int r, c; };
-struct stack {
-	coord c[500]{};
-	int size{};
+struct CircleQueue {
 
 	bool Empty() { return size == 0; }
-	coord Pop() { --size; return c[size]; }
-	void Push(coord _coord) { c[size++] = _coord; }
-} stk;
+	int Next(int cur) { if (++cur >= cap) { cur -= cap; } return cur; }
 
-void CheckPos(int r, int c) {
-	if (farm[r][c]) {
-		farm[r][c] = false;
-		stk.Push({ r,c });
+	void PushBack(int i) { cont[end] = i; end = Next(end); ++size; }
+	int PopFront() { int ret = cont[start]; start = Next(start); --size; return ret; }
+
+	static constexpr int cap = 50000;
+	int cont[cap]{}, size{}, start{}, end{};
+} q;
+int N, K, K2;
+#include <array>
+using arr = std::array<int, 100001>;
+constexpr arr Fill() { arr a; a.fill(-1); return a; }
+arr arriveTime = Fill();
+int BFS() {
+	arriveTime[N] = 0;
+	q.PushBack(N);
+
+	int ret = 0;
+	while (false == q.Empty()) {
+		int cur = q.PopFront();
+		if (cur == K) { 
+			ret = arriveTime[cur]; break; }
+
+		int next = cur * 2;
+		if (next <= K2 && arriveTime[next] == -1) { 
+			arriveTime[next] = arriveTime[cur] + 1;
+			q.PushBack(next); 
+		}
+		next = cur + 1;
+		if (next <= K && arriveTime[next] == -1) { 
+			arriveTime[next] = arriveTime[cur] + 1;
+			q.PushBack(next); 
+		}
+		next = cur - 1;
+		if (0 <= next && arriveTime[next] == -1) { 
+			arriveTime[next] = arriveTime[cur] + 1;
+			q.PushBack(next); 
+		}
 	}
-}
 
-void DFS(int r, int c) {
-	CheckPos(r, c);
-
-	//상좌하우 순으로 stack에 넣는다(꺼낼때는 우하좌상 순으로 꺼내짐)
-	//재귀 형식일때는 stack에서 뽑을 때 지나갔다고 표시하지만
-	//스택일때는 먼저 지나갔다고 표시한 후 stack에 넣어야 한다
-	while (false == stk.Empty()) {
-		coord c = stk.Pop();
-
-		CheckPos(c.r - 1, c.c);
-		CheckPos(c.r, c.c - 1);
-		CheckPos(c.r + 1, c.c);
-		CheckPos(c.r, c.c + 1);
-	}
+	return ret;
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	std::cin >> TC;
-	while (TC--) {
-		std::cin >> N >> M >> K;
-		for (int i = 0; i < K; ++i) {
-			int r, c; std::cin >> r >> c;
-			farm[r + 1][c + 1] = true;
-		}
-
-		int ans = 0;
-		for (int i = 1; i <= N; ++i) {
-			for (int j = 1; j <= M; ++j) {
-				if (farm[i][j]) {
-					++ans;
-					DFS(i, j);
-				}
-			}
-		}
-		std::cout << ans << '\n';
-	}
+	std::cin >> N >> K;
+	//K * 2 이상 가면 차라리 -1씩 해서 내려오는게 나을듯
+	K2 = std::min(100000, K * 2);
+	std::cout << BFS();
 
 	return 0;
 }
