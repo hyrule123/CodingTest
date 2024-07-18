eef27f336997077512986004aaefb122d8dfcb34
hw <ekdrn1@naver.com>
2024-07-18 23:29:07 +0900

    임시

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -4,123 +4,113 @@
 #include <iostream>
 #include <cstring> //memset
 /*
-백준 7569 (토마토) - 7576번 3d 버전
+백준 16928 (뱀과 사다리 게임)
 */
-int X, Y, Z;
-struct Coord { int x, y, z; };
-struct CircleQueue {
-	bool Empty() { return size == 0; }
-	bool Full() { return size == capacity; }
-	int Next(int cur) { if (++cur >= capacity) { cur -= capacity; } return cur; }
-	void PushBack(Coord c) { 
-		if (Full()) { Reserve(capacity * 2); } 
-		cont[end] = c; 
-		end = Next(end); 
-		++size; 
-	}
-	Coord PopFront() { Coord ret = cont[start]; start = Next(start); --size; return ret; }
-	void Reserve(int cap) {
-		if (cap <= capacity) { return; }
+struct pos { int warp = 0, diceCasts = -1; };
+pos board[102]{};
+#include <vector>
+//diceCast가 적은놈을 우선적으로 꺼내줌
+struct posInfo { 
+	int pos, diceCasts;
+};
+struct PriorityQueue {
+	void Insert(posInfo p) {
+		cont.push_back(p);
 
-		Coord* temp = new Coord[cap];
-		if (cont) {
-			if (false == Empty()) {
-				if (start < end) {
-					memcpy(temp, cont + start, sizeof(Coord) * size);
-				}
-				else {
-					int startPartSize = capacity - start;
-					memcpy(temp, cont + start, sizeof(Coord) * startPartSize);
-					memcpy(temp + startPartSize, cont, sizeof(Coord) * end);
-				}
+		int child = (int)cont.size() - 1;
+		while(child > 0) {
+			int parent = (child - 1) / 2;
+			if (cont[child].diceCasts < cont[parent].diceCasts) {
+				std::swap(cont[child], cont[parent]);
 			}
-			delete[] cont;
+			else { break; }
+			child = parent;
 		}
-		start = 0;
-		end = size;
-		capacity = cap;
-		cont = temp;
+	}
+	posInfo Pop() {
+		posInfo ret = cont.front();
+		std::swap(cont.front(), cont.back());
+		cont.resize(cont.size() - 1);
+
+		if (false == cont.empty()) {
+			size_t parent = 0;
+			size_t end = (cont.size() - 1) / 2;
+			while (parent <= end) {
+				size_t smallest = parent;
+				size_t left = smallest * 2 + 1;
+				size_t right = left + 1;
+				if (left < cont.size() && cont[left].diceCasts < cont[smallest].diceCasts) {
+					smallest = left;
+				}
+				if (right < cont.size() && cont[right].diceCasts < cont[smallest].diceCasts) {
+					smallest = right;
+				}
+				
+				//더이상 내려가지 못하면 break
+				if (smallest == parent) { break; }
+
+				std::swap(cont[parent], cont[smallest]);
+				parent = smallest;
+			}
+		}
+
+		return ret;
 	}
 
-	Coord* cont{};
-	int capacity{}, size{}, start{}, end{};
+	std::vector<posInfo> cont;
 } q;
 
-int farm[102][102][102]{};
-void Check(int x, int y, int z, const Coord& orig) {
-	if (farm[x][y][z] == 0) {
-		farm[x][y][z] = farm[orig.x][orig.y][orig.z] + 1;
-		q.PushBack({x, y, z});
-	}
-}
-
 int BFS() {
-	Coord last{};
-	while (false == q.Empty()) {
-		last = q.PopFront();
+	board[1].diceCasts = 0;
+	q.Insert({ 1 , board[1].diceCasts});
+
+	int ret = 0;
+	while (false == q.cont.empty()) {
+		posInfo cur = q.Pop();
+		if (cur.pos == 100) {
+			ret = cur.diceCasts;
+			break;
+		}
 		
-		Check(last.x, last.y, last.z - 1, last);
-		Check(last.x, last.y, last.z + 1, last);
-		Check(last.x, last.y - 1, last.z, last);
-		Check(last.x, last.y + 1, last.z, last);
-		Check(last.x - 1, last.y, last.z, last);
-		Check(last.x + 1, last.y, last.z, last);
+		//워프가 있다면 워프
+		if (board[cur.pos].warp) {
+			int warp = board[cur.pos].warp;
+			if (-1 == board[warp].diceCasts) {
+				board[warp].diceCasts = board[cur.pos].diceCasts;
+				q.Insert({warp, board[warp].diceCasts});
+			}
+		}
+		//없으면 주사위 던진다
+		else {
+			for (int i = 1; i <= 6; ++i) {
+				int dest = cur.pos + i;
+				if (dest <= 100 && -1 == board[dest].diceCasts) {
+					board[dest].diceCasts = board[cur.pos].diceCasts + 1;
+					q.Insert({ dest, board[dest].diceCasts });
+				}
+			}
+		}
 	}
-	return farm[last.x][last.y][last.z] - 1;
+
+	return ret;
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	//3차원 배열 -> 입력과 차원을 맞추기 위해서는 Z Y X 역순으로 받아줘야 함 
-	//_____ /  
-	//|	   | / 
-	//|____|/
-	std::cin >> Z >> Y >> X;
-	//BFS 탐색을 해야하는 범위 주변으로 -1을 둘러쳐 준다.
-	for (int x = 0; x <= X + 1; ++x) {
-		for (int y = 0; y <= Y + 1; ++y) {
-			farm[x][y][0] = -1;
-			farm[x][y][Z + 1] = -1;
-		}
-		for (int z = 0; z <= Z + 1; ++z) {
-			farm[x][0][z] = -1;
-			farm[x][Y + 1][z] = -1;
-		}
+	//어차피 둘다 특정위치로 이동시키는건 똑같음
+	int ladder, snake; std::cin >> ladder >> snake;
+	while (ladder--) {
+		int from, to; std::cin >> from >> to;
+		board[from].warp = to;
 	}
-	for (int y = 0; y <= Y + 1; ++y) {
-		for (int z = 0; z <= Z + 1; ++z) {
-			farm[0][y][z] = -1;
-			farm[X + 1][y][z] = -1;
-		}
+	while (snake--) {
+		int from, to; std::cin >> from >> to;
+		board[from].warp = to;
 	}
-
-	q.Reserve(std::max(X, std::max(Y, Z)));
-	for (int x = 1; x <= X; ++x) {
-		for (int y = 1; y <= Y; ++y) {
-			for (int z = 1; z <= Z; ++z) {
-				std::cin >> farm[x][y][z];
-				if (1 == farm[x][y][z]) {
-					q.PushBack({x, y, z});
-				}
-			}
-		}
-	}
-	int days = BFS();
-
-	//혹시나 안 익은 토마토(0)이 남아있을 경우 -1을 출력하고 프로그램 종료
-	for (int x = 1; x <= X; ++x) {
-		for (int y = 1; y <= Y; ++y) {
-			for (int z = 1; z <= Z; ++z) {
-				if (0 == farm[x][y][z]) {
-					std::cout << -1;
-					return 0;
-				}
-			}
-		}
-	}
-	std::cout << days;
+	q.cont.reserve(100);
+	std::cout << BFS();
 
 	return 0;
 }
