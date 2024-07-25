24023af78110635f1b02682293e7c8bca620542c
HWNOTE <ekdrn1@naver.com>
2024-07-25 22:18:45 +0900

    백준 1504 (특정한 최단 경로)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,130 +5,144 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 1753 (최단경로)
+백준 1504 (특정한 최단 경로)
 */
+//양방향 a-b, 길이 c, 반드시 지나야 하는 정점 v1, v2
+//v1 -> v2 -> N
+//v2 -> v1 -> N
+//둘 중 작은 수를 출력한다.
+using uint = unsigned int;
 #include <vector>
-template <typename T>
+struct travel {
+	uint pos{}, dist{};
+	bool operator < (const travel& other) const {
+		return dist < other.dist;
+	}
+};
 struct minHeap {
-	void Insert(T i) {
-		cont.push_back(i);
-		
-		if (cont.size() < 2) { return; }
+	void Insert(travel t) {
+		cont.push_back(t);
+
 		size_t cur = cont.size() - 1;
 		while (0 < cur) {
 			size_t parent = (cur - 1) / 2;
 
 			if (cont[cur] < cont[parent]) {
 				std::swap(cont[cur], cont[parent]);
+
+				cur = parent;
 			}
 			else {
 				break;
 			}
-
-			cur = parent;
 		}
 	}
-	T Pop() {
-		T ret = cont[0];
-
+	travel Pop() {
+		travel ret = cont.front();
 		std::swap(cont.front(), cont.back());
-		cont.resize(cont.size() - 1);
-
+		cont.pop_back();
+		
 		if (cont.size() < 2) { return ret; }
 
-		size_t parent = 0;
-		size_t end = cont.size() / 2 - 1;
-		while (parent <= end) {
-			size_t smallest = parent;
-			size_t left = smallest * 2 + 1;
+		size_t cur = 0;
+		size_t end = (cont.size() - 1) / 2;
+		while (cur <= end) {
+			size_t minIdx = cur;
+			size_t left = minIdx * 2 + 1;
 			size_t right = left + 1;
-
-			if (left < cont.size() && cont[left] < cont[smallest]) {
-				smallest = left;
+			if (left < cont.size() && cont[left] < cont[minIdx]) {
+				minIdx = left;
 			}
-			if(right < cont.size() && cont[right] < cont[smallest]) {
-				smallest = right;
+			if (right < cont.size() && cont[right] < cont[minIdx]) {
+				minIdx = right;
 			}
 
-			if (smallest != parent) {
-				std::swap(cont[parent], cont[smallest]);
+			if (cur != minIdx) {
+				std::swap(cont[cur], cont[minIdx]);
+				cur = minIdx;
 			}
 			else {
 				break;
 			}
-			parent = smallest;
-
 		}
-
+		
 		return ret;
 	}
 
-	std::vector<T> cont;
-};
-constexpr int INF = std::numeric_limits<int>::max();
-int V, E, K, u, v, w;
-#include <array>
-struct edge { 
-	int to, wt;
-	bool operator < (const edge& other) const {
-		return wt < other.wt;
-	}
-};
-std::array<std::vector<edge>, 20001> edges;
-minHeap<edge> heap;
-//K to idx cost
-int dp[20001];
+	std::vector<travel> cont;
+} heap;
+uint nodes[801][801], dp[801], N, E, a, b, c, v1, v2;
+constexpr uint INF = std::numeric_limits<uint>::max();
 
-void Djikstra() {
-	//시작점: K
-	heap.Insert({ K, 0 });
-	
+void Djikstra(uint _from, uint _to) {
+	heap.Insert({ _from, 0 });
+
 	while (false == heap.cont.empty()) {
-		edge min = heap.Pop();
+		travel from = heap.Pop();
 
-		//이미 방문했다면 최소거리가 등록된 것이므로 continue
-		if (INF != dp[min.to]) {
-			continue;
+		if (INF != dp[from.pos]) { continue; }
+
+		//여행거리를 기록
+		dp[from.pos] = from.dist;
+
+		//도착 시 중단
+		if (_to == from.pos) {
+			break;
 		}
 
-		//방문하지 않았을 경우 최소힙에서 꺼낸 최소 거리이므로 그대로 대입한다.
-		dp[min.to] = min.wt;
-		
-		//heap에서 꺼낸 노드에서 다음 노드로 방문하는 거리를 모조리 등록한다.
-		while (false == edges[min.to].empty()) {
-			edge next = edges[min.to].back(); edges[min.to].pop_back();
-			
-			//아직 목적지에 방문하지 않았을 경우 최소힙에 삽입
-			if (INF == dp[next.to]) {
-				heap.Insert({ next.to, min.wt + next.wt });
+		for (uint i = 1; i <= N; ++i) {
+			if (nodes[from.pos][i]) {
+				//i에 도착, 여행거리 from.dist + 이번 노드 -> 다음노드까지 거리
+				heap.Insert({ i, from.dist + nodes[from.pos][i] });
 			}
 		}
 	}
 }
 
+void Reset() {
+	heap.cont.clear();
+	memset(dp, 0xff, sizeof(uint) * 801);
+}
+uint Stopover(uint first, uint second) {
+	uint ret = 0;
+
+	Reset();
+	Djikstra(1, first);
+	if (dp[first] == INF) { return INF; }
+	ret += dp[first];
+
+	Reset();
+	Djikstra(first, second);
+	if (dp[second] == INF) { return INF; }
+	ret += dp[second];
+
+	Reset();
+	Djikstra(second, N);
+	if (dp[N] == INF) { return INF; }
+	ret += dp[N];
+
+	return ret;
+}
+
+
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	for (int i = 0; i < 20001; ++i) {
-		dp[i] = INF;
+	std::cin >> N >> E;
+	for (uint i = 0; i < E; ++i) {
+		std::cin >> a >> b >> c;
+		nodes[a][b] = c;
+		nodes[b][a] = c;
 	}
-	heap.cont.reserve(10000);
+	std::cin >> v1 >> v2;
+	//1 -> v1 -> v2 -> N으로 가는 경우와 1 -> v2 -> v1 -> N으로 가는 경우 2개
+	uint v1Result = Stopover(v1, v2);
+	uint v2Result = Stopover(v2, v1);
 
-	std::cin >> V >> E >> K;
-	for (int i = 0; i < E; ++i) {
-		std::cin >> u >> v >> w;
-		edges[u].push_back({ v, w });
-	}
-	Djikstra();
-	for (int i = 1; i <= V; ++i) {
-		if (dp[i] == INF) {
-			std::cout << "INF\n";
-		}
-		else {
-			std::cout << dp[i] << '\n';
-		}
-	}
+	uint output = std::min(v1Result, v2Result);
+	if (output == INF) { std::cout << -1; }
+	else { std::cout << output; }
 
 	return 0;
 }
