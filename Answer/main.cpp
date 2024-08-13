e6246519d256d6b6dc0db47cfce66504d6e6a8eb
EHW <ekdrn1@naver.com>
2024-08-13 23:26:27 +0900

    백준 1644 (소수의 연속합) - 다른 분 해답 참고

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,28 +5,33 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 1644 (소수의 연속합)
+백준 1644 (소수의 연속합) - 다른 분 해답 참고
 */
 #include <vector>
 #include <cmath>
 using ull = unsigned long long;
-std::vector<ull> Sieve(ull _n) {
+std::vector<ull> get_prefix_sum_primes(ull _n) {
 	std::vector<ull> ret;
 	ret.reserve(_n / ((ull)std::log(_n) + 1));
 
 	std::vector<bool> sieve(_n + 1, true);
-	sieve[0] = false;
-	sieve[1] = false;
-	for (ull i = 2; i <= _n; ++i) {
+	sieve[0] = sieve[1] = false;
+	for (ull i = 2; i * i <= _n; ++i) {
 		if (sieve[i]) {
-			ret.push_back(i);
-
-			for (ull j = i + i; j <= _n; j += i) {
+			for (ull j = i * i; j <= _n; j += i) {
 				sieve[j] = false;
 			}
 		}
 	}
 
+	ret.push_back(0);
+	for (ull i = 2; i <= _n; ++i) {
+		if (sieve[i]) {
+			ret.push_back(i);
+			*(ret.end() - 1) += *(ret.end() - 2);
+		}
+	}
+	
 	return ret;
 }
 ull N;
@@ -34,7 +39,7 @@ ull N;
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
-
+	
 	std::cin >> N;
 	if (N <= 1) {
 		std::cout << 0;
@@ -42,39 +47,27 @@ int main() {
 	}
 
 	//에라스토테네스의 체
-	std::vector<ull> primes = Sieve(N);
+	std::vector<ull> prefix_sum = get_prefix_sum_primes(N);
 
 	//투 포인터
-	ull max = (ull)primes.size() - 1;
+	ull max = (ull)prefix_sum.size() - 1;
 	ull l = 0;
-	ull r = 0;
-	ull sum = primes[0];
+	ull r = 1;
+	ull sum = prefix_sum[0];
 	ull ans_count = 0;
-	while (true) {
-		if (sum < N) {
-			if (r < max) {
-				sum += primes[++r];
-			}
-			else { break; }
+	while (r <= max) {
+		ull part_sum = prefix_sum[r] - prefix_sum[l];
+
+		if(part_sum < N) {
+			++r;
 		}
-		else if (N < sum) {
-			if (l <= r) {
-				sum -= primes[l]; ++l;
-			}
-			else { break; }
+		else if (N < part_sum) {
+			++l;
 		}
-		else { // sum == N
+		else {
 			++ans_count;
-
-			if (r < max) {
-				sum += primes[++r];
-			}
-			else { break; }
-
-			if (l <= r) {
-				sum -= primes[l]; ++l;
-			}
-			else { break; }
+			++l;
+			++r;
 		}
 	}
 
