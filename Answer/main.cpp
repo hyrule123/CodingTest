4e7ef73588d63853eb63087a3efc51a5a1481129
HWNOTE <ekdrn1@naver.com>
2024-08-24 23:24:18 +0900

    백준 11779 (최소비용 구하기 2)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -8,6 +8,7 @@
 백준 11779 (최소비용 구하기 2)
 */
 #include <vector>
+#include <stack>
 
 template <typename T>
 struct pq {
@@ -62,16 +63,78 @@ struct pq {
 	std::vector<T> cont;
 };
 
-struct edge { int end, cost; };
+struct edge { 
+	int to, cost;
+};
+struct trace_log {
+	int prev = 0, dist = -1;
+};
+struct travel {
+	int from, to, dist;
+	bool operator < (const travel& other) const {
+		return dist < other.dist;
+	}
+};
+
 int n_node, n_edge, dep, dest;
 std::vector<edge> edges[1001];
-int visited[1001];
-pq<int> q;
+trace_log visited[1001];
+pq<travel> q;
 
 void djikstra() {
-	
+	q.Insert({ dep, dep, 0 });
 
+	while (false == q.cont.empty()) {
+		//우선순위 큐로부터 현재 가장 짧은 간선을 반환받는다
+		travel cur = q.top(); q.pop();
 
+		if (visited[cur.to].dist != -1) { continue; }
+
+		visited[cur.to].prev = cur.from;
+		visited[cur.to].dist = cur.dist;
+
+		if (cur.to == dest) {
+			break;
+		}
+		
+		//이번 노드에 연결왼 간선들을 우선순위 큐에 넣는다.
+		travel next;
+		next.from = cur.to;
+		for (const auto& e : edges[cur.to]) {
+			if (visited[e.to].dist == -1) {
+				next.to = e.to;
+				next.dist = cur.dist + e.cost;
+				q.Insert(next);
+			}
+		}
+	}
+}
+
+void trace_back() {
+	//거리 출력
+	std::cout << visited[dest].dist << '\n';
+
+	int cur = dest;
+	int count = 0;
+	std::stack<int> ans;
+	while (true) {
+		ans.push(cur);
+		++count;
+
+		if (cur == dep) {
+			break;
+		}
+
+		cur = visited[cur].prev;
+	}
+
+	//거쳐가는 도시 갯수 출력
+	std::cout << count << '\n';
+
+	//거쳐가는 도시들 나열
+	while (false == ans.empty()) {
+		std::cout << ans.top() << ' '; ans.pop();
+	}
 }
 
 int main() {
@@ -84,18 +147,12 @@ int main() {
 	std::cin >> n_node >> n_edge;
 	for (int i = 0; i < n_edge; ++i) {
 		int from, to, cost; std::cin >> from >> to >> cost;
-		edges[from].push_back({ dep, cost });
+		edges[from].push_back({ to, cost });
 	}
 	std::cin >> dep >> dest;
 
-	for (int i = 100; i > 0; --i) {
-		q.Insert(i);
-	}
-	while (false == q.cont.empty()) {
-		std::cout << q.top() << '\n'; q.pop();
-	}
-
 	djikstra();
+	trace_back();
 
 	return 0;
 }
