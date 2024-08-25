9517a05d3dc029ff1a9b09217070a6a64ba28456
HW_ThinkBook <ekdrn@naver.com>
2024-08-25 23:46:42 +0900

    백준 1167 (트리의 지름) : DFS 버전

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,33 +5,31 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 11725 (트리의 부모 찾기)
+백준 1167 (트리의 지름) : DFS 버전
+*/
+/*
+* 트리의 지름 구하는 법
+1. 임의의 노드로부터 제일 먼 곳에 있는 노드 A를 DFS를 통해 구한다.
+2. 노드 A로부터 제일 먼 곳에 있는 노드 B와의 거리가 트리의 지름이 된다.
 */
 #include <vector>
-#include <queue>
-std::vector<int> tree[100001];
-int N, ans[100001];
+#include <bitset>
+struct edge { int to, dist; };
+int V;
+std::vector<edge> nodes[100001];
+std::bitset<100001> visited;
+int farthest_node = -1, max_dist = -1;
+void DFS(int node, int acc_dist) {
+	visited[node] = true;
 
-void BFS() {
-	std::queue<int> q;
-
-	//루트인 1부터 넣어준다. 1에 가까운 노드 = 트리 기준 1의 자식 노드들
-	q.push(1);
-	ans[1] = 0;
-
-	while (false == q.empty()) {
-		int idx = q.front(); q.pop();
-
-		for (int child : tree[idx]) {
-			if (ans[child] == -1) {
-				ans[child] = idx;
-				q.push(child);
-			}
-		}
+	if (max_dist < acc_dist) {
+		max_dist = acc_dist;
+		farthest_node = node;
 	}
 
-	for (int i = 2; i <= N; ++i) {
-		std::cout << ans[i] << '\n';
+	for (const edge& e : nodes[node]) {
+		if (visited[e.to]) { continue; }
+		DFS(e.to, acc_dist + e.dist);
 	}
 }
 
@@ -39,16 +37,29 @@ int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	memset(ans, -1, sizeof(ans));
-	
-	std::cin >> N;
-	for (int i = 1; i <= N; ++i) {
-		int a, b; std::cin >> a >> b;
-		tree[a].push_back(b);
-		tree[b].push_back(a);
+	std::cin >> V;
+	for (int i = 1; i <= V; ++i) {
+		int node, to, dist; std::cin >> node;
+
+		while (true) {
+			std::cin >> to;
+			if (to == -1) { break; }
+			std::cin >> dist;
+
+			nodes[node].push_back({ to, dist });
+		}
 	}
 
-	BFS();
+	//1번 노드로부터 제일 먼 노드 A를 구한다.
+	DFS(1, 0);
+	int farthest_from_1 = farthest_node;
+	farthest_node = -1;
+	max_dist = -1;
+	visited.reset();
+
+	//A로부터 제일 먼 노드를 구하고, 그 거리를 출력하면 그게 트리의 지름이 된다.
+	DFS(farthest_from_1, 0);
+	std::cout << max_dist;
 
 	return 0;
 }
