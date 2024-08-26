628cc7fc485ecbd9f9065488333bfb5f4edb10f8
HW_ThinkBook <ekdrn@naver.com>
2024-08-26 19:51:51 +0900

    백준 1991 (트리 순회)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,29 +5,39 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 1967 (트리의 지름)
+백준 1991 (트리 순회)
 */
-//동일 문제: 백준 1167
-//BFS, DFS 둘 다 가능하지만 공간복잡도를 생각했을 때 DFS가 좀 더 유리한듯
-#include <vector>
-#include <bitset>
-struct edge { int to, dist; };
-int n_node, farthest_node = -1, farthest_dist = -1;
-std::vector<edge> childs[10001];
-std::bitset<10001> visited;
-void DFS(int cur, int acc_dist) {
-	visited[cur] = true;
+int n_node;
+struct node { char left = '.', right = '.'; };
+node tree['Z' + 1];
 
-	if (farthest_dist < acc_dist) {
-		farthest_node = cur;
-		farthest_dist = acc_dist;
+void pre_order(char idx) {
+	std::cout << idx;
+	
+	if (tree[idx].left != '.') {
+		pre_order(tree[idx].left);
 	}
-
-	for (const edge& e : childs[cur]) {
-		if (false == visited[e.to]) {
-			DFS(e.to, acc_dist + e.dist);
-		}
+	if (tree[idx].right != '.') {
+		pre_order(tree[idx].right);
 	}
+}
+void in_order(char idx) {
+	if (tree[idx].left != '.') {
+		in_order(tree[idx].left);
+	}
+	std::cout << idx;
+	if (tree[idx].right != '.') {
+		in_order(tree[idx].right);
+	}
+}
+void post_order(char idx) {
+	if (tree[idx].left != '.') {
+		post_order(tree[idx].left);
+	}
+	if (tree[idx].right != '.') {
+		post_order(tree[idx].right);
+	}
+	std::cout << idx;
 }
 
 int main() {
@@ -35,21 +45,17 @@ int main() {
 	LOCAL_IO;
 
 	std::cin >> n_node;
-	for (int i = 0; i < n_node - 1; ++i) {
-		int a, b, dist; std::cin >> a >> b >> dist;
-		childs[a].push_back({ b, dist });
-		childs[b].push_back({ a, dist });
+	for (int i = 0; i < n_node; ++i) {
+		char n; std::cin >> n;
+		std::cin >> tree[n].left >> tree[n].right;
 	}
 
-	DFS(1, 0);
-
-	int farthest_from_1 = farthest_node;
-	farthest_node = -1;
-	farthest_dist = -1;
-	visited.reset();
-	DFS(farthest_from_1, 0);
-
-	std::cout << farthest_dist;
+	pre_order('A');
+	std::cout << '\n';
+	in_order('A');
+	std::cout << '\n';
+	post_order('A');
+	
 
 	return 0;
 }
