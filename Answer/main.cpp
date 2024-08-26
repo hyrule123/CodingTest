60e4b4ba5ab6c7072490035e9742ef17bc75ac15
HW_ThinkBook <ekdrn@naver.com>
2024-08-26 17:42:25 +0900

    백준 1967 (트리의 지름)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,66 +5,51 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 1167 (트리의 지름) : BFS 버전
+백준 1967 (트리의 지름)
 */
-/*
-* 트리의 지름 구하는 법
-1. 임의의 노드로부터 제일 먼 곳에 있는 노드 A를 DFS 또는 BFS를 통해 구한다.
-2. 노드 A로부터 제일 먼 곳에 있는 노드 B와의 거리가 트리의 지름이 된다.
-*/
+//동일 문제: 백준 1167
+//BFS, DFS 둘 다 가능하지만 공간복잡도를 생각했을 때 DFS가 좀 더 유리한듯
 #include <vector>
 #include <bitset>
-#include <queue>
 struct edge { int to, dist; };
-int V;
-std::vector<edge> nodes[100001];
-std::bitset<100001> visited;
-std::queue<edge> q;
-edge BFS(int from) {
-	visited.reset();
-	q.push({ from, 0 });
-	visited[from] = true;
+int n_node, farthest_node = -1, farthest_dist = -1;
+std::vector<edge> childs[10001];
+std::bitset<10001> visited;
+void DFS(int cur, int acc_dist) {
+	visited[cur] = true;
 
-	//BFS를 통해 제일 먼 노드 번호와 거리를 구한다.
-	edge farthest = { -1, -1 };
-	while (false == q.empty()) {
-		edge cur = q.front(); q.pop();
-
-		if (farthest.dist < cur.dist) {
-			farthest = cur;
-		}
-
-		for (const edge& e : nodes[cur.to]) {
-			if (visited[e.to]) { continue; }
-			visited[e.to] = true;
-			q.push({ e.to, cur.dist + e.dist });
-		}
+	if (farthest_dist < acc_dist) {
+		farthest_node = cur;
+		farthest_dist = acc_dist;
 	}
 
-	return farthest;
+	for (const edge& e : childs[cur]) {
+		if (false == visited[e.to]) {
+			DFS(e.to, acc_dist + e.dist);
+		}
+	}
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
 
-	std::cin >> V;
-	for (int i = 1; i <= V; ++i) {
-		int node, to, dist; std::cin >> node;
-
-		while (true) {
-			std::cin >> to;
-			if (to == -1) { break; }
-			std::cin >> dist;
-
-			nodes[node].push_back({ to, dist });
-		}
+	std::cin >> n_node;
+	for (int i = 0; i < n_node - 1; ++i) {
+		int a, b, dist; std::cin >> a >> b >> dist;
+		childs[a].push_back({ b, dist });
+		childs[b].push_back({ a, dist });
 	}
 
-	edge farthest_from_1 = BFS(1);
+	DFS(1, 0);
 
-	edge diameter = BFS(farthest_from_1.to);
-	std::cout << diameter.dist;
+	int farthest_from_1 = farthest_node;
+	farthest_node = -1;
+	farthest_dist = -1;
+	visited.reset();
+	DFS(farthest_from_1, 0);
+
+	std::cout << farthest_dist;
 
 	return 0;
 }
