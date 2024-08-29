be13d2ec1816ccbad7ed1a6d809d88e53220bd09
HW_ThinkBook <ekdrn@naver.com>
2024-08-29 17:51:36 +0900

    백준 1976 (여행 가자)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -5,61 +5,67 @@
 #include <limits>
 #include <cstring> //memset
 /*
-백준 1717 (집합의 표현): 유니온 파인드 기초 문제
+백준 1976 (여행 가자)
 */
-#define UNI 0
-#define INT 1
-constexpr int MAX = 1'000'000;
-int n_set, n_operation, sets[MAX]; //sets[i] = j: i는 j와 같은 집합에 속해있다.
-int find_group(int elem) {
-	/*
-	재귀 형태로 sets[elem]은 어느 그룹에 속하는지를 찾고, 자신도 그 그룹 번호로 바꾼다
-	1-2-3이라고 할때 sets[1] = 2, sets[2] = 3, sets[3] = 3 이라면
-	find_group(1) -> find_group(2) -> find_group(3) -> return 3 -> sets[2] = 3 -> sets[1] = 3
-	이렇게 되면 차후에 재귀 횟수를 줄일 수 있다.(최적화)
-	*/
-	if (elem != sets[elem]) {
-		sets[elem] = find_group(sets[elem]);
+#include <bitset>
+constexpr int MAX = 201;
+int n_node, n_stopover, graph[MAX];
+
+int find_group(int node) {
+	if (graph[node] != node) {
+		graph[node] = find_group(graph[node]);
 	}
-	return sets[elem];
+	
+	return graph[node];
 }
 
 int main() {
 	std::cin.tie(nullptr); std::cin.sync_with_stdio(false);
 	LOCAL_IO;
-	
-	std::cin >> n_set >> n_operation;
-	for (int i = 0; i <= n_set; ++i) {
-		sets[i] = i;
+
+	std::cin >> n_node >> n_stopover;
+	for (int i = 1; i <= n_node; ++i) {
+		graph[i] = i;
 	}
-	for (int i = 1; i <= n_operation; ++i) {
-		int op_type, a, b; std::cin >> op_type >> a >> b;
 
-		//무조건 a쪽을 작은쪽으로 둔다.
-		if (b < a) { std::swap(a, b); }
+	
+	for (int i = 1; i <= n_node; ++i) {
+		for (int j = 1; j <= n_node; ++j) {
+			int is_linked; std::cin >> is_linked;
 
-		int group_a = find_group(a);
-		int group_b = find_group(b);
+			//두 노드가 연결되어 있으면 하나의 집합으로 묶는다
+			if (is_linked) {
+				int group_i = find_group(i);
+				int group_j = find_group(j);
+
+				if (group_i < group_j) {
+					graph[group_i] = group_j;
+				}
+				else {
+					graph[group_j] = group_i;
+				}
+			}
+		}
+	}
+
+	//지정된 경유지를 모두 방문할 수 있는지 확인한다.
+	bool travel_possible = true;
+
+	//첫 번째 경유지의 그룹을 확인
+	int group_num; std::cin >> group_num;
+	group_num = find_group(group_num);
+
+	for (int i = 2; i <= n_stopover; ++i) {
+		int stopover; std::cin >> stopover;
 		
-		switch (op_type) {
-		case UNI: {
-			//둘이 속한 집합이 다를 경우 작은쪽의 원소를 다른쪽과 동일하게 변경
-			if (group_a != group_b) {
-				sets[group_a] = group_b;
-			}
-			break;
-		}
-		case INT: {
-			if (group_a == group_b) {
-				std::cout << "YES\n";
-			}
-			else {
-				std::cout << "NO\n";
-			}
-
-			break;
-		}
-		}
+		//전부 동일한 그룹에 속해있는지를 확인한다. 하나라도 그룹이 다를경우 경유 불가능하다는 뜻 
+		travel_possible = travel_possible && (group_num == find_group(stopover));
+	}
+	if (travel_possible) {
+		std::cout << "YES";
+	}
+	else {
+		std::cout << "NO";
 	}
 
 	return 0;
