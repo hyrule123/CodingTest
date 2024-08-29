bd95c1c21545291bc258152124b3e18e2b182ba0
HWNOTE <ekdrn1@naver.com>
2024-08-30 01:44:57 +0900

    백준 4195 (친구 네트워크): 오답 4

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -8,99 +8,35 @@
 백준 4195 (친구 네트워크)
 */
 
-#include <vector>
-constexpr int MAX = 200'003, p1 = 31, p2 = 53;
-struct str {
-	char s[21];
-	int len;
-
-	void clear() {
-		s[0] = '\0';
-		len = 0;
-	}
-
-	bool operator ==(const str& other) const {
-		if (len != other.len) {
-			return false;
-		}
-		for (int i = 0; i < len; ++i) {
-			if (s[i] != other.s[i]) {
-				return false;
-			}
-		}
-
-		return true;
-	}
-};
-std::istream& operator>>(std::istream& stream, str& string) {
-	stream >> string.s;
-	string.len = (int)strlen(string.s);
-	return stream;
-}
-struct pair {
-	str id;
-	int group_hash;
-	int friend_count;
-};
-
-struct hash_map {
-	static int str_hash(const str& p) {
-		int ret = 0;
-
-		for (int i = 0; i < p.len; ++i) {
-			ret = (ret * p1 + p.s[i]) % MAX;
-		}
-
-		return ret;
-	}
-
-	void clear() {
-		memset(cont, 0, sizeof(cont));
-	}
-
-	int insert(const str& key) {
-		int hash = str_hash(key);
-
-		//linear probing
-		while (true) {
-			if (cont[hash].id.len == 0) {
-				cont[hash].id = key;
-				cont[hash].group_hash = hash;
-				cont[hash].friend_count = 1;
-				return hash;
-			}
-			else if (cont[hash].id == key) {
-				return hash;
-			}
-			++hash;
-			if (hash >= MAX) {
-				hash -= MAX;
-			}
-		}
-
-		cont[hash].id = key;
-
-		//들어간 컨테이너 인덱스(해시값)을 반환한다.
-		return hash;
-	}
-
-	pair cont[MAX];
-};
-
-hash_map map;
+#include <unordered_map>
+#include <string>
 int tc, n_link;
+using str = std::string;
+struct group {
+	str leader;
+	int count;
+};
+std::unordered_map<str, group> group_leader;
 
-void reset() {
-	n_link = 0;
-	map.clear();
-}
-
-int find_group(int hash) {
-	if (map.cont[hash].group_hash != hash) {
-		map.cont[hash].group_hash = find_group(map.cont[hash].group_hash);
+group& find_group(str const& name) {
+	if (group_leader[name].leader != name) {
+		group_leader[name] = find_group(group_leader[name].leader);
 	}
 
-	return map.cont[hash].group_hash;
+	return group_leader[name];
+}
+
+//항상 a < b
+void union_group(str const& a, str const& b) {
+	group& group_a = find_group(a);
+	group& group_b = find_group(b);
+
+	//a그룹과 b그룹의 리더가 다르다면 리더를 b그룹 쪽으로 통일시켜준다.
+	//그룹 인원수도 병합
+	if (group_a.leader != group_b.leader) {
+		group_a.leader = group_b.leader;
+		group_b.count += group_a.count;
+	}
 }
 
 int main() {
@@ -109,31 +45,32 @@ int main() {
 
 	std::cin >> tc;
 	while (tc--) {
-		reset();
+		group_leader.clear();
 
 		std::cin >> n_link;
+		
 		for (int i = 0; i < n_link; ++i) {
 			str a, b; std::cin >> a >> b;
 
-			//문자열 해싱
-			int hash_a = map.insert(a);
-			int hash_b = map.insert(b);
-
-			//해시값으로 속한 그룹을 탐색
-			int group_a = find_group(hash_a);
-			int group_b = find_group(hash_b);
-
-			//그룹이 다를 경우 그룹을 합친다
-			if (group_a != group_b) {
-				if (group_a > group_b) {
-					std::swap(group_a, group_b);
-				}
-				map.cont[group_a].group_hash = group_b;
-				map.cont[group_b].friend_count += map.cont[group_a].friend_count;
+			//무조건 a < b가 되도록 만들어줌
+			if (a > b) { std::swap(a, b); }
+			
+			//그룹 리더가 비어있을 경우 새로운 인원이 들어온것. 새 그룹을 생성해준다.
+			auto& group_a = group_leader[a];
+			if (group_a.leader.empty()) {
+				group_a.leader = a;
+				group_a.count = 1;
+			}
+			auto& group_b = group_leader[b];
+			if (group_b.leader.empty()) {
+				group_b.leader = b;
+				group_b.count = 1;
 			}
 
-			//합쳐진 그룹원 수를 출력
-			std::cout << map.cont[group_b].friend_count << '\n';
+			//a와 b를 합친다.
+			union_group(a, b);
+
+			std::cout << group_b.count << '\n';
 		}
 	}
 
