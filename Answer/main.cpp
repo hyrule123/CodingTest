8a65fa1fd9133d0d189f129b1a79afcec4d3d615
HWNOTE <ekdrn1@naver.com>
2024-08-30 02:55:44 +0900

    백준 4195 (친구 네트워크)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -7,6 +7,12 @@
 /*
 백준 4195 (친구 네트워크)
 */
+/* 계속 오답 나왔던 이유
+gcc에서는 문자열 해싱에 'MurMurHash2'라는 알고리즘을 사용 중.
+내가 사용한 단순한 해쉬함수보다 당연히 훨씬 효율적이고 빠를 거고, 
+해시 충돌 해결은 collision chaining 방식으로 해결하는데 아마 여기도 더 좋은 알고리즘을 사용중일 것임.
+아마 차이는 여기서 온게 아닌가 싶음.
+*/
 
 #include <unordered_map>
 #include <string>
@@ -18,24 +24,28 @@ struct group {
 };
 std::unordered_map<str, group> group_leader;
 
-group& find_group(str const& name) {
+str const& find_group(str const& name) {
 	if (group_leader[name].leader != name) {
-		group_leader[name] = find_group(group_leader[name].leader);
+		group_leader[name].leader = find_group(group_leader[name].leader);
 	}
 
-	return group_leader[name];
+	return group_leader[name].leader;
 }
 
-//항상 a < b
 void union_group(str const& a, str const& b) {
-	group& group_a = find_group(a);
-	group& group_b = find_group(b);
+	str leader_a = find_group(a);
+	str leader_b = find_group(b);
 
-	//a그룹과 b그룹의 리더가 다르다면 리더를 b그룹 쪽으로 통일시켜준다.
-	//그룹 인원수도 병합
-	if (group_a.leader != group_b.leader) {
-		group_a.leader = group_b.leader;
-		group_b.count += group_a.count;
+	if (leader_a == leader_b) { return; }
+
+	//union rank는 문자 사전순으로 대체
+	if (leader_a < leader_b) {
+		group_leader[leader_a].leader = leader_b;
+		group_leader[leader_b].count += group_leader[leader_a].count;
+	}
+	else {
+		group_leader[leader_b].leader = leader_a;
+		group_leader[leader_a].count += group_leader[leader_b].count;
 	}
 }
 
@@ -51,9 +61,6 @@ int main() {
 		
 		for (int i = 0; i < n_link; ++i) {
 			str a, b; std::cin >> a >> b;
-
-			//무조건 a < b가 되도록 만들어줌
-			if (a > b) { std::swap(a, b); }
 			
 			//그룹 리더가 비어있을 경우 새로운 인원이 들어온것. 새 그룹을 생성해준다.
 			auto& group_a = group_leader[a];
@@ -69,8 +76,9 @@ int main() {
 
 			//a와 b를 합친다.
 			union_group(a, b);
-
-			std::cout << group_b.count << '\n';
+			
+			//a가 속한 그룹의 인원을 출력한다.
+			std::cout << group_leader[find_group(b)].count << '\n';
 		}
 	}
 
