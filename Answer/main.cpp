c09d3f29e1dda0439f721b285c71dc603e3afde4
HWNOTE <ekdrn1@naver.com>
2024-10-17 00:10:59 +0900

    백준 2166 (다각형의 면적)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,44 +14,40 @@ int main() {
 }
 
 #include <vector>
-int N;
-struct node {
-	int weight;
-	std::vector<int> link;
+#include <cmath>
+//shoelace formula.(식이 신발끈 메는것처럼 생김)
+//https://mathworld.wolfram.com/PolygonArea.html
+struct coord {
+	double x, y;
 };
-std::vector<node> nodes;
-int dp[10001][2];
-constexpr int exclude = 0, include = 1;
-
-void recursive_DP(int cur, int par) {
-	dp[cur][exclude] = 0;
-	dp[cur][include] = nodes[cur].weight;
-
-	for (int next : nodes[cur].link) {
-		if (next != par) {
-			recursive_DP(next, cur);
-
-			//조건 3: 선정되지 못한 마을에 경각심을 불러일으키기 위해서, '우수 마을'로 선정되지 못한 마을은 적어도 하나의 '우수 마을'과는 인접해 있어야 한다.
-			// -> 우수마을 인구수가 최대값이 되기 위해서는 연결 노드 중 최소 한개는 우수마을이여야 함.
-			dp[cur][exclude] += std::max(dp[next][exclude], dp[next][include]);
-			dp[cur][include] += dp[next][exclude];
-		}
-	}
+double round_precision(double val, double precision) {
+	return std::round(val / precision) * precision;
 }
 
-void solve()
-{
+std::vector<coord> polygon;
+int N;
+void solve() {
 	std::cin >> N;
-	nodes.resize(N + 1);
-	for (int i = 1; i <= N; ++i) {
-		std::cin >> nodes[i].weight;
-	}
-	for (int i = 1; i <= N - 1; ++i) {
-		int a, b; std::cin >> a >> b;
-		nodes[a].link.push_back(b);
-		nodes[b].link.push_back(a);
-	}
+	polygon.resize(N);
 	
-	recursive_DP(1, 0);
-	std::cout << std::max(dp[1][exclude], dp[1][include]);
+	for (int i = 0; i < N; ++i) {
+		std::cin >> polygon[i].x >> polygon[i].y;
+	}
+
+	double area = 0;
+	for (size_t i = 0; i < polygon.size() - 1; ++i) {
+		area += (polygon[i].x * polygon[i + 1].y - polygon[i + 1].x * polygon[i].y);
+	}
+
+	//Xn * Y1 - X1 * Yn 
+	area += (polygon.back().x * polygon.front().y - polygon.front().x * polygon.back().y);
+
+	//* 1/2
+	area *= 0.5;
+
+	//Note that the area of a convex polygon is defined to be positive if the points are arranged in a counterclockwise order and negative if they are in clockwise order (Beyer 1987).
+	area = std::abs(area);
+	area = round_precision(area, 0.1);
+
+	printf("%.1lf", area);
 }
