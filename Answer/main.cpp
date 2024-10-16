2aea56e48dd6ac9bd6392d85b0fdbb2a688d11c2
HWNOTE <ekdrn1@naver.com>
2024-10-17 06:31:02 +0900

    백준 25308 (방사형 그래프)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,48 +13,81 @@ int main() {
 	return 0;
 }
 
-//1  1  1
-//x1 y1 z1
-//x2 y2 z2
-struct coord { 
-	double x, y, z; 
-	coord cross(const coord& other) {
-		return { y * other.z - other.y * z, z * other.x - other.z * x, x * other.y - other.x * y };
-	}
+#include <array>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+std::array<int, 8> order = { 0, 1, 2, 3, 4, 5, 6, 7 };
+float stats[8];
 
-	friend std::istream& operator>>(std::istream& str, coord& c) {
-		str >> c.x >> c.y;
-		c.z = 0;
-		return str;
+
+
+struct vec {
+	float x, y;
+	void operator -= (const vec& other) {
+		x -= other.x;
+		y -= other.y;
 	}
-	void operator -= (const coord& c) {
-		x -= c.x; y -= c.y;
+	float cross(const vec& other) {
+		return (x * other.y - other.x * y);
 	}
 };
 
+/*
+* left->right 벡터에서 left->mid 벡터를 외적했을때
+|\
+| \ * <- mid가 여깄으면 반시계 방향이므로 양수: 볼록
+|* \ <- mid가 여깄으면 시계 방향이므로: 오목
+|   \
+---------
+*/
+//1사분면에다 놓고 테스트할 예정, 시계방향 순서대로 입력
+bool is_bolock(float left, float mid, float right) {
+	//cos 45 == sin 45
+	constexpr float cos45 = 0.70710678118f;
+
+	//설명: left 좌표를 기준으로
+	const vec l = { 0, left };
+	vec left_to_right = { right, 0 };
+	vec left_to_mid = { mid * cos45, mid * cos45 };
+
+	//상대벡터 2개를 생성한다.
+	left_to_right -= l;
+	left_to_mid -= l;
+
+	//외적값이 양수면 볼록 삼각형이다.
+	return 0.0f <= left_to_right.cross(left_to_mid);
+}
+
+bool test_case() {
+	for (int i = 0; i < 8; ++i) {
+		float left = stats[order[i]];
+		float mid = stats[order[(i + 1) % 8]];
+		float right = stats[order[(i+ 2) % 8]];
+
+		//하나라도 오목일경우 false
+		if (false == is_bolock(left, mid, right)) {
+			return false;
+		}
+	}
+
+	return true;
+}
 
 void solve() {
-	coord vec1{}, vec2{}, temp{};
-	
-	std::cin >> vec1 >> vec2 >> temp;
-	vec1 -= vec2;
-	vec2 -= temp;
-
-	//외적의 Z값 
-	// 양수 = 반시계방향
-	// 음수 = 시계방향
-	// 0 = 일직선
-	coord cross = vec1.cross(vec2);
-
-	constexpr double epsilon = std::numeric_limits<double>::epsilon();
-
-	if (std::abs(cross.z) < epsilon) {
-		std::cout << 0;
+	for (float& s : stats) {
+		std::cin >> s;
 	}
-	else if (0 <= cross.z) {
-		std::cout << 1;
+
+	int ans = 0;
+	//
+	do {
+		if (test_case()) {
+			++ans;
+		}
 	}
-	else {
-		std::cout << -1;
-	}
+	//아래 코드는 매번 순열 조합에 따라서 숫자를 배열한다.
+	while (std::next_permutation(order.begin(), order.end()));
+
+	std::cout << ans;
 }
