5372051a24379925dee24f05b95a9e4575de8442
HWNOTE <ekdrn1@naver.com>
2024-10-23 03:16:43 +0900

    백준 3015 (오아시스 재결합) : 복습

---
+++ Answer/Template/Baekjoon_Template.h
@@ -0,0 +1,14 @@
+#ifndef LOCAL
+#define LOCAL_IO (void)0
+#endif
+#include <iostream>
+#include <limits>
+#include <cstring> //memset
+
+void solve();
+
+int main() {
+	std::cin.tie(nullptr); std::cin.sync_with_stdio(false); LOCAL_IO;
+	solve();
+	return 0;
+}
--- Answer/main.cpp
+++ Answer/main.cpp
@@ -13,77 +13,58 @@ int main() {
 	return 0;
 }
 
-#include <unordered_map>
-#include <string>
-constexpr int board_size = 8;
-struct coord {
-	int x, y;
-	coord operator +(const coord& other) {
-		return { this->x + other.x, this->y + other.y };
-	}
-	bool operator != (const coord& other) {
-		return { this->x != other.x || this->y != other.y };
-	}
-	bool valid() {
-		return { 0 <= x && 0 <= y && x < board_size && y < board_size };
-	}
+#include <vector>
+//모두 같은 키로 50만명이 서있을 때 (50만)Combination(2) -> 1000억 넘어감
+using int64 = std::int64_t;
+struct person {
+	int64 same_height_count;
+	int height;
 };
+int N;
+std::vector<person> stk;
 
-std::unordered_map<std::string, coord> directions;
-coord king_pos, stone_pos;
+void solve() {
+	std::cin >> N;
+	
+	stk.reserve(500000);
+	int64 pair_count = 0;
+	for (int i = 0; i < N; ++i) {
+		person p;
+		p.same_height_count = 1;
+		std::cin >> p.height;
 
-void push(const coord& dir) {
-	coord next_king_pos = king_pos + dir;
+		//자신보다 키가 작은 사람이 있을 경우 전부 꺼내서 pair 수를 포함시킨다
+		while (false == stk.empty() && stk.back().height < p.height) {
+			pair_count += stk.back().same_height_count;
+			stk.pop_back();
+		}
 
-	//밀고 난 위치가 올바르지 않을경우 이동 취소
-	if (false == next_king_pos.valid()) {
-		return;
+		if (false == stk.empty()) {
+			//키가 같으면
+			if (stk.back().height == p.height) {
+				//합쳐준다. 키 같은사람이 또 추가되면 그만큼의 조합이 또 만들어지므로 
+				p.same_height_count += stk.back().same_height_count;
+
+				//기존의 같은키 여러명으로 만들어지는 조합을 pair에 포함.
+				pair_count += stk.back().same_height_count;
+				
+				//합쳤으므로 기존건 제거
+				stk.pop_back();
+
+				//앞 사람이 서있다면 그걸로 pair + 1
+				if (false == stk.empty()) {
+					++pair_count;
+				}
+			}
+
+			//앞사람이 나보다 키가 클 경우 +1 조합이 만들어짐
+			else {
+				++pair_count;
+			}
+		}
+
+		stk.push_back(p);
 	}
 
-	//돌을 밀지 않았으면 여기서 중단
-	if (stone_pos != next_king_pos) {
-		king_pos = next_king_pos;
-		return;
-	}
-
-	//돌을 민다
-	coord next_stone_pos = stone_pos + dir;
-
-	//돌의 밀고난 위치가 올바르지 않을경우 이동 취소
-	if (false == next_stone_pos.valid()) {
-		return;
-	}
-
-	//돌을 밀었으면 위치 갱신
-	king_pos = next_king_pos;
-	stone_pos = next_stone_pos;
-}
-
-void solve()
-{
-	directions["R"] = { 0, 1 };
-	directions["L"] = { 0, -1 };
-	directions["B"] = { -1, 0 };
-	directions["T"] = { 1, 0 };
-	directions["RT"] = { 1, 1 };
-	directions["LT"] = { 1, -1 };
-	directions["RB"] = { -1, 1 };
-	directions["LB"] = { -1, -1 };
-
-	int row; 
-	char col;
-	std::cin >> col >> row;
-	king_pos = { row - 1, (int)(col - 'A')};
-	std::cin >> col >> row;
-	stone_pos = { row - 1, (int)(col - 'A')};
-
-	int moves; std::cin >> moves;
-	while (moves--) {
-		std::string input; std::cin >> input;
-
-		push(directions[input]);
-	}
-
-	std::cout << (char)(king_pos.y + 'A') << king_pos.x + 1 << '\n'
-		<< (char)(stone_pos.y + 'A') << stone_pos.x + 1;
+	std::cout << pair_count;
 }
