2675245aa92b7ca7102416c66d542029bfe531c8
HWNOTE <ekdrn1@naver.com>
2024-10-23 14:38:18 +0900

    프로그래머스 (이중우선순위큐) : set 사용

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,70 +1,63 @@
-#ifndef LOCAL
-#define LOCAL_IO (void)0
-#endif
-#include <iostream>
-#include <limits>
-#include <cstring> //memset
+#include <string>
+#include <vector>
+#include <set>
+using namespace std;
 
-void solve();
-
-int main() {
-	std::cin.tie(nullptr); std::cin.sync_with_stdio(false); LOCAL_IO;
-	solve();
-	return 0;
+set<int> dpq;
+void insert(int i) {
+    dpq.insert(i);
 }
 
-#include <vector>
-//모두 같은 키로 50만명이 서있을 때 (50만)Combination(2) -> 1000억 넘어감
-using int64 = std::int64_t;
-struct person {
-	int64 same_height_count;
-	int height;
-};
-int N;
-std::vector<person> stk;
+void pop_max() {
+    if (dpq.empty()) { return; }
+    dpq.erase(--(dpq.end()));
+}
 
-void solve() {
-	std::cin >> N;
-	
-	stk.reserve(500000);
-	int64 pair_count = 0;
-	for (int i = 0; i < N; ++i) {
-		person p;
-		p.same_height_count = 1;
-		std::cin >> p.height;
+void pop_min() {
+    if (dpq.empty()) { return; }
+    dpq.erase(dpq.begin());
+}
 
-		//자신보다 키가 작은 사람이 있을 경우 전부 꺼내서 pair 수를 포함시킨다
-		while (false == stk.empty() && stk.back().height < p.height) {
-			pair_count += stk.back().same_height_count;
-			stk.pop_back();
-		}
+void interpret(const std::string& str) {
+    if (str[0] == 'I') {
+        std::string_view numpart(str.data() + 2, str.size() - 2);
+        int num = 0; 
+        bool is_neg = numpart[0] == '-';
+        for (size_t i = is_neg ? 1 : 0; i < numpart.size(); ++i) {
+            num *= 10;
+            num += (int)(numpart[i] - '0');
+        }
+        if (is_neg) {
+            num *= -1;
+        }
+        insert(num);
+    }
+    else if (str == "D -1") {
+        pop_min();
+    }
+    else if (str == "D 1") {
+        pop_max();
+    }
+}
 
-		if (false == stk.empty()) {
-			//키가 같으면
-			if (stk.back().height == p.height) {
-				//합쳐준다. 키 같은사람이 또 추가되면 그만큼의 조합이 또 만들어지므로 
-				p.same_height_count += stk.back().same_height_count;
+vector<int> solution(vector<string> operations) {
+    vector<int> answer(2);
 
-				//기존의 같은키 여러명으로 만들어지는 조합을 pair에 포함.
-				pair_count += stk.back().same_height_count;
-				
-				//합쳤으므로 기존건 제거
-				stk.pop_back();
+    for (const string& str : operations) {
+        interpret(str);
+    }
 
-				//앞 사람이 서있다면 그걸로 pair + 1
-				if (false == stk.empty()) {
-					++pair_count;
-				}
-			}
+    if (false == dpq.empty()) {
+        answer[0] = *(--(dpq.end()));
+        answer[1] = *(dpq.begin());
+    }
 
-			//앞사람이 나보다 키가 클 경우 +1 조합이 만들어짐
-			else {
-				++pair_count;
-			}
-		}
+    return answer;
+}
 
-		stk.push_back(p);
-	}
+int main() {
 
-	std::cout << pair_count;
+    auto ans = solution({ "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" });
+
+    return 0;
 }
