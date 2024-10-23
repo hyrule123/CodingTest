baa59024e59c2216ade1ac3eb8ad583f384dea41
HWNOTE <ekdrn1@naver.com>
2024-10-23 15:15:39 +0900

    백준 7662 (이중 우선순위 큐)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,63 +1,72 @@
-#include <string>
-#include <vector>
-#include <set>
-using namespace std;
+#ifndef LOCAL
+#define LOCAL_IO (void)0
+#endif
+#include <iostream>
+#include <limits>
+#include <cstring> //memset
 
-set<int> dpq;
-void insert(int i) {
-    dpq.insert(i);
+void solve();
+
+int main() {
+    std::cin.tie(nullptr); std::cin.sync_with_stdio(false); LOCAL_IO;
+    solve();
+    return 0;
 }
 
+//red-black tree를 사용하는 set을 통해 이중 우선순위큐 구현
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+map<int, int> dpq;
 void pop_max() {
     if (dpq.empty()) { return; }
-    dpq.erase(--(dpq.end()));
+    auto iter = --(dpq.end());
+    --(iter->second);
+    if (iter->second <= 0) {
+        dpq.erase(iter);
+    }
 }
 
 void pop_min() {
     if (dpq.empty()) { return; }
-    dpq.erase(dpq.begin());
+    auto iter = dpq.begin();
+    --(iter->second);
+    if (iter->second <= 0) {
+        dpq.erase(iter);
+    }
 }
 
-void interpret(const std::string& str) {
-    if (str[0] == 'I') {
-        std::string_view numpart(str.data() + 2, str.size() - 2);
-        int num = 0; 
-        bool is_neg = numpart[0] == '-';
-        for (size_t i = is_neg ? 1 : 0; i < numpart.size(); ++i) {
-            num *= 10;
-            num += (int)(numpart[i] - '0');
+int T, k;
+void solve() {
+    std::cin >> T;
+    while (T--) {
+        dpq.clear();
+
+        std::cin >> k;
+        while (k--) {
+            char type; int input;
+            std::cin >> type >> input;
+
+            if (type == 'I') {
+                dpq[input] += 1;
+            }
+            else {
+                if (-1 == input) {
+                    pop_min();
+                }
+                else {
+                    pop_max();
+                }
+            }
         }
-        if (is_neg) {
-            num *= -1;
+
+        if (dpq.empty()) {
+            std::cout << "EMPTY\n";
         }
-        insert(num);
+        else {
+            std::cout << (--dpq.end())->first << ' ' << (dpq.begin())->first << '\n';
+        }
     }
-    else if (str == "D -1") {
-        pop_min();
-    }
-    else if (str == "D 1") {
-        pop_max();
-    }
-}
-
-vector<int> solution(vector<string> operations) {
-    vector<int> answer(2);
-
-    for (const string& str : operations) {
-        interpret(str);
-    }
-
-    if (false == dpq.empty()) {
-        answer[0] = *(--(dpq.end()));
-        answer[1] = *(dpq.begin());
-    }
-
-    return answer;
-}
-
-int main() {
-
-    auto ans = solution({ "I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333" });
-
-    return 0;
 }
