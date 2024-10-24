e4054bb9b98577c0c33f3dc778e10d1c8797d99b
HWNOTE <ekdrn1@naver.com>
2024-10-24 22:38:51 +0900

    프로그래머스 (모음사전)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,54 +1,78 @@
 #include <string>
 #include <vector>
-#include <cmath>
-#include <bitset>
+
 using namespace std;
-
-int node_num;
-bitset<101> nodes[101];
-
-int dps(int node, int parent) {
-    int count = 1;
-    for (int i = 1; i <= node_num; ++i) {
-        if (nodes[node][i] && i != parent) {
-            count += dps(i, node);
-        }
+char ctoi(char c) {
+    switch (c)
+    {
+    case 'A': return 1;
+    case 'E': return 2;
+    case 'I': return 3;
+    case 'O': return 4;
+    case 'U': return 5;
     }
-    return count;
+    return 0;
 }
 
-int solution(int n, vector<vector<int>> wires) {
-    int answer = (int)1e9;
-
-    node_num = n;
-
-    //bitset에 집어넣는다
-    for (const vector<int>& wire : wires) {
-        nodes[wire[0]][wire[1]] = true;
-        nodes[wire[1]][wire[0]] = true;
+struct word {
+    word(const string& str) {
+        for (size_t i = 0; i < str.size(); ++i) {
+            c[i] = ctoi(str[i]);
+            if (str[i]) {
+                last_idx = (int)i;
+            }
+        }
     }
 
-    for (size_t i = 0; i < wires.size(); ++i) {
-        //확인하려는 전력망끼리 연결을 끊고
-        nodes[wires[i][0]][wires[i][1]] = false;
-        nodes[wires[i][1]][wires[i][0]] = false;
+    word& operator ++() {
+        if (last_idx < 4) {
+            ++last_idx;
+            c[last_idx] = 1;
+        }
+        else {
+            while ((++c[last_idx]) == 6) {
+                c[last_idx] = 0;
 
-        //끊은 전신주를 루트노드로 해서 연결된 전신주 갯수 계산
-        int left_count = dps(wires[i][0], 0);
-        int right_count = n - left_count;
+                if (last_idx == 0) {
+                    break;
+                }
+                --last_idx;
+            }
+        }
 
-        //차이가 제일 적은 값을 대입
-        answer = min(answer, abs(left_count - right_count));
+        return *this;
+    }
 
-        //다시 연결
-        nodes[wires[i][0]][wires[i][1]] = true;
-        nodes[wires[i][1]][wires[i][0]] = true;
+    bool operator != (const word& w) {
+        for (int i = 0; i < 5; ++i) {
+            if (c[i] != w.c[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    int last_idx = 0;
+    char c[8]{ 1, };
+};
+
+int solution(string _word) {
+    int answer = 1;
+    
+    word w(_word);
+    word A("A");
+
+    while (A != w) {
+        ++A;
+        ++answer;
     }
 
     return answer;
 }
 
 int main() {
-    auto ans = solution(9, { {4, 7}, {1, 3}, {2, 3}, {3, 4}, {4, 5}, {4, 6}, {7, 8}, {7, 9}} );
+    auto ans = solution( "AAAE" );
     return 0;
 }
