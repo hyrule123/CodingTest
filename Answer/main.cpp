ba222e4d97b067fb871e2a8705c18286a0a91355
HWNOTE <ekdrn1@naver.com>
2024-10-25 21:47:02 +0900

    프로그래머스 (N으로 표현) : DP

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,32 +1,62 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+#include <unordered_set>
 using namespace std;
 
-int solution(vector<vector<int>> routes) {
-    int answer = 0;
+//dp[i]: N을 i번 사용했을때 결과 모음
+vector<unordered_set<int>> dp;
+int make_NN_N(int N, int digits) {
+    int ret = N;
 
-    //진출 지점 기준으로 정렬하고
-    sort(routes.begin(), routes.end(),
-        [](const vector<int>& a, const vector<int>& b)->bool
-        {
-            return a[1] < b[1];
+    for (int i = 1; i < digits; ++i) {
+        ret *= 10;
+        ret += N;
+    }
+
+    return ret;
+}
+
+int solution(int N, int number) {
+    dp.resize(9);
+    
+    if (N == number) { return 1; }
+
+    //5를 한번써서 만들수있는건 N
+    dp[1].insert(N);
+
+    //최솟값이 8 이하일 경우까지만 계산(문제 조건)
+    for (int digits = 2; digits <= 8; ++digits) {
+
+        dp[digits].insert(make_NN_N(N, digits));
+
+        //ex. 3자리수일때: NNN, 1/2, 2/1
+        for (int i = 1; i < digits; ++i) {
+            for (int left : dp[i]) {
+                for (int right : dp[digits - i]) {
+                    dp[digits].insert(left + right);
+                    dp[digits].insert(left * right);
+                    
+                    //0은 안됨
+                    if (0 < left / right) {
+                        dp[digits].insert(left / right);
+                    }
+
+                    //음수는 안됨
+                    if (0 < left - right) {
+                        dp[digits].insert(left - right);
+                    }
+                }
+            }
         }
-        );
 
-    //진입 지점 기준으로 비교
-    int last_cam_pos = -30001;
-    for (size_t i = 0; i < routes.size(); ++i) {
-        //마지막으로 설치한 카메라 위치가 진입지점보다 뒤쪽일 경우 진출 지점쪽에 새로 설치해야 한다
-        if (last_cam_pos < routes[i][0]) {
-            last_cam_pos = routes[i][1];
-            ++answer;
+        if (dp[digits].find(number) != dp[digits].end()) {
+            return digits;
         }
     }
 
-    return answer;
+    return -1;
 }
 int main() {
-    auto ans = solution({ {-20, -15}, {-14, -5}, {-18, -13}, {-5, -3} });
+    auto ans = solution(5, 12);
     return 0;
 }
