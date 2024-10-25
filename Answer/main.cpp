d93d720ac122364113e1667fea089b739a30ab43
HWNOTE <ekdrn1@naver.com>
2024-10-26 06:43:58 +0900

    프로그래머스 (게임 맵 최단거리): 단순 BFS - 시간초과로 실패

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,36 +1,56 @@
-#include <string>
 #include <vector>
+#include <queue>
 using namespace std;
-vector<bool> visited;
 
-void recursive(int cur, int parent, const vector<vector<int>>& nodes) {
-    if (visited[cur]) { return; }
+int end_x, end_y;
+struct coord {
+    int x, y, dist;
+    coord operator +(const coord& other) {
+        return { x + other.x, y + other.y };
+    }
+    bool operator == (const coord& other) const {
+        return { x == other.x && y == other.y };
+    }
+    bool valid() {
+        return (0 <= x && 0 <= y && x <= end_x && y <= end_y);
+    }
+};
+constexpr coord directions[] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+constexpr int numdir = sizeof(directions) / sizeof(coord);
 
-    visited[cur] = true;
+int solution(vector<vector<int>> maps)
+{
+    int answer = 0;
+    end_x = (int)maps.size() - 1; end_y = (int)maps[0].size() - 1;
 
-    for (int i = 0; i < (int)nodes[cur].size(); ++i) {
-        if (i == parent || i == cur) { continue; }
+    queue<coord> q;
+    q.push({ 0, 0, 0 });
+    while (false == q.empty()) {
+        coord c = q.front(); q.pop();
 
-        if (nodes[cur][i] == 1) {
-            recursive(i, cur, nodes);
+        maps[c.x][c.y] = c.dist - 1;
+        if (c.x == end_x && c.y == end_y) {
+            answer = -maps[c.x][c.y];
+            break;
+        }
+
+        for (int i = 0; i < numdir; ++i) {
+            coord next = c + directions[i];
+            next.dist = c.dist - 1;
+            if (false == next.valid() || maps[next.x][next.y] != 1) { continue; }
+            q.push(next);
         }
     }
-}
 
-int solution(int n, vector<vector<int>> computers) {
-    int answer = 0;
-    visited.resize(computers.size(), false);
-    for (size_t i = 0; i < computers.size(); ++i) {
-        if (visited[i] == false) {
-            ++answer;
-            recursive((int)i, -1, computers);
-        }
+    if (maps[end_x][end_y] == 1) {
+        answer = -1;
     }
 
     return answer;
 }
 
+
 int main() {
-    auto ans = solution(3, {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}} );
+    auto ans = solution({{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}} );
     return 0;
 }
