50e755e458ff5706905ed6cd50ebfef71d2006a1
HWNOTE <ekdrn1@naver.com>
2024-10-26 07:59:12 +0900

    프로그래머스 (단어 변환) : BFS

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,51 +1,69 @@
+#include <string>
 #include <vector>
+#include <unordered_map>
 #include <queue>
 using namespace std;
-
-int end_x, end_y;
-struct coord {
-    int x, y, dist;
-    coord operator +(const coord& other) const {
-        return { x + other.x, y + other.y };
-    }
-    bool invalid() {
-        return x < 0 || y < 0 || end_x < x || end_y < y;
-    }
-};
-constexpr coord directions[] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-constexpr int numdir = sizeof(directions) / sizeof(coord);
-
-int solution(vector<vector<int>> maps)
+template <class CharType, class Traits>
+struct hash<basic_string_view<CharType, Traits>>
 {
-    end_x = (int)maps.size() - 1; end_y = (int)maps[0].size() - 1;
+    typedef basic_string_view<CharType, Traits> argument_type;
+    typedef size_t result_type;
 
-    vector<vector<int>> dist;
-    queue<coord> q;
-    q.push({ 0, 0, 0 });
+    size_t operator()(const basic_string_view<CharType, Traits>) const
+        noexcept;
+};
+struct node {
+    int dist = -1;
+    vector<string_view> convert_to;
+};
+
+unordered_map<string_view, node> nodes;
+
+int solution(string begin, string target, vector<string> words) {
+    words.push_back(begin);
+    const size_t convertable = words[0].size() - 1;
+    for (size_t i = 0; i < words.size(); ++i) {
+        for (size_t j = i + 1; j < words.size(); ++j) {
+            size_t matches = 0;
+            for (size_t k = 0; k < words[i].size(); ++k) {
+                matches += (size_t)(words[i][k] == words[j][k]);
+            }
+            if(matches == convertable){
+                nodes[words[i]].convert_to.push_back(words[j]);
+                nodes[words[j]].convert_to.push_back(words[i]);
+            }
+        }
+    }
+    
+    //node 포인터를 보관
+    struct travel {
+        string_view key; int dist;
+    };
+    queue<travel> q;
+    nodes[begin].dist = 0;
+    q.push({ begin, -1 });
     while (false == q.empty()) {
-        const coord& c = q.front();
+        travel t = q.front(); q.pop();
 
-        maps[c.x][c.y] = c.dist - 1;
-        if (c.x == end_x && c.y == end_y) {
-            return -maps[c.x][c.y];
+        node& cur = nodes[t.key];
+        cur.dist += t.dist + 1;
+        if (t.key == target) {
+            return cur.dist;
         }
 
-        for (int i = 0; i < numdir; ++i) {
-            coord next = c + directions[i];
-            if (next.invalid() || maps[next.x][next.y] != 1) { continue; }
-            maps[next.x][next.y] = 0;
-            next.dist = c.dist - 1;
-            q.push(next);
+        for (const string_view& next_name : cur.convert_to) {
+            node& next = nodes[next_name];
+            if (next.dist != -1) { continue; }
+            next.dist = 0;
+            q.push({ next_name, cur.dist });
         }
-
-        q.pop();
     }
 
-    return -1;
+
+    return 0;
 }
 
-
 int main() {
-    auto ans = solution({{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}} );
+    auto ans = solution("hit", "cog", { "hot", "dot", "dog", "lot", "log", "cog" });
     return 0;
 }
