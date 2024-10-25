60e75f9d3bbef962a601639dae2a6ea370d55381
HWNOTE <ekdrn1@naver.com>
2024-10-25 23:20:47 +0900

    프로그래머스 (등굣길) : DP

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -2,31 +2,38 @@
 #include <vector>
 using namespace std;
 
-int clamp(int i, int min, int max) {
-    if (i < min) { return min; }
-    if (max <= i) { return max - 1; }
+constexpr int p = 1'000'000'007;
+constexpr int BLOCK = -1;
+vector<vector<int>> memo;
 
-    return i;
-}
+//m = col, n = row
+int solution(int m, int n, vector<vector<int>> puddles) {
+    memo.resize(n + 1, vector<int>((size_t)(m + 1)));
+    for (const vector<int>& pud : puddles) {
+        memo[pud[1]][pud[0]] = BLOCK;
+    }
 
-int solution(vector<vector<int>> triangle) {
-    int answer = 0;
-
-    //역으로 생각해서 윗쪽의 좌/우 값중 큰걸 취사선택
-    for (int i = 1; i < (int)triangle.size(); ++i) {
-        for (int j = 0; j < (int)triangle[i].size(); ++j) {
-            int left = clamp(j - 1, 0, (int)triangle[i - 1].size());
-            int right = clamp(j, 0, (int)triangle[i - 1].size());
-            //부모: j-1, j 중 하나
-            triangle[i][j] += max(triangle[i - 1][left], triangle[i - 1][right]);
-            answer = max(answer, triangle[i][j]);
+    //[1][1]을 1로 만들어주기 위해서 설정
+    memo[0][1] = 1;
+    for (int r = 1; r <= n; ++r) {
+        for (int c = 1; c <= m; ++c) {
+            //BLOCK은 이동 불가, BLOCK으로부터 오는것도 불가
+            if (memo[r][c] != BLOCK) {
+                if (memo[r - 1][c] != BLOCK) {
+                    memo[r][c] += memo[r - 1][c];
+                }
+                if (memo[r][c - 1] != BLOCK) {
+                    memo[r][c] += memo[r][c - 1];
+                }
+                memo[r][c] %= p;
+            }
         }
     }
 
-    return answer;
+    return memo[n][m];
 }
 
 int main() {
-    auto ans = solution({{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}} );
+    auto ans = solution(4, 3, { {2, 2} });
     return 0;
 }
