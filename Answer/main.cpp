f24e8a8d87f6bfaa14d2160bb589c7f2476b6e1e
HWNOTE <ekdrn1@naver.com>
2024-10-25 19:22:11 +0900

    프로그래머스 (구명보트)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -1,35 +1,30 @@
 #include <string>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
-string solution(string number, int k) {
-    string answer = "";
-    size_t ans_size = number.size() - k;
-    answer.resize(ans_size);
+int solution(vector<int> people, int limit) {
+    int boat_count = 0;
 
-    //number에서 탐색중인 인덱스 자리
-    size_t cursor = 0;
-
-    //한글자씩 채워나간다.
-    for (size_t i = 0; i < ans_size; ++i) {
-        size_t max_idx = cursor;
-        
-        //건너뛸 수 있는 최대 자리는 i + k개(현재 위치로부터 K개)
-        //시작 인덱스는 마지막 커서 자리 + 1
-        for (size_t j = cursor + 1; j <= i + k; ++j) {
-            if (number[max_idx] < number[j]) {
-                max_idx = j;
-            }
+    sort(people.begin(), people.end());
+    
+    int l = 0, r = (int)people.size() - 1;
+    while (l <= r) {
+        if (people[l] + people[r] <= limit) {
+            ++l; --r;
+        }
+        //둘이 합해서 limit를 넘을 경우 무거운 승객을 혼자 태워 보낸다
+        else {
+            --r;
         }
 
-        cursor = max_idx + 1;
-        answer[i] = number[max_idx];
+        ++boat_count;
     }
 
-    return answer;
+    return boat_count;
 }
+
 int main() {
-    auto ans = solution("179252841", 6);
+    auto ans = solution({70, 80, 50 }, 100);
     return 0;
 }
