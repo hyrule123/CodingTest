2e598b4b279d5a894026bad657700eaecadf4359
HWNOTE <ekdrn1@naver.com>
2024-10-31 10:14:52 +0900

    백준 7869 (두 원) : 기하

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,116 +14,66 @@ int main() {
 	return 0;
 }
 
-#include <vector>
-#include <algorithm>
-struct vec2 {
-	double x, y;
+//필요한 지식
+//1. 코사인 법칙으로 각도 구하기
+//2. 이등변삼각형의 넓이 구하기
+//2-1. 삼각형의 합차 공식
+#include <cmath>
+constexpr double pi = 3.14159265358979, _2pi = pi * 2;
 
-	auto operator <=> (const vec2& other) const {
-		if (x == other.x) {
-			return y <=> other.y;
-		}
-		return x <=> other.x;
-	}
-
-	auto operator == (const vec2& other) const {
-		return x == other.x && y == other.y;
-	}
-	auto operator != (const vec2& other) const {
-		return !(*this == other);
-	}
-	double cross(const vec2& other) const {
-		return x * other.y - other.x * y;
-	}
-	vec2 operator - (const vec2& other) const {
-		return { x - other.x, y - other.y };
-	}
-
-	static double ccw(const vec2& from, const vec2& std, const vec2& a, const vec2& b) {
-		vec2 standard = std - from;
-		return (standard.cross(a - from) * standard.cross(b - from));
-	}
-};
-
-struct line_seg {
-	int group, rank = 0, size = 1;
-	vec2 p[2];
-};
-
-//v1 실패 원인: biggest_group_size를 0으로 잡았음
-int N, num_group, biggest_group_size = 1;
-vector<line_seg> lines;
-
-int find_group(int node) {
-	if (lines[node].group != node) {
-		lines[node].group = find_group(lines[node].group);
-	}
-	return lines[node].group;
-}
-void union_group(int na, int nb) {
-	int pa = find_group(na);
-	int pb = find_group(nb);
-
-	if (pa == pb) { return; }
-
-	//union이 발생했으므로 집합 개수 1개 감소
-	//v1 실패 원인: pa, pb를 사용한것이 아니라 na, nb를 사용
-	--num_group;
-	if (lines[pa].rank < lines[pb].rank) {
-		lines[pa].group = pb;
-
-		//사이즈 병합
-		lines[pb].size += lines[pa].size;
-		biggest_group_size = max(biggest_group_size, lines[pb].size);
-	}
-	else {
-		lines[pb].group = pa;
-		lines[pa].size += lines[pb].size;
-		biggest_group_size = max(biggest_group_size, lines[pa].size);
-	}
-
-	if (lines[pa].rank == lines[pb].rank) {
-		++(lines[pa].rank);
-	}
+//코사인 법칙 활용
+//c^2 = a^2 + b^2 - 2ab * cos(theta)
+//cos(theta) = (a^2 + b^2 - c^2) / 2ab
+double get_theta(double opposite_edge, double a, double b) {
+	return acos((a * a + b * b - opposite_edge * opposite_edge) / (2 * a * b));
 }
 
 void solve() {
-	cin >> N;
-	num_group = N;
-	lines.resize(N);
-	for (int i = 0; i < N; ++i) {
-		line_seg& l = lines[i];
-		cin >> l.p[0].x >> l.p[0].y >> l.p[1].x >> l.p[1].y;
-		if (l.p[0] > l.p[1]) {
-			swap(l.p[0], l.p[1]);
-		}
-		l.group = i;
+	double x1, y1, r1, x2, y2, r2;
+	cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+	
+	//원과 원 사이의 거리
+	double dist_x = x2 - x1, dist_y = y2 - y1;
+	double d = sqrt(dist_x * dist_x + dist_y * dist_y);
+	
+	//접점이 한곳이거나 겹치지 않는 경우 스킵
+	double output = 0;
+	if (r1 + r2 <= d) {
+		output = 0.0;
+	}
+	//한 원이 다른 원 안에 포함되는 경우
+	else if(d <= abs(r1 - r2)){
+		double small = min(r1, r2);
+		output = pi * small * small;
+	}
+	//일부만 겹치는 경우
+	else {
+		//1. 위 아래 삼각형으로 좌측 theta, 우측 theta 구한다.
+		double
+			theta_1 = 2 * get_theta(r2, r1, d),
+			theta_2 = 2 * get_theta(r1, r2, d);
+
+		//2. 좌우 부채꼴의 넓이를 구한다
+		double
+			sector_1 = pi * r1 * r1 * (theta_1 / _2pi),
+			sector_2 = pi * r2 * r2 * (theta_2 / _2pi);
+
+		//3. 좌우 삼각형의 넓이를 구한다.
+		//유도과정
+		//3-1. 좌우 삼각형은 각각 두 변이 반지름인 이등변삼각형
+		//3-2. 반으로 잘라서 직각삼각형을 만들면 삼각함수를 통해 높이 h와 빗변 o/2를 구할수 있다
+		//3-3. h * o / 2 == 삼각형의 넓이 == a * cos(theta/2) * 2a * sin(theta/2) / 2
+		//3-4. 삼각함수의 합차공식을 사용하여 정리하면 
+		//a^2 * sin(theta) / 2
+		double
+			isosceles_1 = r1 * r1 * sin(theta_1) / 2.0,
+			isosceles_2 = r2 * r2 * sin(theta_2) / 2.0;
+
+		//4. 부채꼴 1 + 부채꼴 2 - 삼각형 1 - 삼각형 2 = 겹치는 부분
+		double ans = sector_1 + sector_2 - isosceles_1 - isosceles_2;
+		ans *= 1000.0;
+		output = round(ans) / 1000.0;
 	}
 
-	for (int i = 0; i < N; ++i) {
-		for (int j = i + 1; j < N; ++j) {
-			line_seg& l = lines[i], & r = lines[j];
-			
-			double
-				ccw1 = vec2::ccw(l.p[0], l.p[1], r.p[0], r.p[1]),
-				ccw2 = vec2::ccw(r.p[0], r.p[1], l.p[0], l.p[1]);
-
-			//같은 직선 위의 점
-			if (ccw1 == 0.0 && ccw2 == 0.0) {
-				bool no_intersect1 = l.p[0] > r.p[1];
-				bool no_intersect2 = r.p[0] > l.p[1];
-
-				//둘이 교차한다 -> 그룹 통일
-				if (!(no_intersect1 || no_intersect2)) {
-					union_group(i, j);
-				}
-			}
-			//서로 한 점에서 교차 -> 그룹 통일
-			else if (ccw1 <= 0.0 && ccw2 <= 0.0) {
-				union_group(i, j);
-			}
-		}
-	}
-
-	cout << num_group << '\n' << biggest_group_size;
+	printf("%.3lf", output);
 }
