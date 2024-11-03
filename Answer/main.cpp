e71f7626bfe6645333e448c49a4315d2b3c12160
HWNOTE <ekdrn1@naver.com>
2024-11-04 06:04:59 +0900

    백준 1019 (책 페이지) 브루트 포스 사용(당연히 안됨)

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,61 +14,24 @@ int main() {
 	return 0;
 }
 
-//필요한 지식
-//1. 코사인 법칙으로 각도 구하기
-//2. 이등변삼각형의 넓이 구하기
-//2-1. 삼각형의 합차 공식
-#include <cmath>
-constexpr double pi = 3.14159265358979, _2pi = pi * 2;
+int N, result[10];
 
-//코사인 법칙 활용
-//c^2 = a^2 + b^2 - 2ab * cos(theta)
-//cos(theta) = (a^2 + b^2 - c^2) / 2ab
-double get_theta(double opposite_edge, double a, double b) {
-	return acos((a * a + b * b - opposite_edge * opposite_edge) / (2 * a * b));
+void calc(int i) {
+	while (i >= 10) {
+		++(result[i % 10]);
+		i /= 10;
+	}
+	++(result[i % 10]);
 }
 
 void solve() {
-	double x1, y1, r1, x2, y2, r2;
-	cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
-	
-	//원과 원 사이의 거리
-	double dist_x = x2 - x1, dist_y = y2 - y1;
-	double d = sqrt(dist_x * dist_x + dist_y * dist_y);
-	
-	//접점이 한곳이거나 겹치지 않는 경우 스킵
-	double output = 0;
-	if (r1 + r2 <= d) {
-		output = 0.0;
-	}
-	//한 원이 다른 원 안에 포함되는 경우
-	else if(d <= abs(r1 - r2)){
-		double small = min(r1, r2);
-		output = pi * small * small;
-	}
-	//일부만 겹치는 경우
-	else {
-		//1. 위 아래 삼각형으로 좌측 theta, 우측 theta 구한다.
-		double
-			theta_1 = 2 * get_theta(r2, r1, d),
-			theta_2 = 2 * get_theta(r1, r2, d);
-
-		//2. 좌우 부채꼴의 넓이를 구한다
-		double
-			sector_1 = pi * r1 * r1 * (theta_1 / _2pi),
-			sector_2 = pi * r2 * r2 * (theta_2 / _2pi);
-
-		//3. 좌우 (이등변)삼각형의 넓이를 구한다.
-		//합차공식을 사용하면 아래 코드보다 더 간단한 연산으로도 가능함.
-		double
-			isosceles_1 = r1 * cos(theta_1 / 2.0) * sqrt(2 * r1 * r1 * (1.0 - cos(theta_1))) / 2.0,
-			isosceles_2 = r2 * cos(theta_2 / 2.0) * sqrt(2 * r2 * r2 * (1.0 - cos(theta_2))) / 2.0;
-
-		//4. 부채꼴 1 + 부채꼴 2 - 삼각형 1 - 삼각형 2 = 겹치는 부분
-		double ans = sector_1 + sector_2 - isosceles_1 - isosceles_2;
-		ans *= 1000.0;
-		output = round(ans) / 1000.0;
+	cin >> N;
+	while (1 <= N) {
+		calc(N);
+		--N;
 	}
 
-	printf("%.3lf", output);
+	for (int i : result) {
+		cout << i << ' ';
+	}
 }
