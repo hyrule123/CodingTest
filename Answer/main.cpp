894d00d365a5d895f80aacbe46141fed2d2158a1
HWNOTE <ekdrn1@naver.com>
2024-11-09 09:25:26 +0900

    백준 2482 (색상환) [DP]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,45 +14,35 @@ int main() {
 	return 0;
 }
 
-//마지막 집을 고정시키고 
-constexpr int r = 0, g = 1, b = 2, INF = 987654321;
-int N;
-int dp[1001][3], costs[1001][3];
+int N, K;
+int dp[1001][1001];	//dp[n][k] = 갯수 n개의 색상환에서 k개를 칠하는 방법의 수
+constexpr int mod = 1'000'000'003;
 
 void solve()
 {
-	cin >> N;
+	cin >> N >> K;
 
-	for (int i = 1; i <= N; ++i) {
-		cin >> costs[i][r] >> costs[i][g] >> costs[i][b];
+	for (int i = 0; i <= N; ++i) {
+		dp[i][0] = 1;
+		dp[i][1] = i;//i개의 색상환에서 1개만 색칠하는 방법은 i개
 	}
 
-	int ans = INF;
-	for (int start = 0; start < 3; ++start) {
-		memset(dp, 0, sizeof(dp));
-
-		//시작점을 하나로 고정
-		for (int i = 0; i < 3; ++i) {
-			if (start == i) {
-				dp[1][i] = costs[1][start];
-			}
-			else {
-				dp[1][i] = INF;
-			}
-		}
-
-		for (int i = 2; i <= N; ++i) {
-			dp[i][r] = min(dp[i - 1][g], dp[i - 1][b]) + costs[i][r];
-			dp[i][g] = min(dp[i - 1][r], dp[i - 1][b]) + costs[i][g];
-			dp[i][b] = min(dp[i - 1][r], dp[i - 1][g]) + costs[i][b];
-		}
-
-		for (int end = 0; end < 3; ++end) {
-			//시작점과 동일하게 끝난 값은 무의미
-			if (start == end) { continue; }
-			ans = min(ans, dp[N][end]);
+	for (int i = 2; i <= N; ++i) {
+		for (int j = 2; j <= K; ++j) {
+			//이번 색상 미사용: dp[i-1][j]
+			//이번 색상 사용: dp[i-2][j-1](바로 전 색상은 칠해져있으면 안되므로 전전 색상의 경우의 수를 가져온다)
+			dp[i][j] = (dp[i - 1][j] + dp[i - 2][j - 1]) % mod;
 		}
 	}
 
+	//[N] 사용하는 경우: [1], [N-1] 사용 불가
+	//[2] ~ [N - 2]범위를 칠하는 경우의 수와 같다. 
+	//[1] ~ [N - 3] == (N - 3)개의 색상환에서 (K - 1)개를 칠하는 방법의 수
+	//dp[N - 3][K - 1]
+	
+	//[N] 미사용: [1]의 사용 여부와 관계 없이 [N - 1][K]까지 색칠이 완료되어야 함
+	//dp[N - 1][K]
+	
+	int ans = (dp[N - 1][K] + dp[N - 3][K - 1]) % mod;
 	cout << ans;
 }
