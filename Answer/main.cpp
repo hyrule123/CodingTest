615c805dd0c8c978a2c6fff2b71601b073d201a7
HWNOTE <ekdrn1@naver.com>
2024-11-12 13:25:59 +0900

    백준 (찾기) [KMP][문자열]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,85 +14,71 @@ int main() {
 	return 0;
 }
 
-#include <queue>
+#include <string>
+#include <vector>
+constexpr int max_len = 1'000'001;
+int lenT, lenP;
+char T[max_len], P[max_len];
+int Pi[max_len];	//KMP 알고리즘의 pi 배열: P[i]까지 문자열에서 찾을 수 있는 prefix == suffix 가장 긴 길이)
 
-struct edge {
-	int to, dist;
-	bool operator < (const edge& o) const { return dist > o.dist; }
-};
+void calc_last_match() {
+	int prefix = 0;	//previous longest prefix len
+	for (int suffix = 1; suffix < lenP; ++suffix) {
 
-int N, E, v1, v2;
-vector<edge> edges[801];
-priority_queue<edge> pq;
-int memo[801];
-constexpr int INF = numeric_limits<int>::max();
-
-//from으로부터 모든 노드 사이의 거리를 계산
-void djikstra(int from) {
-	//기본 container로 vector 사용하므로 재사용
-	while (false == pq.empty()) {
-		pq.pop();
-	}
-	for (int& i : memo) {
-		i = INF;
-	}
-
-	pq.push({ from, 0 });
-	memo[from] = 0;
-
-	while (false == pq.empty()) {
-		edge t = pq.top(); pq.pop();
-
-		for (const edge& next : edges[t.to]) {
-			int next_dist = t.dist + next.dist;
-
-			//삽입 조건 확인
-			if (memo[next.to] > next_dist) {
-				memo[next.to] = next_dist;
-				pq.push({ next.to, next_dist });
-			}
+		//suffix 문자가 prefix 문자와 일치하지 않으면 Pi 배열을 참조하여 줄여나간다.
+		//prefix = Pi[prefix - 1] 인 이유
+		//AAAB'AAA(3) -> AAAB'AAAA
+		//지난 prefix길이: 3(AAA)
+		//문자열 추가, 패턴 일치 확인 -> AAAB != AAAA(불일치)
+		//지난 prefix 중 다음으로 긴 걸 가져온다: Pi[3 - 1] -> "AAA" 문자열에서 일치하는 prefix와 suffix -> "AA"(2)
+		while (0 < prefix && P[prefix] != P[suffix]) {
+			prefix = Pi[prefix - 1];
 		}
+
+		if (P[suffix] == P[prefix]) {
+			Pi[suffix] = ++prefix;
+		}
+		//else Pi[suffix] = 0
 	}
 }
 
-//1->v1 v1->v2 v2->N, N->v1 v1->v2 v2->N
+void KMP() {
+	int matches = 0;
+	vector<int> match_idx{};
+
+	int j = 0;
+	for (int i = 0; i < lenT; ++i) {
+		//패턴 불일 치 시 그 다음으로 긴 prefix를 가져온다.
+		while (0 < j && T[i] != P[j]) {
+			j = Pi[j - 1];
+		}
+
+		//문자 일치 시
+		if (T[i] == P[j]) {
+			//전체 패턴 일치 시 답에 추가하고 부분일치 인덱스를 찾아 다시 이동
+			if (lenP - 1 == j) {
+				++matches;
+				match_idx.push_back(i - lenP + 2);	//답은 1부터 시작
+				j = Pi[j];
+			}
+			else {
+				++j;
+			}
+		}
+	}
+
+	cout << matches << '\n';
+	for (int idx : match_idx) {
+		cout << idx << ' ';
+	}
+}
+
 void solve() {
-	cin >> N >> E;
-	for (int i = 0; i < E; ++i) {
-		int a, b, c; cin >> a >> b >> c;
-		edges[a].push_back({ b, c });
-		edges[b].push_back({ a, c });
-	}
-	cin >> v1 >> v2;
-
-	djikstra(v1);
-	int
-		v1_1 = memo[1],
-		v1_N = memo[N],
-		v1_v2 = memo[v2];
-
-	djikstra(v2);
-	int
-		v2_1 = memo[1],
-		v2_N = memo[N];
-
-	int path_1 = -1, path_2 = -1;
-
-	if (v1_v2 == INF) {
-		cout << -1;
-		return;
-	}
-
-	if (INF != v1_1 && INF != v2_N) {
-		path_1 = v1_1 + v1_v2 + v2_N;
-	}
-	if (INF != v2_1 && INF != v1_N) {
-		path_2 = v2_1 + v1_v2 + v1_N;
-	}
-
-	if (path_1 == -1) { cout << path_2; }
-	else if (path_2 == -1) { cout << path_1; }
-	else {
-		cout << (path_1 < path_2 ? path_1 : path_2);
-	}
+	cin.getline(T, max_len);
+	lenT = (int)strlen(T);
+	cin.getline(P, max_len);
+	lenP = (int)strlen(P);
+	
+	calc_last_match();
+	KMP();
 }
