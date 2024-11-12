da484ef5e78a467e0d65a316a73fe743f1883303
HWNOTE <ekdrn1@naver.com>
2024-11-12 19:36:17 +0900

    백준 14425 (문자열 집합) [문자열][trie][동적할당 + 재귀 ver(1091156kb, 1532ms)]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,67 +14,68 @@ int main() {
 	return 0;
 }
 
-#include <list>
-#include <vector>
-#include <string>
-#include <map>
+#include <unordered_set>
+constexpr int alphabets = 'z' - 'a' + 1;
 
 struct trie {
-	void add_child(int _level, const vector<string_view>& branch) {
-		if (level < _level) {
-			trie& child = childs[branch[level]];
-
-			//새로 생성 되었다면
-			if (-1 == child.level) {
-				child.level = level + 1;
-				child.s = branch[level];
-			}
-
-			child.add_child(_level, branch);
+	~trie() {
+		for (auto* p : childs) {
+			delete p;
 		}
 	}
-	
-	void print_recursive() const {
-		
-		for (int i = 2; i <= level; ++i) {
-			cout << "--";
+
+	void add(const string& s) {
+		if (level == (int)s.size()) {
+			is_eow = true;
+			return;
 		}
 
-		if (0 < level) {
-			cout << s << '\n';
+		if (level < (int)s.size()) {
+			trie*& next = childs[s[level] - 'a'];
+
+			if (next == nullptr) {
+				next = new trie;
+				next->level = level + 1;
+			}
+
+			next->add(s);
+		}
+	}
+
+	bool find(const string& s) {
+		//종료 조건
+		if (is_eow && level == (int)s.size()) {
+			return true;
 		}
 
-		for (const auto& child : childs) {
-			child.second.print_recursive();
+		trie*& next = childs[s[level] - 'a'];
+		if (next) {
+			return next->find(s);
 		}
+
+		return false;
 	}
 
 	int level = -1;
-	string_view s;
-	map<string_view, trie> childs;
+	bool is_eow = false;
+	trie* childs[alphabets]{};
 };
 
 void solve() {
 	trie root;
 	root.level = 0;
-
-	int N; cin >> N;
-
-	//string은 list에 저장해두고 string_view를 사용
-	list<string> strings;
-
-	for (int i = 0; i < N; ++i) {
-		int level;
-		vector<string_view> branches;
-		cin >> level;
-		for (int i = 1; i <= level; ++i) {
-			string s; cin >> s;
-			strings.push_back(s);
-			branches.push_back(strings.back());
-		}
-
-		root.add_child(level, branches);
+	int N, M; cin >> N >> M;
+	while (N--) {
+		string s; cin >> s;
+		root.add(s);
 	}
 
-	root.print_recursive();
+	int count = 0;
+	while (M--) {
+		string s; cin >> s;
+		if (root.find(s)) {
+			++count;
+		}
+	}
+	cout << count;
 }
