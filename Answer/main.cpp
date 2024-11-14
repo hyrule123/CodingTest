732c6732e79b1f2f24be7cf19445a825cabdac0b
HWNOTE <ekdrn1@naver.com>
2024-11-14 15:57:40 +0900

    백준 2252 (줄 세우기) [위상정렬]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -15,75 +15,51 @@ int main() {
 }
 
 #include <vector>
-#include <iomanip>
-#include <cmath>
-
-constexpr int alphabets = 'z' - 'a' + 1;
-
-struct trie {
-	bool is_word_end;
-	char num_childs;	//등록된 자식의 수
-	int childs[alphabets];
+#include <queue>
+struct node {
+	int in_degree{};
+	vector<int> edges;
 };
 
-int N, allocater_idx = 0;
-vector<string> inputs;
+int N, M;
+node nodes[32001];
 
-//단어의 총합 길이가 10e6을 넘지 않는다고 했으므로 정적 배열을 사용
-trie tries[(int)1e6 + 1];
-int new_trie() { return ++allocater_idx; }
+void topology_sort(queue<int>& q, vector<int>& result) {
+	while (false == q.empty()) {
+		int n = q.front(); q.pop();
 
-void clear() {
-	allocater_idx = 0;
-	memset(tries, 0, sizeof(tries));
-	inputs.clear();
-}
+		result.push_back(n);
 
-void add_trie(const string& str) {
-	int cur = 0;
-	for (size_t i = 0; i < str.size(); ++i) {
-		int& child_idx = tries[cur].childs[str[i] - 'a'];
-		if (0 == child_idx) {
-			++(tries[cur].num_childs);
-			child_idx = new_trie();
+		for (int next : nodes[n].edges) {
+			--(nodes[next].in_degree);
+			if (0 == nodes[next].in_degree) {
+				q.push(next);
+			}
 		}
-		cur = child_idx;
 	}
-	tries[cur].is_word_end = true;
-}
-
-int get_input_count(const string& str) {
-	int count = 1;	//첫 분기 입력은 무조건 수동
-
-	int cur = tries[0].childs[str[0] - 'a'];
-	for (size_t i = 1; i < str.size(); ++i) {
-		//(안 끝났는데) 단어의 끝 발견 시 또는 분기 발견 시 +1
-		if (tries[cur].is_word_end || 1 < tries[cur].num_childs) {
-			++count;
-		}
-		cur = tries[cur].childs[str[i] - 'a'];
-	}
-
-	return count;
 }
 
 void solve() {
-	cout << fixed << setprecision(2);
-	while (cin >> N) {
-		clear();
+	cin >> N >> M;
+	for (int i = 1; i <= M; ++i) {
+		int a, b; cin >> a >> b;
+		++(nodes[b].in_degree);	//진입차수를 기록
+		nodes[a].edges.push_back(b);
+	}
 
-		inputs.resize(N);
-		for (int i = 0; i < N; ++i) {
-			inputs[i].reserve(80);
-			cin >> inputs[i];
-			add_trie(inputs[i]);
+	queue<int> q;
+	vector<int> ans;
+	for (int i = 1; i <= N; ++i) {
+		//진입차수가 0인 노드를 큐에 삽입
+		if (0 == nodes[i].in_degree) {
+			q.push(i);
 		}
+	}
 
-		int count = 0;
-		for (const string& s : inputs) {
-			count += get_input_count(s);
-		}
-		double ans = (double)count / (double)N;
-		cout << round(ans * 100.0) / 100.0 << '\n';
+	//정렬
+	topology_sort(q, ans);
+
+	for (int order : ans) {
+		cout << order << ' ';
 	}
 }
