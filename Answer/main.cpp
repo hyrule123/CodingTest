a1ee46e92fdc530af85abb04a6ff7fb3e36adcc8
HWNOTE <ekdrn1@naver.com>
2024-11-16 22:53:01 +0900

    백준 1002 (터렛) [epsilon 사용]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,46 +14,76 @@ int main() {
 	return 0;
 }
 
-//백준 17435 (합성함수와 쿼리) [LCA 기초지식]
+//백준 1002 (터렛)[epsilon 사용]
+//epsilon을 사용하거나
+//sqrt를 하지 않고 제곱된 상태를 그대로 사용하거나
+//double을 사용하거나 해서 오차를 줄여야 한다
 #include <cmath>
+constexpr double epsilon = 2.2204460492503131e-016;
+struct vec2 {
+	double x, y;
 
-//f[n][x]
-//n: 이진법 비트시프트 횟수(1<<1, 1<<2, ...1<<19)
-//십진법일 때
-// f[8][x] = f[4][f[4][x]] = f[4][f[2][f[2][x]]] = f[4][f[2][f[1][f[1][x]]]]...
-// f[1<<3]... = f[1<<2]... = f[1<<1]... f[1<<0]...
-constexpr int bin_lift = 19;
-int m, f[bin_lift][200001], Q;
+	bool operator ==(const vec2& other) const {
+		return x == other.x && y == other.y;
+	}
+
+	vec2 operator -(const vec2& other) const {
+		return { x - other.x, y - other.y };
+	}
+
+	double length(const vec2& other) const {
+		vec2 v = (*this) - other;
+		return sqrt(v.x * v.x + v.y * v.y);
+	}
+};
 
 void solve() {
-	//double l2 = ceil(log2(500000)); //19
+	int T; cin >> T;
+	while (T--) {
+		double ra, rb;
+		vec2 a, b; cin >> a.x >> a.y >> ra >> b.x >> b.y >> rb;
 
-	cin >> m;
-	for (int i = 1; i <= m; ++i) {
-		cin >> f[0][i];
-	}
-
-	//precompute
-	for (int i = 1; i < bin_lift; ++i) {
-		for (int j = 1; j <= m; ++j) {
-			f[i][j] = f[i - 1][f[i - 1][j]];
+		//반지름이 큰 원을 b로 보낸다
+		if (rb < ra) {
+			swap(a, b);
+			swap(ra, rb);
 		}
-	}
-	
 
-	cin >> Q;
-	for (int i = 0; i < Q; ++i) {
-		int n, x; cin >> n >> x;
+		//동일한 원일 경우 무한대
+		if (a == b && ra == rb) {
+			cout << -1 << '\n';
+			continue;
+		}
 
-		//미리 계산된 데이터를 활용, 값을 구함
-		for (int j = 0; j < 19; ++j) {
-			int bit = 1 << j;
-			if (n < bit) { break; }
-			if (n & (1 << j)) {
-				x = f[j][x];
+		double dist = a.length(b);
+
+		//한 원의 중심이 다른 원 안에 속할때
+		if (dist < rb) {
+			//한 원이 아예 다른 원 안에 속할때: 말이안됨(0개)
+			if (dist + ra < rb) {
+				cout << 0 << '\n';
+			}
+			//원의 길이가 동일할 때
+			else if (abs(rb - (ra + dist)) < epsilon) {
+				cout << 1 << '\n';
+			}
+			else {
+				cout << 2 << '\n';
 			}
 		}
-
-		cout << x << '\n';
+		else {
+			//서로 떨어져 있을 경우
+			if (ra + rb < dist) {
+				cout << 0 << '\n';
+			}
+			//붙어 있을경우
+			else if (abs(ra + rb - dist) < epsilon) {
+				cout << 1 << '\n';
+			}
+			//둘다 아닐경우
+			else {
+				cout << 2 << '\n';
+			}
+		}
 	}
 }
