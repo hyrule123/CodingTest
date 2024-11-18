6b44eb3062f75d6edc099647648087219a7b855b
HWNOTE <ekdrn1@naver.com>
2024-11-18 12:20:25 +0900

    백준 15681 (트리와 쿼리) [복습][그래프][DP]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,22 +14,52 @@ int main() {
 	return 0;
 }
 
-int memo[41][2];
+#include <array>
+#include <vector>
+#include <stack>
+
+struct node {
+	int parent;
+	vector<int> childs;
+};
+constexpr int MAX = (int)1e5 + 1;
+int N, R, Q;
+array<node, MAX> graph;
+array<int, MAX> dp;
+
+void build_parent(int cur, int par) {
+	graph[cur].parent = par;
+
+	for (int next : graph[cur].childs) {
+		if (next == par) { continue; }
+		build_parent(next, cur);
+	}
+}
+
+int query(int u) {
+	if (dp[u] == 0) {
+		dp[u] = 1;
+
+		for (int next : graph[u].childs) {
+			if (next == graph[u].parent) { continue; }
+			dp[u] += query(next);
+		}
+	}
+	return dp[u];
+}
 
 void solve() {
-	memo[0][0] = 1;
-	memo[1][1] = 1;
-
-	//fib(2) = fib(1) + fib(0) -> dp[1] + dp[0]
-	//fib(3) = fib(2) + fib(1)
-	for (int i = 2; i <= 40; ++i) {
-		memo[i][0] = memo[i - 1][0] + memo[i - 2][0];
-		memo[i][1] = memo[i - 1][1] + memo[i - 2][1];
+	cin >> N >> R >> Q;
+	for (int i = 1; i <= N - 1; ++i) {
+		int u, v; cin >> u >> v;
+		graph[u].childs.push_back(v);
+		graph[v].childs.push_back(u);
 	}
 
-	int TC; cin >> TC;
-	while (TC--) {
-		int input; cin >> input;
-		cout << memo[input][0] << ' ' << memo[input][1] << '\n';
+	build_parent(R, 0);
+
+	for (int i = 1; i <= Q; ++i) {
+		int u; cin >> u;
+		cout << query(u) << '\n';
 	}
 }
