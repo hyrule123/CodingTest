79e9790ca013e493fd16a3ae37600388254bff73
HWNOTE <ekdrn1@naver.com>
2024-11-19 20:02:12 +0900

    백준 13511 (트리와 쿼리 2) [LCA][희소 배열]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,116 +14,168 @@ int main() {
 	return 0;
 }
 
-//백준 3176 (도로 네트워크) [LCA: 이전 풀이보다 효율적(292ms -> 160ms)]
-//경로를 찾는다는건 곧 최소 공통 조상을 찾은 뒤 그 조상을 거쳐 이동하는 것이므로
-//LCA 알고리즘으로 찾을 수 있을 듯
+#include <array>
 #include <vector>
+#include <cmath>
 #include <stack>
-using l = long;
 using ll = long long;
+constexpr ll MAX = 100'001;
+struct Edge { int dest, dist; };
+struct Node { int parent; ll dist; };
+using Tree = vector<vector<Node>>;
 
-struct edge { l dest, dist; };
-struct dp { l par, min, max; };
+void build_tree(const vector<vector<Edge>>& graph, Tree& tree, vector<int>& depth) {
+	const size_t log_max = (size_t)ceil(log2(graph.size())) + 1;
+	tree.resize(graph.size(), vector<Node>(log_max));
+	depth.resize(graph.size());
 
-constexpr l MAX = 100'001, LOG_MAX = 18;
-vector<edge> edges[MAX];
-l N, depth[MAX];
-dp parent_of[LOG_MAX][MAX];
-
-void build_tree() {
-	vector<bool> visited; visited.resize(N + 1);
-	stack<l> stk;
-	stk.push(1);
+	stack<int> stk;
+	vector<bool> visited(graph.size());
 	visited[1] = true;
-	parent_of[0][1].max = -1;
-	parent_of[0][1].min = 1'000'001;
-
+	stk.push(1);
 	while (false == stk.empty()) {
-		l cur = stk.top(); stk.pop();
+		int cur = stk.top(); stk.pop();
 
-		for (const edge& next : edges[cur]) {
+		for (const auto& next : graph[cur]) {
 			if (visited[next.dest]) { continue; }
 			visited[next.dest] = true;
-
-			parent_of[0][next.dest].par = cur;
-			parent_of[0][next.dest].max = next.dist;
-			parent_of[0][next.dest].min = next.dist;
+			
+			tree[next.dest][0].parent = cur;
+			tree[next.dest][0].dist = next.dist;
 			depth[next.dest] = depth[cur] + 1;
-
 			stk.push(next.dest);
 		}
 	}
 }
 
-void query(l a, l b) {
-	l _max = -1, _min = 1'000'001;
+void pre_compute(Tree& tree) {
+	for (size_t i = 1; i < tree[0].size(); ++i) {
+		for (size_t j = 1; j < tree.size(); ++j) {
+			auto& cur = tree[j][i];
+			const auto&
+				fx = tree[j][i - 1],
+				//tree[tree[j][i-1]][i-1]
+				fxx = tree[fx.parent][i - 1];
 
-	if (depth[a] > depth[b]) {
+			cur.dist = fx.dist + fxx.dist;
+			cur.parent = fxx.parent;
+		}
+	}
+}
+
+ll query_1(const Tree& tree, const vector<int>& depth, int a, int b) {
+	if (depth[a] < depth[b]) {
 		swap(a, b);
 	}
 
-	l depth_diff = depth[b] - depth[a];
-	for (l shift = 0; shift < LOG_MAX; ++shift) {
-		l bit = 1 << shift;
-		if (bit > depth_diff) { break; }
+	ll dist = 0;
+	int depth_diff = depth[a] - depth[b];
+	for (int shift = 0; shift < (int)tree[0].size(); ++shift) {
+		int bit = 1 << shift;
+		if (depth_diff < bit) { break; }
 		if (depth_diff & bit) {
-			_min = min(_min, parent_of[shift][b].min);
-			_max = max(_max, parent_of[shift][b].max);
-			b = parent_of[shift][b].par;
+			dist += tree[a][shift].dist;
+			a = tree[a][shift].parent;
 		}
 	}
 
-	//LCA를 발견하지 못했다면
+	if (a != b) { 
+		for (int shift = (int)tree[0].size() - 1; shift >= 0; --shift) {
+			if (tree[a][shift].parent == tree[b][shift].parent) { continue; }
+
+			dist += tree[a][shift].dist + tree[b][shift].dist;
+			a = tree[a][shift].parent;
+			b = tree[b][shift].parent;
+		}
+		dist += tree[a][0].dist + tree[b][0].dist;
+	}
+
+	return dist;
+}
+int query_2(const Tree& tree, const vector<int>& depth, int from, int to, int k) {
+	//k: k번째 노드 -> from에서 k-1 간선 개수만큼 이동하는걸로 변경
+	--k;
+
+	//간선 0개면 from 반환
+	if (k == 0) {
+		return from;
+	}
+	
+	int a = from, b = to;
+	if (depth[a] < depth[b]) {
+		swap(a, b);
+	}
+
+	int depth_diff = depth[a] - depth[b];
+	for (int shift = 0; shift < (int)tree[0].size(); ++shift) {
+		int bit = 1 << shift;
+		if (depth_diff < bit) { break; }
+		if (depth_diff & bit) {
+			a = tree[a][shift].parent;
+		}
+	}
+
 	if (a != b) {
-		//LCA의 자식 노드를 탐색
-		for (l shift = LOG_MAX - 1; shift >= 0; --shift) {
-			if (parent_of[shift][a].par == parent_of[shift][b].par) { continue; }
-
-			_min = min(_min, min(parent_of[shift][a].min, parent_of[shift][b].min));
-			_max = max(_max, max(parent_of[shift][a].max, parent_of[shift][b].max));
-			a = parent_of[shift][a].par;
-			b = parent_of[shift][b].par;
+		for (int shift = (int)tree[0].size() - 1; shift >= 0; --shift) {
+			if (tree[a][shift].parent == tree[b][shift].parent) { continue; }
+			a = tree[a][shift].parent;
+			b = tree[b][shift].parent;
 		}
 
-		//LCA 바로 밑 노드 -> LCA
-		_min = min(_min, min(parent_of[0][a].min, parent_of[0][b].min));
-		_max = max(_max, max(parent_of[0][a].max, parent_of[0][b].max));
+		a = tree[a][0].parent;
+		b = tree[b][0].parent;
 	}
 
-	cout << _min << ' ' << _max << '\n';
+	//전체 간선 개수
+	int total_len = depth[from] + depth[to] - 2 * depth[a];
+	if (k == total_len) { return to; }
+
+	//LCA와 from 사이 간선 개수를 구한다
+	int from2LCA = depth[from] - depth[a], 
+		ret = from;
+	
+	if (from2LCA < k) {
+		k = total_len - k;
+		ret = to;
+	}
+
+	//from 혹은 to로부터 k 혹은 k - from2LCA 거리의 노드를 반환
+	for (int shift = 0; shift < tree[0].size(); ++shift) {
+		int bit = 1 << shift;
+		if (k < bit) { break; }
+		if (bit & k) {
+			ret = tree[ret][shift].parent;
+		}
+	}
+	return ret;
 }
 
 void solve() {
-	cin >> N;
-	for (l i = 1; i <= N - 1; ++i) {
-		l a, b, c; cin >> a >> b >> c;
-		edges[a].push_back({ b, c });
-		edges[b].push_back({ a, c });
+	ll N; cin >> N;
+	vector<vector<Edge>> graph;
+	graph.resize(N + 1);
+	for (int i = 1; i <= N - 1; ++i) {
+		int u, v, w; cin >> u >> v >> w;
+		graph[u].push_back({ v, w });
+		graph[v].push_back({ u, w });
 	}
+	
+	vector<int> depth;
+	Tree tree;
 
-	//1번을 루트노드로 트리를 구성
-	build_tree();
+	build_tree(graph, tree, depth);
+	pre_compute(tree);
 
-	//dp precompute
-	for (l i = 1; i < LOG_MAX; ++i) {
-		for (l j = 1; j <= N; ++j) {
-			dp& cur = parent_of[i][j];
-
-			const dp& fx = parent_of[i - 1][j];
-			const dp& fxx = parent_of[i - 1][fx.par];
-
-			cur.par = fxx.par;
-
-			//min, max 비교대상f(x), f(f(x))
-			cur.min = min(fx.min, fxx.min);
-			cur.max = max(fx.max, fxx.max);
+	int M; cin >> M;
+	while (M--) {
+		int q; cin >> q;
+		if (q == 1) {
+			int a, b; cin >> a >> b;
+			cout << query_1(tree, depth, a, b) << '\n';
 		}
-	}
-
-	//query
-	l K; cin >> K;
-	while (K--) {
-		l d, e; cin >> d >> e;
-		query(d, e);
+		else if(q == 2) {
+			int a, b, c; cin >> a >> b >> c;
+			cout << query_2(tree, depth, a, b, c) << '\n';
+		}
 	}
 }
