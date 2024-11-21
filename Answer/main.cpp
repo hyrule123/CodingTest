e14d4f14bd6a8b36bd0426e9e2b69184a1657ba6
HWNOTE <ekdrn1@naver.com>
2024-11-21 17:27:58 +0900

    백준 1004 (어린 왕자) [수학]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,89 +14,49 @@ int main() {
 	return 0;
 }
 
-#include <vector>
-#include <stack>
-#include <algorithm>
-constexpr int MAX = 100'001;
-int N, M, id, scc_id;
-vector<vector<int>> graph;
-vector<int> scc_indegrees;
-vector<bool> finished;
-vector<int> ID;
+/*
+백준 1004 (어린 왕자)
+출발점과 도착점이 원 바깥에 있으면 얼마든지 돌아가면 되니까
+출발점, 도착점이 몇개의 '다른'원 안에 있는지를 검사하면 될듯(같은 원 안일 경우 안쪽에서 돌아서 갈수 있으므로)
+*/
 
-stack<int> stk;
-int DFS(int cur) {
-	ID[cur] = ++id;
+struct vec2 {
+	double x, y;
 
-	int last_id = ID[cur];
-	stk.push(cur);
-	for (int next : graph[cur]) {
-		if (ID[next] == 0) {
-			last_id = min(last_id, DFS(next));
-		}
-		else if (false == finished[next]) {
-			last_id = min(last_id, ID[next]);
-		}
+	vec2 operator -(const vec2& o) const {
+		return { x - o.x, y - o.y };
 	}
-
-	if (ID[cur] == last_id) {
-		++scc_id;
-		while (true) {
-			int last = stk.top(); stk.pop();
-
-			//몇 번쨰 SCC에 속하는지를 (더이상 사용하지 않는)ID 배열에 저장한다.
-			ID[last] = scc_id;
-
-			finished[last] = true;
-			if (cur == last) { break; }
-		}
+	double dist_squared(const vec2& o) const {
+		vec2 sub = (*this) - o;
+		return sub.x * sub.x + sub.y * sub.y;
 	}
-
-	return last_id;
-}
+};
 
 void solve() {
-	int TC; cin >> TC;
-	while (TC--) {
-		cin >> N >> M;
-		
-		id = 0;
-		scc_id = 0;
-		graph.clear();
-		scc_indegrees.clear();
-		finished.clear();
-		ID.clear();
-		graph.resize(N + 1);
-		scc_indegrees.resize(graph.size());
-		finished.resize(graph.size());
-		ID.resize(graph.size());
+	int T; cin >> T;
 
-		for (int i = 0; i < M; ++i) {
-			int a, b; cin >> a >> b;
-			graph[a].push_back(b);
-		}
+	while (T--) {
+		vec2 from, to;
+		cin >> from.x >> from.y >> to.x >> to.y;
 
-		for (int i = 1; i <= N; ++i) {
-			if (false == finished[i]) {
-				DFS(i);
+		int n; cin >> n;
+		int count = 0;
+		while (n--) {
+			vec2 center; cin >> center.x >> center.y;
+			double r; cin >> r;
+			r = r * r;
+
+			double
+				f2c = from.dist_squared(center),
+				t2c = to.dist_squared(center);
+
+			//둘다 같은 원 안에 있으면 원을 벗어날 필요가 없으므로 continue
+			if (f2c <= r && t2c <= r) {
+				continue;
 			}
-		}
 
-		scc_indegrees.clear();
-		scc_indegrees.resize(scc_id + 1);
-		for (int cur = 1; cur <= N; ++cur) {
-			for (int next : graph[cur]) {
-				//그룹이 다를 경우 해당 그룹의 in_degree를 늘려준다.
-				if (ID[cur] != ID[next]) {
-					scc_indegrees[ID[next]]++;
-				}
-			}
-		}
-
-		//in_degree가 0인 scc로부터 시작하면 가장 적은 횟수로 도달 가능.
-		int count{};
-		for (size_t i = 1; i < scc_indegrees.size(); ++i) {
-			if (scc_indegrees[i] == 0) {
+			//다른 원일 경우 -> 둘 중 하나만 원 안에 속해 있을 경우
+			if (f2c <= r || t2c <= r) {
 				++count;
 			}
 		}
