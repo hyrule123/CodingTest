b271c106ce005d2b681488bbf8e1fd4b222c0951
HWNOTE <ekdrn1@naver.com>
2024-11-27 22:45:58 +0900

    2-SAT문제 이론정리중, 그래프 구현

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,133 +14,85 @@ int main() {
 	return 0;
 }
 /*
-백준 4013 (ATM)
-A와 B가 Strongly Connected Component라는 건
-A에서 출발하던 B에서 출발하던 서로에게 도달할 수 있다는 것
-하나의 SCC에서 다른 노드로 얼마든지 갈 수 있으므로
-SCC에서 뽑을수 있는 액수와 레스토랑 유무를 뽑아서 묶어놓은 후 길찾기를 하면 될듯
-또한 SCC로 새로 그래프를 짤 경우
-사이클이 없는 방향 그래프(DAG, Directed Acyclic Graph)가 된다. -> 위상 정렬을 사용할 수 있게 됨
+백준 11277 (2-SAT - 1)
+CNF는 AND로 연결 -> 모든 절은 그래프처럼 연결되어 있음
+또한 각 bool 변수들은 여러가지 절에 들어가있을수도 있다 -> 이 때문에 사이클이 생길 수 있다
+사이클이 생기게 되면, 사이클 안의 모든 변수들은 서로 논리구조가 연결된다.(SCC 사용이 가능하다는뜻)
 
-어떤 노드에서 출발해도 하나 이상의 레스토랑에 도착 가능
-출발 장소 S 주어짐
+논리 관계를 그래프로 만들기
+P->Q 간선의 의미: P가 거짓이면 Q는 반드시 true여야 한다는 뜻
 
+참고) 경우의 수
+P->Q: NOT P가 bool일때 Q가 bool이라면
+1. NOT P = true(P = false) -> Q = true: (false | true) = true
+2. NOT P = true(P = false) -> Q = false: (false | false) = false
+3. NOT P = false(P = true) -> Q = false: (true | false) = true
+4. NOT P = false(P = true) -> Q = true: (true | true) = true
+
+A V B: 둘중 하나는 반드시 true여야 함
+A가 F -> B는 반드시 T여야 한다, B가 F -> A는 반드시 T여야 한다 
+하나의 조건에 두 가지 간선이 만들어짐. NOT A->B, NOT B->A 두 간선을 그래프에 추가
+
+(A V B)
+nA->B
+nB->A
+2개 간선이 생성
+
+(A V nB) ∧ (B ∧ nA) ^ (A ^ B)
+nA->nB
+B->A
+nB->nA
+A->B
+nA->B
+nB->A
+4개 간선이 생성
+
+nA->nB->A라는 사이클이 하나 생성되는데,
+여기서 nA->A와 A->nA로 이동이 가능해진다.
+nA->A는 곧 NOT NOT A -> A 이므로 위의 예시에서 
+
+
+
+케이스가 적어서 단순 DFS로 가능할거 같음
 */
 
+#include <bitset>
 #include <vector>
-#include <stack>
-#include <queue>
-struct node {
-	bool rest; int atm; vector<int> edges;
+#include <array>
+bitset<21> vars;
+
+struct x {
+	int i;
+	operator int&() { return i; }
+	x operator !() { return x(-i); }
 };
-struct scc_node{
-	bool rest; int deposit; vector<int> members, edges;
+
+struct Graph {
+	void resize(int size) {
+		nodes.resize(size * 2);
+	}
+
+	vector<x>& operator [](x idx) {
+		return (0 <= idx ? nodes[idx] : nodes[(!idx) * 2]);
+	}
+	vector<vector<x>> nodes;
 };
-vector<node> graph;
-int N, M, S, P, id_temp, scc_idx;
-vector<int> scc_ID;
-vector<bool> finished;
-stack<int> stk;
-vector<scc_node> scc_graph;
+Graph graph;
+int N, M;
 
-void clearstk() { while (!stk.empty()) { stk.pop(); } }
-
-int build_SCC(const int cur) {
-	scc_ID[cur] = ++id_temp;
-	stk.push(cur);
-	int ret = scc_ID[cur];
-	for (int next : graph[cur].edges) {
-		if (scc_ID[next] == 0) {
-			ret = min(ret, build_SCC(next));
-		}
-		else if (false == finished[next]) {
-			ret = min(ret, scc_ID[next]);
-		}
-	}
-
-	if (scc_ID[cur] == ret) {
-		scc_node scc{};
-		++scc_idx;
-		
-		while (true) {
-			int mem = stk.top(); stk.pop();
-			scc_ID[mem] = scc_idx;
-			finished[mem] = true;
-			scc.members.push_back(mem);
-			scc.deposit += graph[mem].atm;
-			scc.rest |= graph[mem].rest;
-
-			if (cur == mem) { break; }
-		}
-
-		scc_graph.push_back(scc);
-	}
-
-	return ret;
-}
-
-int BFS() {
-	//edge 생성
-	for (int i = 1; i <= N; ++i) {
-		for (int next : graph[i].edges) {
-			if (scc_ID[i] != scc_ID[next]) {
-				scc_graph[scc_ID[i]].edges.push_back(scc_ID[next]);
-			}
-		}
-	}
-
-	queue<int> q;
-	vector<int> dp(scc_graph.size());
-	q.push(scc_ID[S]);
-	dp[q.front()] = scc_graph[q.front()].deposit;
-
-	int ans = 0;
-	
-	while (false == q.empty()) {
-		int cur = q.front(); q.pop();
-
-		if (scc_graph[cur].rest) {
-			ans = max(ans, dp[cur]);
-		}
-
-		for (int next : scc_graph[cur].edges) {
-			//새로운 값으로 갱신되면 queue에 넣는다
-			if (dp[next] < dp[cur] + scc_graph[next].deposit) {
-				dp[next] = dp[cur] + scc_graph[next].deposit;
-				q.push(next);
-			}
-		}
-	}
-
-	return ans;
+bool DFS(int par, int cur) {
 }
 
 void solve() {
 	cin >> N >> M;
+
 	graph.resize(N + 1);
-	scc_ID.resize(N + 1);
-	finished.resize(N + 1);
+
 	for (int i = 1; i <= M; ++i) {
-		int a, b; cin >> a >> b;
-		graph[a].edges.push_back(b);
+		x a, b; cin >> a >> b;
+		graph[!a].push_back(b);
+		graph[!b].push_back(a);
 	}
-	for (int i = 1; i <= N; ++i) {
-		int money; cin >> money;
-		graph[i].atm = money;
-	}
-	cin >> S >> P;
-	for (int i = 1; i <= P; ++i) {
-		int p; cin >> p;
-		graph[p].rest = true;
-	}
-	
-	//SCC를 만든 다음
-	scc_graph.resize(1);
-	for (int i = 1; i <= N; ++i) {
-		if (false == finished[i]) {
-			build_SCC(i);
-		}
-	}
-	
-	cout << BFS();
+
+	cout << DFS(0, 1);
 }
