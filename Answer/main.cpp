b34fa1866ff393f3026ad0536f269484b6415b16
HWNOTE <ekdrn1@naver.com>
2024-12-10 17:22:45 +0900

    백준 3648 (아이돌) [SCC][위상정렬][복습]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,136 +14,88 @@ int main() {
 	return 0;
 }
 /*
-백준 3648 (아이돌) [SCC][위상정렬]
-심사위원 한 명이 두 명에게 투표 -> 반대(NOT), 찬성 -> 둘중 최소 한표는 유효해야 하므로 OR
-그냥 2-SAT 문제를 해결했을 때 상근이(1번)이 true인지를 보면 되나?
--> 상근이를 true로 만드는 조건을 CNF식에 추가하면 된다.(X1 V X1)
-그러면 그래프에는 NOT X1 -> X1 간선이 추가됨
+백준 3648 (아이돌) [SCC][위상정렬][복습]
 */
 #include <vector>
 #include <stack>
-#include <bitset>
-#include <string>
-constexpr int n_MAX = 2001;
-int n, m, node_num;
-int x_idx(int x) {
+int n, m;
+
+//not A = -A
+//if -A, then -A * 2 - 1
+//if A, then A * 2
+int idx(int x) {
 	return (0 <= x ? x * 2 : -x * 2 - 1);
 }
+vector<vector<int>> graph;
+
+int scc_num, scc_id_gen;
+vector<int> scc_id;
+vector<bool> scc_finished;
 stack<int> stk;
-vector<int> graph[n_MAX];
+int build_SCC(int cur) {
+	scc_id[cur] = ++scc_id_gen;
+	stk.push(cur);
 
-//SCC section
-int scc_count, temp_id;
-int scc_id[n_MAX];
-bitset<n_MAX> finished;
-
-int DFS(int cur_idx) {
-	scc_id[cur_idx] = ++temp_id;
-	stk.push(cur_idx);
-
-	int ret = scc_id[cur_idx];
-	for (int next_idx : graph[cur_idx]) {
-		if (0 == scc_id[next_idx]) {
-			ret = min(ret, DFS(next_idx));
+	int result = scc_id[cur];
+	for (int next : graph[cur]) {
+		if (scc_id[next] == 0) {
+			result = min(result, build_SCC(next));
 		}
-		else if (false == finished[next_idx]) {
-			ret = min(ret, scc_id[next_idx]);
+		else if (false == scc_finished[next]) {
+			result = min(result, scc_id[next]);
 		}
 	}
 
-	if (ret == scc_id[cur_idx]) {
-		++scc_count;
-
+	if (result == scc_id[cur]) {
+		++scc_num;
+		
 		while (true) {
-			int mem = stk.top(); stk.pop();
-			finished[mem] = true;
-			scc_id[mem] = scc_count;
-
-			if (cur_idx == mem) { break; }
+			int member = stk.top(); stk.pop();
+			scc_id[member] = scc_num;
+			scc_finished[member] = true;
+			
+			if (cur == member) { break; }
 		}
 	}
 
-	return ret;
-}
-void build_scc() {
-	memset(scc_id, 0, sizeof(scc_id));
-	finished.reset();
-	scc_count = temp_id = 0;
-	while (false == stk.empty()) { stk.pop(); }
-
-	for (int i = 1; i <= node_num; ++i) {
-		if (false == finished[i]) {
-			DFS(i);
-		}
-	}
-}
-
-vector<vector<int>> scc_edges;
-vector<int> scc_indeg, scc_order;
-void toposort() {
-	scc_edges.clear(); scc_edges.resize(scc_count + 1);
-	scc_indeg.clear(); scc_indeg.resize(scc_count + 1);
-	for (int from = 1; from <= node_num; ++from) {
-		for (int to : graph[from]) {
-			int scc_from = scc_id[from], scc_to = scc_id[to];
-			if (scc_from != scc_to) {
-				scc_edges[scc_from].push_back(scc_to);
-				scc_indeg[scc_to]++;
-			}
-		}
-	}
-
-	while (false == stk.empty()) { stk.pop(); }
-	for (int i = 1; i <= scc_count; ++i) {
-		if (scc_indeg[i] == 0) {
-			stk.push(i);
-		}
-	}
-
-	scc_order.clear(); scc_order.resize(scc_count + 1);
-	int order = 0;
-	while (false == stk.empty()) {
-		int cur = stk.top(); stk.pop();
-
-		scc_order[cur] = ++order;
-
-		for (int next : scc_edges[cur]) {
-			if (0 == --(scc_indeg[next])) {
-				stk.push(next);
-			}
-		}
-	}
+	return result;
 }
 
 void solve() {
 	while (cin >> n >> m) {
-		node_num = n * 2;
+		graph.clear(); graph.resize(n * 2 + 1);
+		while (false == stk.empty()) { stk.pop(); }
 
-		for (int i = 1; i <= node_num; ++i) {
-			graph[i].clear();
-		}
-
-		//1번 노드는 반드시 true가 되어야 함 -> (X1 V X1) 조건을 추가. X1이 false 일시 false가 됨
-		//그래프로는 NOT X1 -> X1
-		graph[x_idx(-1)].push_back(x_idx(1));
 		while (m--) {
 			int a, b; cin >> a >> b;
-			graph[x_idx(-a)].push_back(x_idx(b));
-			graph[x_idx(-b)].push_back(x_idx(a));
+
+			//CNF 식을 그래프 형태로 변경
+			//NOT A -> B: NOT A가 참이면, B는 참이여야 한다.
+			//NOT B -> A: NOT B가 참이면, A는 참이어야 한다.
+			graph[idx(-a)].push_back(idx(b));
+			graph[idx(-b)].push_back(idx(a));
 		}
 
-		build_scc();
-		toposort();
+		//상근이(1번)을 무조건 true로 만드는 CNF 식을 하나 추가한다.
+		//1 V 1 == NOT 1 -> 1
+		graph[idx(-1)].push_back(idx(1));
 
-		auto& ans_x = finished;
-		ans_x.reset();
+		//SCC build
+		scc_id.clear(); scc_id.resize(graph.size());
+		scc_finished.clear(); scc_finished.resize(graph.size());
+		while (false == stk.empty()) { stk.pop(); };
+		scc_num = scc_id_gen = 0;
 
+		for (int i = 1; i < (int)scc_finished.size(); ++i) {
+			if(false == scc_finished[i]) {
+				build_SCC((int)i);
+			}
+		}
+
+		//같은 SCC 안에 NOT i와 i가 동시에 존재하면 false
 		bool valid = true;
-		for (int x = 1; x <= n; ++x) {
-			int X = x_idx(x), NOT_X = x_idx(-x);
-			int scc_X = scc_id[X], scc_NOT_X = scc_id[NOT_X];
-
-			if (scc_X == scc_NOT_X) {
+		for (int i = 1; i <= n; ++i) {
+			if (scc_id[idx(-i)] == scc_id[idx(i)]) {
 				valid = false;
 				break;
 			}
