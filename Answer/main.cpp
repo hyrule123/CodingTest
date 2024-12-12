6df3904c80bc33bd6d5b516552f598633c753ab9
HWNOTE <ekdrn1@naver.com>
2024-12-12 20:11:39 +0900

    백준 2042 (구간 합 구하기) [세그먼트 트리]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,79 +14,91 @@ int main() {
 	return 0;
 }
 /*
-백준 1927 (최소 힙) [힙][복습]
+백준 2042 (구간 합 구하기) [세그먼트 트리]
 */
-#include <vector>
+using ll = int64_t;
+constexpr ll MAX = 1'000'000;
+ll N, M, K, segtree[MAX * 4 + 1];
 
-int N;
-struct min_heap {
-	void heapify_up(int idx) {
-		int cur = idx;
-		while (0 < cur) {
-			int par = (cur - 1) / 2;
-
-			if (cont[cur] < cont[par]) {
-				swap(cont[cur], cont[par]);
-				cur = par;
-			}
-			else {
-				break;
-			}
-		}
+/*
+빌드: 재귀 형태로 탐색
+*/
+ll build_segtree(ll cur, ll start, ll end) {
+	if (start >= end) {
+		//전위 순회 이므로 리프노드에서 값을 받아올 경우 1, 2, 3, ... 순서로 받아옴
+		cin >> segtree[cur]; 
+		return segtree[cur];
 	}
 
-	void heapify_down(int cur) {
-		while (cur < size) {
-			int least = cur;
-			int l = cur * 2 + 1, r = l + 1;
+	ll mid = (start + end) / 2;
+	ll left = cur * 2, right = left + 1;
 
-			if (l < size && cont[least] > cont[l]) {
-				least = l;
-			}
-			if (r < size && cont[least] > cont[r]) {
-				least = r;
-			}
+	segtree[cur] = 
+		build_segtree(left, start, mid) + 
+		build_segtree(right, mid + 1, end);
 
-			if (cur == least) {
-				break;
-			}
-			else {
-				swap(cont[cur], cont[least]);
-				cur = least;
-			}
-		}
+	return segtree[cur];
+}
+
+//변경: 범위 안의 구간만 탐색(이분 탐색)
+ll modify(ll cur, ll start, ll end, ll target_idx, ll modify_to) {
+	if (start >= end) { 
+		segtree[cur] = modify_to; return modify_to; 
 	}
 
-	void insert(int i) {
-		cont[size] = i;
-		heapify_up(size);
-		++size;
+	ll mid = (start + end) / 2;
+	ll left = cur * 2, right = left + 1;
+
+	//분할 탐색
+	if (target_idx <= mid) {
+		segtree[cur] = modify(left, start, mid, target_idx, modify_to) + segtree[right];
+	}
+	else {
+		segtree[cur] = segtree[left] + modify(right, mid + 1, end, target_idx, modify_to);
 	}
 
-	int pop() {
-		if (size == 0) { return 0; }
-		if (size == 1) { --size; return cont[0]; }
+	return segtree[cur];
+}
 
-		int ret = cont[0];
-
-		swap(cont[0], cont[--size]);
-		heapify_down(0);
-
-		return ret;
+/* 구간합 쿼리
+탐색 범위에서 완전히 벗어나는 경우: 0 반환
+현재 탐색 범위(start~end)안일 경우: 구간합(segtree[cur])을 반환
+부분적으로만 겹칠 경우: 재귀탐색 수행
+*/
+ll query(ll cur, ll start, ll end, ll from, ll to) {
+	//찾고자 하는 범위 바깥인 경우
+	if (end < from || to < start) {
+		return 0;
 	}
 
-	int size, cont[100'000];
-} heap;
+	//현재 탐색범위가 구간합 범위 안일 경우
+	if (from <= start && end <= to) {
+		return segtree[cur];
+	}
+
+	//일부만 겹치는 경우: 좌우 분할 탐색
+	ll mid = (start + end) / 2;
+	ll left = cur * 2, right = left + 1;
+	
+	return query(left, start, mid, from, to)
+		+ query(right, mid + 1, end, from, to);
+}
 
 void solve() {
-	cin >> N;
-	for (int i = 0; i < N; ++i) {
-		int input; cin >> input;
-		if (input == 0) {
-			cout << heap.pop() << '\n';
+	cin >> N >> M >> K;
+	build_segtree(1, 1, N);
+
+	for (int i = 0; i < M + K; ++i) {
+		ll a, b, c; cin >> a >> b >> c;
+		switch (a) {
+		case 1ll: {
+			modify(1, 1, N, b, c);
+			break;
 		}
-		else {
-			heap.insert(input);
+		case 2ll: {
+			cout << query(1, 1, N, b, c) << '\n';
+			break;
+		}
 		}
 	}
 }
