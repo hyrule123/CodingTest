a52d3fd8cd62913920fbd4e86a8c05afd602dea4
HWNOTE <ekdrn1@naver.com>
2024-12-30 21:00:01 +0900

    백준 22968 (균형) [DP][Parametric Search]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -15,27 +15,54 @@ int main() {
 	return 0;
 }
 /*
-백준 25206 (너의 평점은) [수학?][복습]
+백준 22968 (균형) [DP][Parametric Search]
+AVL 트리에서 깊이 h 이상을 달성하기 위한 최소한의 노드 개수 = N(h)
+N(1) = 1
+N(2) = 2
+N(3) = 4 (한쪽 깊이 1, 반대쪽 깊이 2 부터)
+N(4) = 7 (한쪽 깊이 2, 반대쪽 깊이 3 부터)
+-> N(h) = N(h - 2) + N(h - 1) + 1
 */
-#include <string>
-#include <map>
-constexpr const char* strs[] = { "A+", "A0", "B+", "B0", "C+", "C0", "D+", "D0"};
+#include <cmath>
+#include <vector>
+constexpr int MAX = 1'000'000'000;
+vector<int> min_N_to_build_h;
+void precompute() {
+	min_N_to_build_h.reserve(50);
+	min_N_to_build_h = { 0, 1, 2 };
+	while (min_N_to_build_h.back() < MAX) {
+		auto h_minus_2 = min_N_to_build_h.end() - 2;
+		auto h_minus_1 = min_N_to_build_h.end() - 1;
+		min_N_to_build_h.push_back((*h_minus_2) + (*h_minus_1) + 1);
+	}
+}
 
+int find_height(int start, int end, int N) {
+	int ret = start;
+
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		
+		//깊이 h를 만들기 위한 최소한의 N개 이므로
+		if (min_N_to_build_h[mid] <= N) {
+			ret = mid;
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+
+	return ret;
+}
+
+int T;
 void solve() {
-	map<string, double> grades;
-	for (int i = 0; i < sizeof(strs) / sizeof(const char*); ++i) {
-		grades[strs[i]] = 4.5 - (0.5 * (double)i);
-	}
-	grades["F"] = 0;
-	grades["P"] = 0;
+	precompute();
 
-	double score_sum{}, count{};
-	double score{}; string name{}, grade{};
-	while (cin >> name >> score >> grade) {
-		if (grade == "P") { continue; }
-		score_sum += score * grades[grade];
-		count += score;
+	cin >> T;
+	while (T--) {
+		int input; cin >> input;
+		cout << find_height(1, (int)min_N_to_build_h.size() - 1, input) << '\n';
 	}
-
-	cout << score_sum / count;
 }
