c2a3fbae232db610dcc5ad86ffce7ff57d0a105b
HWNOTE <ekdrn1@naver.com>
2025-01-02 16:54:48 +0900

    백준 24459 (회로의 저항) [DFS]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -15,148 +15,110 @@ int main() {
 	return 0;
 }
 /*
-백준 24459 (회로의 저항) [LCA][오답]
+백준 24459 (회로의 저항) [DFS]
 리프노드부터 리프노드까지의 거리 중 최장거리와 최단거리 구하기
 * 유사 문제: 백준 3176 (도로 네트워크)
 * LCA를 사용하여 푸는 문제인가...했으나 아닌 듯
 마지막에 리프노드의 모든 쌍끼리 거리를 구하는 과정에서 O(N^2)가 될 수도 있음
+
+https://m.kin.naver.com/qna/dirs/10402/docs/471094489
+DFS로 찾고 있는듯. 이 코드는 위 사이트를 참고하여 이해하고 주석을 단 뒤 제출하였음.
 */
 #include <vector>
-#include <stack>
-#include <cmath>
 using pii = pair<int, int>;
-#define TO first
-#define DIST second
-#define shortest first
-#define longest second
-struct parent_info {
-	int parent, dist;
+#define dest first
+#define dist second
+constexpr int imax = (int)1e9, imin = 0;
+
+struct ret_val {
+	int min_first = imax, 
+		min_second = imax, 
+		max_first = imin, 
+		max_second = imin;
 };
-//2^16 = 65535, 2^17은 해야 10만이 넘음
-//binary lifting을 하기 위해선 최소 1 << 17까지는 쓸 수 있어야 하므로 18을 배열의 길이로 준다
-constexpr int N_MAX = 100'000, N_MAX_LOG2 = 18;
-int N;
-vector<pii> graph[N_MAX];
-parent_info parent_of[N_MAX][N_MAX_LOG2];
-int depth[N_MAX];
-vector<int> leaf_nodes;
 
-void build_parent_of() {
-	vector<bool> visited; visited.resize(N);
-	stack<int> stk;
-	stk.push(0);
-	visited[0] = true;
+int N, ans_max = imin, ans_min = imax;
+vector<pii> edges[100'000];
 
-	while (false == stk.empty()) {
-		int cur = stk.top(); stk.pop();
+ret_val DFS(int cur, int prev) {
+	ret_val ret{};
+	bool path_found = false;
+	
+	/*
+	다음 간선으로 출발
+	자신이 임의의 중간 지점의 노드이고
+	구해야하는 것은 리프 노드 사이의 최단거리, 최장거리일 경우
+	리프 노드까지 DFS 탐색을 한 후 두 최단거리의 합 / 최장거리의 합을 구하면 된다.
+	ex.
+	다음 노드가 A, B, C 라면
+	최단거리는 A, B, C 노드로 이동했을때 3가지 경우를 DFS 돌린 후 가장 가까운 경로 2개와 가장 먼 경로 2개를 선정한 후 합하여 반환한다.
+	*/
+	for (const pii& next : edges[cur]) {
+		if (next.dest == prev) { continue; }
+		
+		path_found = true;
+		ret_val result = DFS(next.dest, cur);
 
-		if (graph[cur].size() == 1) {
-			leaf_nodes.push_back(cur);
+		//이번 DFS 결과에 이번 간선의 길이를 더한다
+		int found_dist_min = result.min_first + next.dist;
+		int found_dist_max = result.max_first + next.dist;
+
+		//가장 짧은 거리 2개를 저장한다.
+		if (found_dist_min < ret.min_first) {
+			ret.min_second = ret.min_first;
+			ret.min_first = found_dist_min;
+		}
+		else if (found_dist_min < ret.min_second) {
+			ret.min_second = found_dist_min;
 		}
 
-		for (const pii& next : graph[cur]) {
-			if (visited[next.TO]) { continue; }
-			visited[next.TO] = true;
-
-			//next의 부모님은 cur,
-			//next에서 cur로 가는 최단거리, 최장거리는 DIST
-			parent_of[next.TO][0].parent = cur;
-			parent_of[next.TO][0].dist = next.DIST;
-			depth[next.TO] = depth[cur] + 1;
-
-			stk.push(next.TO);
+		//가장 먼 거리 2개를 저장한다.
+		if (ret.max_first < found_dist_max) {
+			ret.max_second = ret.max_first;
+			ret.max_first = found_dist_max;
 		}
-	}
-}
-
-void precompute_parent_of() {
-	for (int h = 1; h < N_MAX_LOG2; ++h) {
-		for (int i = 0; i < N_MAX; ++i) {
-			/*
-			i의 1<<h번째 부모
-			0은 직계부모(1 << 0)
-			1은 1 << 1 = 2 이므로 부모의 부모의 부모
-			부모의 부모(1 << 0) 의 부모(1 << 0)
-			
-			1<<2 는 4 이므로 
-			1<<2의 부모의 1<<1번째 부모(a)
-			a의 1<<1번쨰 부모를 구하면 됨
-
-			쉽게 말해서 n의 1<<h번쨰 부모를 구하라 라고 하면
-			1<<h-1번쨰 부모를 찾고, 그 부모의 1<<h-1번째 부모를 찾으면 되는것이다
-			*/
-			auto& cur = parent_of[i][h];
-
-			auto& par = parent_of[i][h - 1];
-			auto& par_par = parent_of[par.parent][h - 1];
-
-			cur.parent = par_par.parent;
-			cur.dist = par.dist + par_par.dist;
-		}
-	}
-}
-
-int query_dist(int a, int b) {
-	if (depth[a] > depth[b]) {
-		swap(a, b);
-	}
-
-	int total_dist = 0;
-	int depth_diff = depth[b] - depth[a];
-	for (int shifts = 0; shifts < N_MAX_LOG2; ++shifts) {
-		int bit = 1 << shifts;
-		if (depth_diff < bit) { break; }
-		if (depth_diff & bit) {
-			total_dist += parent_of[b][shifts].dist;
-			b = parent_of[b][shifts].parent;
+		else if (ret.max_second < found_dist_max) {
+			ret.max_second = found_dist_max;
 		}
 	}
 
-	//a가 b의 부모 노드였을 경우 리턴
-	if (a == b) {
-		return total_dist;
-	}
-
-	//먼 거리부터 탐색. 공통 조상이 달라지는 처음 부분을 찾는다.
-	for (int h = N_MAX_LOG2 - 1; h >= 0; --h) {
-		if (parent_of[a][h].parent == parent_of[b][h].parent) {
-			continue;
+	if (path_found) {
+		//최소거리와 최대거리가 2개가 저장되어 있다면
+		// 즉, 리프 노드가 아니라 중간 노드라면
+		//두개의 합을 더해서 갱신
+		//ex. A - B - C에서 B 노드일 경우에만 답지를 갱신
+		if (ret.max_second != imin) {
+			ans_min = min(ans_min, ret.min_first + ret.min_second);
+			ans_max = max(ans_max, ret.max_first + ret.max_second);
 		}
-
-		total_dist += parent_of[a][h].dist;
-		total_dist += parent_of[b][h].dist;
-
-		a = parent_of[a][h].parent;
-		b = parent_of[b][h].parent;
+	}
+	//DFS의 끝에 도달했을 경우에는 0을 반환한다
+	else {
+		ret.max_first = 0;
+		ret.max_second = 0;
+		ret.min_first = 0;
+		ret.min_second = 0;
 	}
 
-	//공통 조상이 달라지는 처음 부분을 찾은것이므로 직계 조상까지의 거리도 더해줘야 한다
-	total_dist += parent_of[a][0].dist;
-	total_dist += parent_of[b][0].dist;
-
-	return total_dist;
+	return ret;
 }
 
 void solve() {
 	cin >> N;
 	for (int i = 0; i < N - 1; ++i) {
-		int p, q, r; cin >> p >> q >> r;
-		graph[p].push_back({ q, r });
-		graph[q].push_back({ p, r });
+		int p, q, w; cin >> p >> q >> w;
+		edges[p].push_back({ q, w });
+		edges[q].push_back({ p, w });
+	}
+	
+	ret_val result = DFS(0, -1);
+	//만약 출발 노드(0)가 리프 노드였을 경우, 한쪽으로밖에 진행할 수 없으므로
+	//한쪽 방향의 값이 결과로 들어올 것이다.
+	//이 경우에는 한쪽 방향으로의 값이 최종 답이 된다.
+	if (result.max_second == imin) {
+		ans_max = max(ans_max, result.max_first);
+		ans_min = min(ans_min, result.min_first);
 	}
 
-	build_parent_of();
-	precompute_parent_of();
-
-	int min_dist = numeric_limits<int>::max(),
-		max_dist = numeric_limits<int>::min();
-	for (int i = 0; i < (int)leaf_nodes.size(); ++i) {
-		for (int j = i + 1; j < (int)leaf_nodes.size(); ++j) {
-			int dist = query_dist(leaf_nodes[i], leaf_nodes[j]);
-			min_dist = min(min_dist, dist);
-			max_dist = max(max_dist, dist);
-		}
-	}
-
-	cout << max_dist << '\n' << min_dist;
+	cout << ans_max << '\n' << ans_min;
 }
