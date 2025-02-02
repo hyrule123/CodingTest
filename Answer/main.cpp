364185395f8235845ff79b38303e90a98acc7f98
HW <ekdrn1@naver.com>
2025-02-02 17:33:24 +0900

    백준 16929 (Two Dots) [그래프]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,106 +14,110 @@ int main()
 }
 
 /*
-백준 28291 (레드스톤) [그래프]
+백준 16929 (Two Dots) [그래프]
 */
-
+#include <vector>
 #include <string>
 #include <queue>
-#include <vector>
-int w, h;
-
-struct coord 
-{ 
-	int x, y;
-	coord operator +(const coord& o) const 
+int N, M;
+vector<string> board;
+struct coord
+{
+	int r, c;
+	coord operator +(const coord& o)
 	{
-		return { x + o.x, y + o.y };
+		return { r + o.r, c + o.c };
 	}
-	bool is_valid() const
+	bool is_valid()
 	{
-		return !(x < 0 || w <= x || y < 0 || h <= y);
+		return !(r < 0 || N <= r || c < 0 || M <= c);
+	}
+	int to_1d()
+	{
+		return r * M + c;
 	}
 };
-constexpr coord dirs[] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
-constexpr string_view block_str = "redstone_block", lamp_str = "redstone_lamp", dust_str = "redstone_dust";
+constexpr coord dirs[] = { {-1,0}, {1,0}, {0,-1}, {0,1} };
+vector<vector<int>> graph;
+vector<bool> visited;
 
-using pci = pair<coord, int>;
-#define cod first
-#define energy second
-queue<pci> bfs;
-vector<coord> lamp_pos;
-int board[50][50];
+void make_graph()
+{
+	graph.resize(M * N);
+	for (int r = 0; r < N; ++r)
+	{
+		for (int c = 0; c < M; ++c)
+		{
+			coord cur = { r,c };
+			for (const auto& dir : dirs)
+			{
+				coord child = cur + dir;
+				if (false == child.is_valid())
+				{
+					continue;
+				}
+
+				//색이 같으면 간선을 연결
+				if (board[r][c] == board[child.r][child.c])
+				{
+					graph[cur.to_1d()].push_back(child.to_1d());
+				}
+			}
+		}
+	}
+}
+
+bool detect_cycle(int start)
+{
+	struct travel
+	{
+		int now, from;
+	};
+	queue<travel> q;
+	
+	visited[start] = true;
+	q.push({ start, -1 });
+
+	while (false == q.empty())
+	{
+		travel cur = q.front(); q.pop();
+		for (int next : graph[cur.now])
+		{
+			if (next == cur.from) { continue; }
+
+			if (visited[next]) { return true; }
+
+			visited[next] = true;
+			q.push({ next, cur.now });
+		}
+	}
+
+	return false;
+}
 
 void solve()
 {
-	cin >> w >> h;
-
-	//기본 16으로 초기화
-	for (int i = 0; i < w; ++i)
-	{
-		for (int j = 0; j < h; ++j)
-		{
-			board[i][j] = 16;
-		}
+	cin >> N >> M;
+	board.resize(N, string((size_t)M, 0));
+	for (int i = 0; i < N; ++i)
+	{;
+		cin >> board[i];
 	}
-
-	int n; cin >> n;
-	for (int i = 0; i < n; ++i)
-	{
-		string input; coord c;
-		cin >> input >> c.x >> c.y;
 	
-		if (input == block_str)
-		{
-			bfs.push({ c, 15 });
-			board[c.x][c.y] = 0;
-		}
-		else if (input == lamp_str)
-		{
-			lamp_pos.push_back(c);
-		}
-		else if (input == dust_str)
-		{
-			board[c.x][c.y] = 0;
-		}
-	}
+	make_graph();
 
-	while (false == bfs.empty())
+	visited.resize(graph.size());
+	for (int i = 0; i < visited.size(); ++i)
 	{
-		pci p = bfs.front(); bfs.pop();
-		board[p.cod.x][p.cod.y] = p.energy;
-
-		if (p.energy <= 0) { continue; }
-
-		for (int i = 0; i < 4; ++i)
+		if (visited[i] == false)
 		{
-			coord next_c = p.cod + dirs[i];
-			if (false == next_c.is_valid()) { continue; }
-			//레드스톤 블록이 아닐 경우 or 이미 같거나 큰 에너지로 갱신이 된 블록인 경우
-			if (p.energy - 1 <= board[next_c.x][next_c.y]) { continue; }
-			bfs.push({ next_c, p.energy - 1 });
-		}
-	}
-
-	//사방을 확인해서 동력이 연결된 부분이 있는지 확인
-	//단 하나라도 연결되어있지 않을 경우 fail
-	for (const auto& lamp : lamp_pos)
-	{
-		bool cur_on = false;
-		for (int i = 0; i < 4; ++i)
-		{
-			coord roundpos = lamp + dirs[i];
-			if (roundpos.is_valid())
+			if (detect_cycle(i))
 			{
-				cur_on |= (0 < board[roundpos.x][roundpos.y] && board[roundpos.x][roundpos.y] < 16);
+				cout << "Yes";
+				return;
 			}
 		}
-		if (false == cur_on)
-		{
-			cout << "failed";
-			return;
-		}
 	}
 
-	cout << "success";
+	cout << "No";
 }
