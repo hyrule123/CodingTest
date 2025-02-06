e623dae776a2713454a3c6d0b87a5c586762db8b
HW <ekdrn1@naver.com>
2025-02-06 19:22:56 +0900

    백준 23250 (하노이 탑 K) [재귀]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,7 +14,10 @@ int main()
 }
 
 /*
-백준 23250 (하노이 탑 K) [오답-시간 초과]
+백준 23250 (하노이 탑 K) [재귀]
+* 핵심: 최대 2^60번의 작업 중 K를 찾아야 한다
+* 일일히 카운트해가며 찾으면 당연 시간 초과
+* 찾기 위해서는 답이 나올수 없는 구간은 생략해야 한다
 */
 using ull = unsigned long long;
 ull N, K, cur;
@@ -22,9 +25,19 @@ ull N, K, cur;
 void recursive(ull level, ull from, ull to, ull temp)
 {
 	if (level < 1) { return; }
+	if (cur > K) { return; }
 
+	//이번 원판을 옮기는 비용: 2^level - 1
+	ull count_to_move = (1ull << level) - 1;
+	//이번 원판을 다 옮겨도 K번째가 되지 않는다면
+	if (cur + count_to_move < K)
+	{
+		//과정을 생략한다
+		cur = cur + count_to_move;
+		return;
+	}
 
-	//그럼일단 내 위의 원판(N-1번) 원판을 전부 임시 봉으로 옮겨야 한다
+	//내 원판을 옮길 수 있는 상태가 되려면 위에 있는 모든 원판을 치워야 함
 	recursive(level - 1, from, temp, to);
 
 	//내 원판을 목표로 옮긴다
@@ -43,6 +56,5 @@ void recursive(ull level, ull from, ull to, ull temp)
 void solve()
 {
 	cin >> N >> K;
-
 	recursive(N, 1, 3, 2);
 }
