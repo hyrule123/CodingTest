73f1451a816a432cd625663da506e4374a386c69
HW <ekdrn1@naver.com>
2025-02-06 19:46:55 +0900

    백준 24390 (또 전자레인지야?) [그리디]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,47 +14,44 @@ int main()
 }
 
 /*
-백준 23250 (하노이 탑 K) [재귀]
-* 핵심: 최대 2^60번의 작업 중 K를 찾아야 한다
-* 일일히 카운트해가며 찾으면 당연 시간 초과
-* 찾기 위해서는 답이 나올수 없는 구간은 생략해야 한다
+백준 24390 (또 전자레인지야?) [그리디]
+* 조리버튼을 먼저 누르고 시작하는 경우(-30초)
+* 먼저 분을 맞추고 + 마지막에 조리시작 버튼을 누르는 경우
 */
-using ull = unsigned long long;
-ull N, K, cur;
-//N번 원판을 옮기고 싶다
-void recursive(ull level, ull from, ull to, ull temp)
+int times[] = { 600, 60, 10 };
+int check_count(int sec)
 {
-	if (level < 1) { return; }
-	if (cur > K) { return; }
-
-	//이번 원판을 옮기는 비용: 2^level - 1
-	ull count_to_move = (1ull << level) - 1;
-	//이번 원판을 다 옮겨도 K번째가 되지 않는다면
-	if (cur + count_to_move < K)
+	int count = 1;	//조리버튼 먼저 카운트
+	for (const int t : times)
 	{
-		//과정을 생략한다
-		cur = cur + count_to_move;
-		return;
+		while (t <= sec)
+		{
+			sec -= t;
+			++count;
+		}
 	}
-
-	//내 원판을 옮길 수 있는 상태가 되려면 위에 있는 모든 원판을 치워야 함
-	recursive(level - 1, from, temp, to);
-
-	//내 원판을 목표로 옮긴다
-	//이번 작업이 K번째 작업이라면 출력하고 리턴
-	++cur;
-	if (cur == K)
-	{
-		cout << from << ' ' << to;
-		return;
-	}
-
-	//임시 봉에 있던 원판을 다시 원상복구한다.
-	recursive(level - 1, temp, to, from);
+	return count;
 }
 
 void solve()
 {
-	cin >> N >> K;
-	recursive(N, 1, 3, 2);
+	int m, s; char c;
+	cin >> m >> c >> s;
+	s += m * 60;
+
+	//30초 딱코는 그냥 조리버튼 한번 누르면 됨
+	if (s == 30)
+	{
+		cout << 1;
+		return;
+	}
+
+	int jori_later = check_count(s);
+	int jori_first = 10000;
+	if (30 < s)
+	{
+		jori_first = check_count(s - 30);
+	}
+
+	cout << min(jori_first, jori_later);
 }
