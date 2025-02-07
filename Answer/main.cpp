0348ebf479a66f373a325ef11e05f28e8ffa130b
HW <ekdrn1@naver.com>
2025-02-07 19:04:32 +0900

    백준 14852 (타일 채우기 3) [DP][v2][9832kb][12ms]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,8 +14,9 @@ int main()
 }
 
 /*
-백준 14852 (타일 채우기 3) [DP][v1][17644kb][12ms]
-v1: 첫 번째 풀이
+백준 14852 (타일 채우기 3) [DP][v2][9832kb][12ms]
+v1: 첫번째 풀이 [17644kb][12ms]
+v2: 누적합만 사용하여 메모리 사용 줄인 버전 [9832kb][12ms]
 
 * 위 또는 아래에 ㅡㅡ(1 * 2)를 채울 경우: 4칸을 고려해야함
 	ㅡㅡ		ㅡㅡ		ㅁㅁ
@@ -43,30 +44,28 @@ v1: 첫 번째 풀이
 	길이 3 이상부터 유니크 패턴이 생기므로, 누적합으로 별도로 저장해야 한다.
 */
 using ll = long long;
-constexpr ll mod = 1'000'000'007, cur = 0, partsum = 1;
+constexpr ll mod = 1'000'000'007;
 
-ll dp[1'000'001][2];
+ll dp[1'000'001];
 
 void solve()
 {
 	ll N; cin >> N;
-	dp[1][cur] = 2;
-	dp[1][partsum] = 2;
-	dp[2][cur] = 7;
-	dp[2][partsum] = 2 + 7;
+	dp[1] = 2;
+	dp[2] = 2 + 7;
 	for (ll i = 3; i <= N; ++i)
 	{
-		dp[i][cur] =
-			dp[i - 1][cur] * 2 //세로
-			+ dp[i - 2][cur] * 3 //가로 2 * 2칸을 채우는 방법
-			+ 2	//이번 길이에서 생성된 유니크 패턴 2개
-			+ dp[i - 3][partsum] * 2; //1~(i - 3)까지 채우는 방법에 유니크 패턴을 채우는 방법의 수
-		dp[i][cur] %= mod;
+		//i - 2가 mod로 나눠져서 더 작은수일수도 있으므로 mod를 더해준다.
+		ll prev2 = (mod + dp[i - 2] - dp[i - 3]) % mod;
 
-		//구간합 계산
-		dp[i][partsum] = dp[i - 1][partsum] + dp[i][cur];
-		dp[i][partsum] %= mod;
+		/*
+		dp[i] = dp[i - 1] * 2 + dp[i - 2] * '3' + dp[i - 4] * 2 * ... * dp[1] * 2 + 2
+			== partsum[i - 1] * 2 + (partsum[i - 2] - partsum[i - 3]) + 2
+		-> 2로 묶고, dp[i-2]만 따로 한번 더해줌
+		*/
+		ll cur = (dp[i - 1] * 2 + prev2 + 2) % mod;
+		dp[i] = (dp[i - 1] + cur) % mod;
 	}
 
-	cout << dp[N][cur];
+	cout << (mod + dp[N] - dp[N - 1]) % mod;
 }
