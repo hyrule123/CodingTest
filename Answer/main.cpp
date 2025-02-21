882e6519b17d7fe8514fc26e42f3a905bfd3c000
HW <ekdrn1@naver.com>
2025-02-21 19:34:20 +0900

    백준 18405(경쟁적 전염) [BFS][v2][4ms]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,15 +14,29 @@ int main()
 }
 
 /*
-백준 18405(경쟁적 전염) [BFS][v1][628ms]
+백준 18405(경쟁적 전염) [BFS][v2][4ms]
+v1: 각 박테리아 별로 큐 1000개 사용
+v2: 굳이 큐를 1000개 써야되나? 인풋에서 받고 정렬한 뒤 큐에 넣으면 되지 않나? -> 맞음
 */
 #include <queue>
-queue<pair<int, int>> qs[1001];
+#include <vector>
+#include <algorithm>
+struct input { 
+	int label; 
+	pair<int, int> cod;
+	auto operator <=> (const input& o) const
+	{
+		return label <=> o.label;
+	}
+};
+vector<input> inputs;
+queue<pair<int, int>> q[2];
 int N, K, S, X, Y, board[201][201];
 
 void solve()
 {
 	cin >> N >> K;
+	inputs.reserve(K);
 	for (int r = 1; r <= N; ++r)
 	{
 		for (int c = 1; c <= N; ++c)
@@ -30,9 +44,17 @@ void solve()
 			cin >> board[r][c];
 			if (board[r][c] != 0)
 			{
-				qs[board[r][c]].push({ r, c });
+				inputs.push_back({ board[r][c], {r, c} });
 			}
 		}
+	}
+	//박테리아 번호 순으로 정렬
+	sort(inputs.begin(), inputs.end());
+
+	bool cur = 0;
+	for (auto cod : inputs)
+	{
+		q[cur].push(cod.cod);
 	}
 	cin >> S >> X >> Y;
 
@@ -40,46 +62,42 @@ void solve()
 	while (S)
 	{
 		S--;
-		for (int i = 1; i <= K; ++i)
+		//이번 큐와 다음 큐를 분리
+		while (false == q[cur].empty())
 		{
-			queue<pair<int, int>> new_q;
-			//이번 큐에 있는거 다 쓰고
-			while (false == qs[i].empty())
+			pair<int, int> cur_cod = q[cur].front(); q[cur].pop();
+
+			//up
+			const int R = cur_cod.first, C = cur_cod.second;
+			if (1 <= R - 1 && board[R - 1][C] == 0)
 			{
-				pair<int, int> cur_cod = qs[i].front(); qs[i].pop();
-
-				//up
-				const int R = cur_cod.first, C = cur_cod.second;
-				if (1 <= R - 1 && board[R - 1][C] == 0)
-				{
-					board[R - 1][C] = i;
-					new_q.push({ R - 1, C });
-				}
-
-				//down
-				if (R + 1 <= N && board[R + 1][C] == 0)
-				{
-					board[R + 1][C] = i;
-					new_q.push({ R + 1, C });
-				}
-
-				//left
-				if (1 <= C - 1 && board[R][C - 1] == 0)
-				{
-					board[R][C - 1] = i;
-					new_q.push({ R, C - 1 });
-				}
-
-				//right
-				if (C + 1 <= N && board[R][C + 1] == 0)
-				{
-					board[R][C + 1] = i;
-					new_q.push({ R, C + 1 });
-				}
+				board[R - 1][C] = board[R][C];
+				q[!cur].push({ R - 1, C });
 			}
-			//새로 쌓인 좌표들을 등록
-			qs[i].swap(new_q);
+
+			//down
+			if (R + 1 <= N && board[R + 1][C] == 0)
+			{
+				board[R + 1][C] = board[R][C];
+				q[!cur].push({ R + 1, C });
+			}
+
+			//left
+			if (1 <= C - 1 && board[R][C - 1] == 0)
+			{
+				board[R][C - 1] = board[R][C];
+				q[!cur].push({ R, C - 1 });
+			}
+
+			//right
+			if (C + 1 <= N && board[R][C + 1] == 0)
+			{
+				board[R][C + 1] = board[R][C];
+				q[!cur].push({ R, C + 1 });
+			}
 		}
+		//큐 교체
+		cur = !cur;
 	}
 
 	cout << board[X][Y];
