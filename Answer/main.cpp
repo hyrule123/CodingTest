aa9579bc904953a35060eaf9b8e7ec358e7b4d29
HW <ekdrn1@naver.com>
2025-02-21 19:17:29 +0900

    백준 18405(경쟁적 전염) [BFS][v1][628ms]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,58 +14,73 @@ int main()
 }
 
 /*
-백준 14657(준오는 최종인재야!!) [그래프]
-visited 안 쓴 v2
+백준 18405(경쟁적 전염) [BFS][v1][628ms]
 */
-
-#include <vector>
-#include <cmath>
-double T;
-int N, farthest_node = -1, farthest_node_count = -1, farthest_weighted = -1;
-vector<vector<pair<int, int>>> graph;
-
-void DFS(int cur, int prev, int acc_node_count, int acc_weighted)
-{
-	//노드 개수가 많을 경우 무조건 갱신
-	if (farthest_node_count < acc_node_count)
-	{
-		farthest_node = cur;
-		farthest_node_count = acc_node_count;
-		farthest_weighted = acc_weighted;
-	}
-	//노드 갯수는 동일하나 거리가 더 짧을 경우(더 적은 시간으로 더 많은 문제)
-	else if (farthest_node_count == acc_node_count 
-		&& acc_weighted < farthest_weighted)
-	{
-		farthest_node = cur;
-		farthest_weighted = acc_weighted;
-	}
-
-	for (const auto& edge : graph[cur])
-	{
-		if (edge.first == prev) { continue; }
-		DFS(edge.first, cur, acc_node_count + 1, acc_weighted + edge.second);
-	}
-}
+#include <queue>
+queue<pair<int, int>> qs[1001];
+int N, K, S, X, Y, board[201][201];
 
 void solve()
 {
-	cin >> N >> T;
-	graph.resize(N + 1);
-	
-	for (int i = 0; i < N - 1; ++i)
+	cin >> N >> K;
+	for (int r = 1; r <= N; ++r)
 	{
-		int a, b, c; cin >> a >> b >> c;
-		graph[a].push_back({ b, c });
-		graph[b].push_back({ a, c });
+		for (int c = 1; c <= N; ++c)
+		{
+			cin >> board[r][c];
+			if (board[r][c] != 0)
+			{
+				qs[board[r][c]].push({ r, c });
+			}
+		}
+	}
+	cin >> S >> X >> Y;
+
+	//BFS
+	while (S)
+	{
+		S--;
+		for (int i = 1; i <= K; ++i)
+		{
+			queue<pair<int, int>> new_q;
+			//이번 큐에 있는거 다 쓰고
+			while (false == qs[i].empty())
+			{
+				pair<int, int> cur_cod = qs[i].front(); qs[i].pop();
+
+				//up
+				const int R = cur_cod.first, C = cur_cod.second;
+				if (1 <= R - 1 && board[R - 1][C] == 0)
+				{
+					board[R - 1][C] = i;
+					new_q.push({ R - 1, C });
+				}
+
+				//down
+				if (R + 1 <= N && board[R + 1][C] == 0)
+				{
+					board[R + 1][C] = i;
+					new_q.push({ R + 1, C });
+				}
+
+				//left
+				if (1 <= C - 1 && board[R][C - 1] == 0)
+				{
+					board[R][C - 1] = i;
+					new_q.push({ R, C - 1 });
+				}
+
+				//right
+				if (C + 1 <= N && board[R][C + 1] == 0)
+				{
+					board[R][C + 1] = i;
+					new_q.push({ R, C + 1 });
+				}
+			}
+			//새로 쌓인 좌표들을 등록
+			qs[i].swap(new_q);
+		}
 	}
 
-
-	DFS(1, 0, 0, 0);
-	int farthest_a = farthest_node;
-
-	farthest_node_count = farthest_node = farthest_weighted = -1;
-	DFS(farthest_a, 0, 0, 0);
-
-	cout << (int)ceil((double)farthest_weighted / T);
+	cout << board[X][Y];
 }
