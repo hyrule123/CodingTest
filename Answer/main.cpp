1c4c28218f04feb4613408a3ae427c3172c4e139
HW_LUNAR <ekdrn1@naver.com>
2025-03-20 20:26:29 +0900

    백준 14225 (부분수열의 합) [조합]

--- Answer/main.cpp
+++ Answer/main.cpp
@@ -14,70 +14,49 @@ int main()
 }
 
 /*
-백준 7453 (합이 0인 네 정수) [투 포인터][v4]
-v4: 인풋 받을때 캐시 적중률 올리면 효과 있는지 확인(A[4000], B[4000], ... -> ABCD[4000][4])
--> 효과 없다? 이런것도 최적화를 해 주는건가...
+백준 14225 (부분수열의 합) [조합]
 */
+#include <bitset>
 #include <vector>
 #include <algorithm>
-constexpr int MAX = 4000;
-enum e { a, b, c, d };
-enum ee { ab, cd };
-int AB[MAX * MAX], CD[MAX * MAX], ABCD[MAX][4], N;
+int seq[20], N;
+bitset<2'000'001> is_number;
 
 void solve()
 {
 	cin >> N;
-
 	for (int i = 0; i < N; ++i)
 	{
-		cin >> ABCD[i][a] >> ABCD[i][b] >> ABCD[i][c] >> ABCD[i][d];
+		cin >> seq[i];
 	}
 
-	for (int idx = 0, i = 0; i < N; ++i)
+	for (int len = 1; len <= N; ++len)
 	{
-		for (int j = 0; j < N; ++j)
+		vector<bool> comb; comb.resize(N);
+		for (int i = N - len; i < N; ++i)
 		{
-			AB[idx] = ABCD[i][a] + ABCD[j][b];
-			CD[idx] = ABCD[i][c] + ABCD[j][d];
-			++idx;
+			comb[i] = true;
 		}
+
+		do
+		{
+			int sum = 0;
+			for (int i = 0; i < N; ++i)
+			{
+				if (comb[i])
+				{
+					sum += seq[i];
+				}
+			}
+			is_number[sum] = true;
+		} while (next_permutation(comb.begin(), comb.end()));
 	}
 
-	sort(begin(AB), begin(AB) + N * N);
-	sort(begin(CD), begin(CD) + N * N);
-
-	long long ans = 0;
-	int l = 0, r = N * N - 1;
-	while (l < N * N && r >= 0)
+	for (int i = 1; i < N * 100'000; ++i)
 	{
-		int sum = AB[l] + CD[r];
-		if (sum < 0)
-		{
-			++l;
-		}
-		else if (sum > 0)
-		{
-			--r;
-		}
-		else
-		{
-			int l_n = l + 1, r_n = r - 1;
+		if (is_number[i]) { continue; }
 
-			while (l_n < N * N && AB[l] == AB[l_n])
-			{
-				++l_n;
-			}
-			while (r_n >= 0 && CD[r] == CD[r_n])
-			{
-				--r_n;
-			}
-
-			ans += (long long)(l_n - l) * (long long)(r - r_n);
-
-			l = l_n; r = r_n;
-		}
+		cout << i;
+		return;
 	}
-
-	cout << ans;
 }
